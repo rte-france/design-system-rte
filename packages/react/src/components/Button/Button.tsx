6ccb6457c84afd0520be267af85bdb6b4b4fe02e
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-03-20 16:21:35 +0100

    feat: add icon to button component

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,25 +1,38 @@
 import { forwardRef } from 'react';
 
 import style from './Button.module.scss';
-import { ButtonProps as CoreButtonProps } from '../../../../core/components/button/button.interface';
+import { ButtonSize, ButtonProps as CoreButtonProps } from '../../../../core/components/button/button.interface';
+import Icon from '../Icon/Icon';
 
 type ButtonProps = CoreButtonProps & Omit<React.HTMLProps<HTMLButtonElement>, 'size' | 'type'>;
 
+const buttonIconSize: Record<ButtonSize, number> = {
+  s: 16,
+  m: 20,
+  l: 24,
+};
+
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ size = 'm', label, type = 'filled', disabled = false, className = '', ...props }, ref) => {
+  ({ size = 'm', label, type = 'filled', disabled = false, className = '', leftIcon, rightIcon, ...props }, ref) => {
     return (
-      <button
-        ref={ref}
-        type="button"
-        // TODO: use clsx to generate className concatenation
-        className={style.coreButton + ' ' + className}
-        data-size={size}
-        data-type={type}
-        disabled={disabled}
-        {...props}
-      >
-        {label}
-      </button>
+      <>
+        <button
+          ref={ref}
+          type="button"
+          // TODO: use clsx to generate className concatenation
+          className={style.button + ' ' + className}
+          data-size={size}
+          data-type={type}
+          disabled={disabled}
+          {...props}
+        >
+          {leftIcon && <Icon name={leftIcon} size={buttonIconSize[size]} />}
+          <span data-size={size} className={style.label}>
+            {label}
+          </span>
+          {rightIcon && <Icon name={rightIcon} />}
+        </button>
+      </>
     );
   },
 );
---
+++ packages/react/src/components/Icon/Icon.tsx
@@ -0,0 +1,16 @@
+import { IconProps } from '../../../../core/components/icon/icon.interface';
+
+export default function Icon({ name, size = 24 }: IconProps) {
+  return (
+    <span
+      data-testid={`icon-${name}`}
+      className="material-symbols-rounded"
+      style={{
+        fontSize: size,
+        color: 'inherit',
+      }}
+    >
+      {name}
+    </span>
+  );
+}
