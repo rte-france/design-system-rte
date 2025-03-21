92aa71a118a84995ee0be672aba2014f5592fa58
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-03-21 10:52:19 +0100

    feat: use interface to define props definition

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -4,7 +4,9 @@ import style from './Button.module.scss';
 import { ButtonSize, ButtonProps as CoreButtonProps } from '../../../../core/components/button/button.interface';
 import Icon from '../Icon/Icon';
 
-type ButtonProps = CoreButtonProps & Omit<React.HTMLProps<HTMLButtonElement>, 'size' | 'type'>;
+interface ButtonProps
+  extends CoreButtonProps,
+    Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'size' | 'type' | 'label'> {}
 
 const buttonIconSize: Record<ButtonSize, number> = {
   s: 16,
@@ -15,24 +17,22 @@ const buttonIconSize: Record<ButtonSize, number> = {
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ size = 'm', label, type = 'filled', disabled = false, className = '', leftIcon, rightIcon, ...props }, ref) => {
     return (
-      <>
-        <button
-          ref={ref}
-          type="button"
-          // TODO: use clsx to generate className concatenation
-          className={style.button + ' ' + className}
-          data-size={size}
-          data-type={type}
-          disabled={disabled}
-          {...props}
-        >
-          {leftIcon && <Icon name={leftIcon} size={buttonIconSize[size]} />}
-          <span data-size={size} className={style.label}>
-            {label}
-          </span>
-          {rightIcon && <Icon name={rightIcon} />}
-        </button>
-      </>
+      <button
+        ref={ref}
+        type="button"
+        // TODO: use clsx to generate className concatenation
+        className={style.button + ' ' + className}
+        data-size={size}
+        data-type={type}
+        disabled={disabled}
+        {...props}
+      >
+        {leftIcon && <Icon name={leftIcon} size={buttonIconSize[size]} />}
+        <span data-size={size} className={style.label}>
+          {label}
+        </span>
+        {rightIcon && <Icon name={rightIcon} />}
+      </button>
     );
   },
 );
