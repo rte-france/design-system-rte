24fba23b5809f1022da42af57235ae565c119a42
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-03-21 15:55:20 +0100

    feat: add onClick props

--- packages/core/components/button/button.interface.d.ts
+++ packages/core/components/button/button.interface.d.ts
@@ -1,11 +1,12 @@
 export type ButtonType = 'filled' | 'outlined' | 'text' | 'transparent' | 'danger'
 export type ButtonSize = 's' | 'm' | 'l';
+export type ButtonIconPosition = 'left' | 'right';
 
 export interface ButtonProps {
     label: string;
     type?: ButtonType;
     size?: ButtonSize;
     disabled?: boolean;
-    leftIcon?: string;
-    rightIcon?: string;
+    icon?: string;
+    iconPosition?: ButtonIconPosition
 }
--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,29 +1,14 @@
 import { forwardRef } from 'react';
 
+import { ButtonSize, ButtonProps as CoreButtonProps } from '@design-system-rte/core/components/button/button.interface';
 import style from './Button.module.scss';
-import { ButtonSize, ButtonProps as CoreButtonProps } from '../../../../core/components/button/button.interface';
 import Icon from '../Icon/Icon';
 
-interface BaseButtonProps
+interface ButtonProps
   extends CoreButtonProps,
-    Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'size' | 'type' | 'label'> {}
-
-interface LeftIconButtonProps extends BaseButtonProps {
-  leftIcon: string;
-  rightIcon?: never;
+    Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'size' | 'type' | 'label' | 'onClick'> {
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
-
-interface RightIconButtonProps extends BaseButtonProps {
-  rightIcon: string;
-  leftIcon?: never;
-}
-
-interface NoIconButtonProps extends BaseButtonProps {
-  leftIcon?: never;
-  rightIcon?: never;
-}
-
-type ButtonProps = LeftIconButtonProps | RightIconButtonProps | NoIconButtonProps;
 
 const buttonIconSize: Record<ButtonSize, number> = {
   s: 16,
@@ -32,7 +17,20 @@ const buttonIconSize: Record<ButtonSize, number> = {
 };
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ size = 'm', label, type = 'filled', disabled = false, className = '', leftIcon, rightIcon, ...props }, ref) => {
+  (
+    {
+      size = 'm',
+      label,
+      type = 'filled',
+      disabled = false,
+      className = '',
+      icon,
+      iconPosition = 'left',
+      onClick,
+      ...props
+    },
+    ref,
+  ) => {
     return (
       <button
         ref={ref}
@@ -42,13 +40,14 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         data-size={size}
         data-type={type}
         disabled={disabled}
+        onClick={onClick}
         {...props}
       >
-        {leftIcon && <Icon name={leftIcon} size={buttonIconSize[size]} />}
+        {icon && iconPosition === 'left' && <Icon name={icon} size={buttonIconSize[size]} />}
         <span data-size={size} className={style.label}>
           {label}
         </span>
-        {rightIcon && <Icon name={rightIcon} />}
+        {icon && iconPosition === 'right' && <Icon name={icon} size={buttonIconSize[size]} />}
       </button>
     );
   },
