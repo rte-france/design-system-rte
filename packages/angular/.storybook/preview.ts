c53a6b1638f86d474ecdf528f781f86565afda52
Tommahs <thomascandille0508@gmail.com>
2025-03-27 10:53:21 +0100

    feat: add Angular and React references to Storybook configuration

--- packages/angular/.storybook/preview.ts
+++ packages/angular/.storybook/preview.ts
@@ -3,6 +3,7 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
+
 const preview: Preview = {
   parameters: {
     controls: {
--- packages/react/storybook-static/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { default as Button } from './components/Button/Button';
-export { Button };
