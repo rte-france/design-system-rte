7dfda22f93f4f1ff9a01fc1af0040003bba7f7e0
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-04-16 17:20:14 +0200

    feat: add packages/core publication config

---
+++ packages/react/src/components/Button/Button.tsx
@@ -0,0 +1,27 @@
+import { forwardRef } from 'react';
+
+import style from './Button.module.scss';
+import { ButtonProps as CoreButtonProps } from '@design-system-rte/core';
+
+type ButtonProps = CoreButtonProps & Omit<React.HTMLProps<HTMLButtonElement>, 'size' | 'type'>;
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ size = 'm', label, type = 'filled', disabled = false, className = '', ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        type="button"
+        // TODO: use clsx to generate className concatenation
+        className={style.coreButton + ' ' + className}
+        data-size={size}
+        data-type={type}
+        disabled={disabled}
+        {...props}
+      >
+        {label}
+      </button>
+    );
+  },
+);
+
+export default Button;
