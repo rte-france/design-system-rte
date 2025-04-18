6beb56a5ba9afd550d4ca80c1f5b4a31c1f66813
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-04-18 16:45:29 +0200

    feat: add react grid component

---
+++ packages/react/src/components/grid/Grid.tsx
@@ -0,0 +1,49 @@
+import { concatClassNames } from '../utils';
+import {
+  GridProps as CoreGridProps,
+  ColProps as CoreColProps,
+} from '@design-system-rte/core/components/grid/grid.interface';
+import './Grid.scss';
+
+interface GridProps extends CoreGridProps, Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {
+  children: React.ReactNode[];
+}
+
+interface ColProps extends CoreColProps, Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {
+  children: React.ReactNode;
+}
+
+const Col = ({ children, className, xxs, xs, s, m, l, xl, ...props }: ColProps) => {
+  const generateColumnClass = (prefix: string, size?: number) => (size ? `${prefix}-${size}` : '');
+
+  const colClasses = concatClassNames(
+    'col',
+    generateColumnClass('col-xxs', xxs),
+    generateColumnClass('col-xs', xs),
+    generateColumnClass('col-s', s),
+    generateColumnClass('col-m', m),
+    generateColumnClass('col-l', l),
+    generateColumnClass('col-xl', xl),
+    className,
+  );
+
+  return (
+    <div className={colClasses} {...props}>
+      {children}
+    </div>
+  );
+};
+
+const Grid = ({ gridType = 'fluid', className, children }: GridProps) => {
+  const gridClasses = concatClassNames('grid', className);
+
+  return (
+    <div className={gridClasses} data-gridtype={gridType}>
+      {children}
+    </div>
+  );
+};
+
+Grid.Col = Col;
+
+export default Grid;
