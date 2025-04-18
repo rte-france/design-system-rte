10cc583d66de031abcef4aa50603bd45fbf73a84
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-04-18 16:45:29 +0200

    feat: add react grid component

---
+++ packages/core/components/grid/grid.interface.d.ts
@@ -0,0 +1,12 @@
+export interface GridProps {
+  gridType?: 'fluid' | 'fixed-narrow' | 'fixed-wide';
+}
+
+export interface ColProps {
+  xxs?: number;
+  xs?: number;
+  s?: number;
+  m?: number;
+  l?: number;
+  xl?: number;
+}
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,5 @@
 import './styles.d.ts';
 import Button from './components/button/Button';
+import Grid from './components/grid/Grid.tsx';
 
-export { Button };
+export { Button, Grid };
