8cfa74ef02861ae04b62b25a1203a5fcdfc094fe
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-04-25 17:34:34 +0200

    [WIP] feature: implement /src to /dist direct build solution

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,23 +1,35 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import glob from 'glob';
+
+const files = glob.sync('src/**/*.ts', {
+  ignore: ['src/**/*.test.ts', 'src/**/*.stories.ts']
+});
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
+      entry: files.reduce((entries, file) => {
+        const name = file.replace('src/', '');
+        entries[name.replace('.ts', '')] = resolve(__dirname, file);
+        return entries;
+      }, {}),
+      formats: ['es']
+    },
+    rollupOptions: {
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        entryFileNames: '[name].js'
+      }
     }
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.d.ts'],
   },
   plugins: [
     dts({
-      include: ['src/**/*.ts', 'src/**/*.d.ts'],
+      include: ['src/**/*.ts'],
       exclude: ['**/*.test.ts'],
-      rollupTypes: true,
-      insertTypesEntry: true,
+      rollupTypes: true
     })
   ]
 });
