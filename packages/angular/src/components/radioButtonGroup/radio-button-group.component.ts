33b951b6e2c890ef32ed2b5387c35f7458168d46
Tommahs <thomascandille0508@gmail.com>
2025-04-30 16:41:29 +0200

    feat: add RadioButtonGroup component with interactive states and styles for Angular

---
+++ packages/angular/src/components/radioButtonGroup/radio-button-group.component.ts
@@ -0,0 +1,30 @@
+import { Component, computed, input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RadioButtonComponent } from "../radioButton/radio-button.component";
+
+@Component({
+  selector: 'rte-radio-button-group',
+  standalone: true,
+  imports: [CommonModule, RadioButtonComponent],
+  templateUrl: './radio-button-group.component.html',
+  styleUrls: ['./radio-button-group.component.scss'],
+})
+
+export class RadioButtonGroupComponent {
+    groupName = input('');
+    items = input<string[]>([]);
+    direction = input('horizontal');
+    showItemsLabel = input(true);
+    groupTitle = input('');
+    showGroupTitle = input(false);
+    groupHelpText = input('');
+    showHelpText = input(false);
+    errorMessage = input('');
+    error = input(false);
+    disabled = input(false);
+    readOnly = input(false);
+
+    isInteractive = computed(() => !(this.disabled()==true && this.error()==true));
+
+}
+
