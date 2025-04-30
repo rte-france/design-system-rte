fe66341ae990ae902ff4fef29fa7e2398798fd43
Tommahs <thomascandille0508@gmail.com>
2025-04-30 16:41:29 +0200

    feat: add RadioButtonGroup component with styles and stories for React

---
+++ packages/react/src/components/radioButtonGroup/RadioButtonGroup.tsx
@@ -0,0 +1,69 @@
+import React, { forwardRef } from "react";
+import { RadioButtonGroupProps as CoreRadioButtonGroupProps } from "@design-system-rte/core/components/radio-button-group/radio-button-group.interface";
+import { concatClassNames } from "../utils";
+import RadioButton from "../radioButton/RadioButton";
+import style from "./RadioButtonGroup.module.scss";
+
+interface RadioButtonGroupProps
+    extends CoreRadioButtonGroupProps,
+    React.AnchorHTMLAttributes<HTMLDivElement> { }
+
+const RadioButtonGroup = forwardRef<HTMLDivElement, RadioButtonGroupProps>(({
+    groupName,
+    items,
+    direction = "horizontal",
+    showItemsLabel = true,
+    groupTitle = '',
+    showGroupTitle = false,
+    groupHelpText = '',
+    showHelpText = false,
+    errorMessage = '',
+    error = false,
+    disabled = false,
+    readOnly = false,
+    className = "",
+    ...props
+}, ref) => {
+
+    if (disabled && error) {
+        return null;
+    }
+
+    return (
+        <div
+            ref={ref}
+            className={concatClassNames(style.radioButtonGroupContainer, className)}
+            data-error={error}
+            data-disabled={disabled}
+            data-read-only={readOnly}
+            {...props}
+        >
+            <div className={style.radioButtonGroupHeader}>
+                {groupTitle && showGroupTitle && (
+                    <h3 className={style.groupTitle}>{groupTitle}</h3>
+                )}
+                {groupHelpText && showHelpText && (
+                    <p className={style.groupHelpText}>{groupHelpText}</p>
+                )}
+                {errorMessage && error && (
+                    <p className={style.errorMessage}>{errorMessage}</p>
+                )}
+            </div>
+            <div className={style.radioButtonGroup}>
+                {items.map((item, index) => (
+                    <RadioButton
+                        key={index}
+                        label={item}
+                        groupName={groupName}
+                        showLabel={showItemsLabel}
+                        disabled={disabled}
+                        error={error}
+                        readOnly={readOnly}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+})
+
+export default RadioButtonGroup;
