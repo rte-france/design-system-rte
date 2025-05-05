219dadb45deadf4a34eecdba9efd9ce0d5d24950
Jeremy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-05-05 18:28:27 +0200

    feat: Add stories for checkbox

--- packages/react/src/components/checkbox/Checkbox.stories.tsx
+++ packages/react/src/components/checkbox/Checkbox.stories.tsx
@@ -1,11 +1,35 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Checkbox from './Checkbox';
+import { userEvent, within, expect } from '@storybook/test';
+import { SPACE_KEY, TAB_KEY } from '@design-system-rte/core/constants/keyboard.constants';
 
 const meta = {
   title: 'Checkbox',
   component: Checkbox,
   tags: ['autodocs'],
+  argTypes: {
+    label: {
+      control: 'text',
+      defaultValue: 'Label',
+    },
+    showLabel: {
+      control: 'boolean',
+      defaultValue: true,
+    },
+    disabled: {
+      control: 'boolean',
+      defaultValue: false,
+    },
+    errorMessage: {
+      control: 'text',
+      defaultValue: '',
+    },
+    readOnly: {
+      control: 'boolean',
+      defaultValue: false,
+    },
+  },
 } satisfies Meta<typeof Checkbox>;
 
 export default meta;
@@ -13,11 +37,79 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
+    id: 'my-checkbox',
     label: 'Label',
     description: 'Description',
     disabled: false,
     readOnly: false,
-    indeterminate: true,
+    showLabel: true,
+    indeterminate: false,
     errorMessage: '',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const checkbox = canvas.getByRole('checkbox');
+    await userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+    checkbox.blur();
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const checkbox = canvas.getByRole('checkbox');
+    expect(checkbox).toBeDisabled();
+    await userEvent.click(checkbox);
+    expect(checkbox).not.toBeChecked();
+  },
+};
+
+export const Indeterminated: Story = {
+  args: {
+    ...Default.args,
+    indeterminate: true,
+  },
+};
+
+export const ReadOnly: Story = {
+  args: {
+    ...Default.args,
+    readOnly: true,
+    checked: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const checkbox = canvas.getByRole('checkbox');
+    await userEvent.keyboard(`{${TAB_KEY}}`);
+    expect(checkbox).toHaveFocus();
+    await userEvent.keyboard(`{${SPACE_KEY}}`);
+    expect(checkbox).toBeChecked();
+  },
+};
+
+export const Error: Story = {
+  args: {
+    ...Default.args,
+    errorMessage: 'Error message',
+  },
+};
+
+export const KeyboardInteractions: Story = {
+  args: {
+    ...Default.args,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const checkbox = canvas.getByRole('checkbox');
+    await userEvent.keyboard(`{${TAB_KEY}}`);
+    expect(checkbox).toHaveFocus();
+    await userEvent.keyboard(`{${SPACE_KEY}}`);
+    expect(checkbox).toBeChecked();
+    checkbox.blur();
   },
 };
