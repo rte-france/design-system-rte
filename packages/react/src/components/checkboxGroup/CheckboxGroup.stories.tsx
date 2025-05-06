d41ff9dac401f75818b8b90f5a4b05c6270529e9
Jeremy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-05-06 17:19:29 +0200

    feat: Add react Checkbox group

---
+++ packages/react/src/components/checkboxGroup/CheckboxGroup.stories.tsx
@@ -0,0 +1,134 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within, expect } from '@storybook/test';
+import CheckboxGroup from './CheckboxGroup';
+
+const meta = {
+  title: 'CheckboxGroup',
+  component: CheckboxGroup,
+  tags: ['autodocs'],
+  argTypes: {
+    groupName: {
+      control: 'text',
+      description: 'The name of the checkbox group.',
+      defaultValue: 'group1',
+    },
+    items: {
+      control: 'object',
+      description: 'The items in the checkbox group.',
+      defaultValue: ['Option 1', 'Option 2', 'Option 3'],
+    },
+    direction: {
+      control: 'select',
+      options: ['horizontal', 'vertical'],
+      description: 'The direction of the checkbox group.',
+      defaultValue: 'horizontal',
+    },
+    showItemsLabel: {
+      control: 'boolean',
+      description: 'Whether to show the label for each item.',
+      defaultValue: true,
+    },
+    groupTitle: {
+      control: 'text',
+      description: 'The title of the checkbox group.',
+      defaultValue: 'Checkbox Group Title',
+    },
+    showGroupTitle: {
+      control: 'boolean',
+      description: 'Whether to show the group title.',
+      defaultValue: true,
+    },
+    groupHelpText: {
+      control: 'text',
+      description: 'The help text for the checkbox group.',
+      defaultValue: 'This is a help text for the checkbox group.',
+    },
+    showHelpText: {
+      control: 'boolean',
+      description: 'Whether to show the help text.',
+      defaultValue: true,
+    },
+    errorMessage: {
+      control: 'text',
+      description: 'The error message to display when there is an error. Use `error` prop to trigger this message.',
+      defaultValue: 'This is an error message. Please select an option.',
+    },
+    error: {
+      control: 'boolean',
+      description: 'Whether to show the error message. Use `errorMessage` prop to set the message.',
+      defaultValue: false,
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Whether the checkbox group is disabled. This will disable all checkboxs in the group.',
+      defaultValue: false,
+    },
+    readOnly: {
+      control: 'boolean',
+      description: 'Whether the checkbox group is read-only. This will make all checkboxs in the group read-only.',
+      defaultValue: false,
+    },
+  },
+} satisfies Meta<typeof CheckboxGroup>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    groupName: 'group1',
+    items: ['Option 1', 'Option 2', 'Option 3'],
+    direction: 'horizontal',
+    showItemsLabel: true,
+    groupTitle: 'Checkbox Group Title',
+    showGroupTitle: true,
+    groupHelpText: 'This is a help text for the checkbox group.',
+    showHelpText: true,
+    errorMessage: 'This is an error message. Please select an option.',
+    error: false,
+    disabled: false,
+    readOnly: false,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const radioButton = canvas.getByRole('radio', { name: 'Option 1' });
+    await userEvent.click(radioButton);
+    expect(radioButton).toBeChecked();
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
+};
+
+export const Error: Story = {
+  args: {
+    ...Default.args,
+    error: true,
+    errorMessage: 'This is an error message. Please select an option.',
+  },
+};
+
+export const ReadOnly: Story = {
+  args: {
+    ...Default.args,
+    readOnly: true,
+  },
+};
+
+export const Directions: Story = {
+  args: {
+    ...Default.args,
+  },
+  render: (args) => {
+    return (
+      <div style={{ display: 'flex', gap: 8 }}>
+        <CheckboxGroup {...args} direction="horizontal" groupName="horizontal" />
+        <CheckboxGroup {...args} direction="vertical" groupName="vertical" />
+      </div>
+    );
+  },
+};
---
+++ packages/react/src/components/checkboxGroup/CheckboxGroup.tsx
@@ -0,0 +1,64 @@
+import React, { forwardRef } from 'react';
+import { CheckboxGroupProps as CoreCheckboxGroupProps } from '@design-system-rte/core/components/checkbox-group/checkbox-group.interface';
+import { concatClassNames } from '../utils';
+import Checkbox from '../checkbox/Checkbox';
+import style from './CheckboxGroup.module.scss';
+
+interface CheckboxGroupProps extends CoreCheckboxGroupProps, React.InputHTMLAttributes<HTMLDivElement> {}
+
+const CheckboxGroup = forwardRef<HTMLDivElement, CheckboxGroupProps>(
+  (
+    {
+      groupName,
+      items,
+      direction = 'horizontal',
+      showItemsLabel = true,
+      groupTitle = '',
+      showGroupTitle = false,
+      groupHelpText = '',
+      showHelpText = false,
+      errorMessage = '',
+      error = false,
+      disabled = false,
+      readOnly = false,
+      className = '',
+      ...props
+    },
+    ref,
+  ) => {
+    if (disabled && error) {
+      return;
+    }
+
+    return (
+      <div ref={ref} className={concatClassNames(style.checkboxGroupContainer, className)} {...props}>
+        <div
+          className={style.checkboxGroupHeader}
+          data-error={error}
+          data-disabled={disabled}
+          data-read-only={readOnly}
+        >
+          {groupTitle && showGroupTitle && <h3 className={style.groupTitle}>{groupTitle}</h3>}
+          {groupHelpText && showHelpText && <p className={style.groupHelpText}>{groupHelpText}</p>}
+          {errorMessage && error && <p className={style.errorMessage}>{errorMessage}</p>}
+        </div>
+        <div className={style.checkboxGroup} data-direction={direction}>
+          {items.map((item, index) => (
+            <Checkbox
+              id={`${item}-${index}`}
+              key={`${item}-${index}`}
+              label={item}
+              groupName={groupName}
+              showLabel={showItemsLabel}
+              disabled={disabled}
+              error={error}
+              readOnly={readOnly}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  },
+);
+
+export default CheckboxGroup;
