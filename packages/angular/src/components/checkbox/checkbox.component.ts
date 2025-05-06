7a9a3f05b7c0422d75f8b1a44e355afc1d8ef283
Jeremy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-05-06 11:51:13 +0200

    feat: Add angular checkbox

---
+++ packages/angular/src/components/checkbox/checkbox.component.ts
@@ -0,0 +1,23 @@
+import { Component, input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: 'rte-checkbox',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss',
+})
+
+export class CheckboxComponent {
+    id = input('');
+    value = input('');
+    label = input('');
+    indeterminate = input(false);
+    description = input('');
+    showLabel = input(true);
+    disabled = input(false);
+    errorMessage = input('');
+    readOnly = input(false);
+    checked = input(false);
+}
