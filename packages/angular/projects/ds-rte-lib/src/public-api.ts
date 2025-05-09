649e9bf0a21b6bd0eef4a583cf71391b62079df0
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-05-09 17:07:28 +0200

    feat: set angular bundling for publication

---
+++ packages/angular/projects/ds-rte-lib/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of lib
+ */
+
+export * from './lib/components/button/button.component';
+export * from './lib/components/grid/grid.directive';
+export * from './lib/components/grid/col/col.directive';
+export * from './lib/components/link/link.component';
+export * from './lib/components/radio-button/radio-button.component';
+export * from './lib/components/radio-button-group/radio-button-group.component';
