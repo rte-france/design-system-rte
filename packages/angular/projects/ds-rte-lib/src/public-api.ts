b585992d1fb776b06dee4553197aa723614c9013
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-05-13 15:25:18 +0200

    feature: add checkbox to new angular bundle structure

--- packages/angular/projects/ds-rte-lib/src/public-api.ts
+++ packages/angular/projects/ds-rte-lib/src/public-api.ts
@@ -8,3 +8,6 @@ export * from './lib/components/grid/col/col.directive';
 export * from './lib/components/link/link.component';
 export * from './lib/components/radio-button/radio-button.component';
 export * from './lib/components/radio-button-group/radio-button-group.component';
+export * from './lib/components/checkbox/checkbox.component';
+export * from './lib/components/checkbox-group/checkbox-group.component';
+
