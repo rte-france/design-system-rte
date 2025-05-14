efda9aded1f9b3f6920c4b3cc85a05a52a9b18b4
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-05-14 18:12:00 +0200

    feat: use new Icon component

--- packages/react/src/components/icon/Icon.tsx
+++ packages/react/src/components/icon/Icon.tsx
@@ -2,10 +2,10 @@ import { IconIds } from './IconMap';
 
 export type IconIdKey = keyof typeof IconIds;
 
-const Icon = ({ name, size }: { name: IconIdKey; size?: number }) => {
+const Icon = ({ name, size, fill = 'currentColor' }: { name: IconIdKey; size?: number; fill?: string }) => {
   const Icon = IconIds[name as IconIdKey];
 
-  return <Icon width={size} height={size} fill="currentColor" />;
+  return <Icon width={size} height={size} fill={fill} />;
 };
 
 export default Icon;
