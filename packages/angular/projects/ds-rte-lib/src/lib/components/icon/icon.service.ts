745e873488fd6dac4c3d30a225cc2c3c182a9919
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-05-23 17:42:53 +0200

    feat: add handle appearances for icon

--- packages/angular/projects/ds-rte-lib/src/lib/components/icon/icon.service.ts
+++ packages/angular/projects/ds-rte-lib/src/lib/components/icon/icon.service.ts
@@ -2,23 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import {iconMap} from './icon-map';
+import {regularIcons, togglableIcons} from './icon-map';
 
-export type IconType = keyof typeof iconMap;
+export type RegularIconIdKey = keyof typeof regularIcons;
+export type TogglableIconIdKey = keyof typeof togglableIcons;
 
 @Injectable()
 export class IconService {
 
   constructor(private http: HttpClient) {}
 
-  public getSvgForName(name: string): Observable<string> {
+  public getSvg(name: RegularIconIdKey | TogglableIconIdKey, appearance: 'outlined' | 'filled' = 'outlined'): Observable<string> {
     if(!this.http) {
       throw new Error('HttpClient is not available');
     }
-    const iconName = iconMap[name as IconType];
-    if (!iconName) {
-      throw new Error(`Icon ${name} not found`);
+    const toggableIcon = togglableIcons[name as TogglableIconIdKey];
+
+    if (toggableIcon) {
+      const [outlinedIconName, filledIconName] = toggableIcon;
+      return appearance === 'filled' ? this.http.get(`assets/icons/${filledIconName}.svg`, { responseType: 'text' }) : this.http.get(`assets/icons/${outlinedIconName}.svg`, { responseType: 'text' });
+    } else {
+      const iconName = regularIcons[name as RegularIconIdKey];
+      if (!iconName) {
+        throw new Error(`Icon ${name} not found`);
+      }
+      return this.http.get(`assets/icons/${iconName}.svg`, { responseType: 'text' });
     }
-    return this.http.get(`assets/icons/${iconName}.svg`, { responseType: 'text' });
   }
 }
--- packages/react/src/components/icon/Icon.tsx
+++ packages/react/src/components/icon/Icon.tsx
@@ -1,12 +1,31 @@
-import { IconWrapperProps } from '@design-system-rte/core/components/icon/icon.interface';
-import { IconIds } from './IconMap';
+import { IconWrapperProps as CoreIconWrapperProps } from '@design-system-rte/core/components/icon/icon.interface';
+import { IconIds, TogglableIcons } from './IconMap';
 
-export type IconIdKey = keyof typeof IconIds;
+export type RegularIconIdKey = keyof typeof IconIds;
+export type TogglableIconIdKey = keyof typeof TogglableIcons;
 
-const Icon = ({ name, size, color }: IconWrapperProps) => {
-  const Icon = IconIds[name as IconIdKey];
+interface IconWrapperProps extends CoreIconWrapperProps {
+  name: RegularIconIdKey | TogglableIconIdKey;
+  appearance?: 'outlined' | 'filled';
+}
 
-  return <Icon width={size} height={size} fill={color} />;
+const Icon = ({ name, size, color, appearance }: IconWrapperProps) => {
+  const toggableIcon = TogglableIcons[name as TogglableIconIdKey];
+  if (toggableIcon) {
+    const [OutlinedIcon, FilledIcon] = toggableIcon;
+    return appearance === 'filled' ? (
+      <FilledIcon width={size} height={size} fill={color} />
+    ) : (
+      <OutlinedIcon width={size} height={size} fill={color} />
+    );
+  } else {
+    const Icon = IconIds[name as RegularIconIdKey];
+    if (!Icon) {
+      console.warn(`Icon with name "${name}" not found.`);
+      return null;
+    }
+    return <Icon width={size} height={size} fill={color} />;
+  }
 };
 
 export default Icon;
