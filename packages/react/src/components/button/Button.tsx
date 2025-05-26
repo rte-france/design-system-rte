e6d3ec5a0ebf0e8b2a1ec90c78443c00f74a8c6a
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-05-26 16:19:24 +0200

    feat: add Icon stories

--- packages/react/src/components/button/Button.tsx
+++ packages/react/src/components/button/Button.tsx
@@ -1,8 +1,6 @@
 import { forwardRef } from 'react';
 
 import { ButtonProps as CoreButtonProps } from '@design-system-rte/core/components/button/button.interface';
-import { IconKeys } from '@design-system-rte/core/components/icon/icon.interface';
-import { buttonIconSize } from '@design-system-rte/core/components/button/button.constants';
 import style from './Button.module.scss';
 import { concatClassNames } from '../utils';
 import Icon from '../icon/Icon';
@@ -10,41 +8,28 @@ import Icon from '../icon/Icon';
 interface ButtonProps
   extends Omit<CoreButtonProps, 'disabled'>,
     Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'> {
-  icon?: IconKeys;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    {
-      size = 'm',
-      label,
-      variant = 'primary',
-      className = '',
-      icon,
-      iconPosition = 'left',
-      onClick,
-      type = 'button',
-      ...props
-    },
-    ref,
-  ) => {
+  ({ size = 'm', label, variant = 'primary', className = '', onClick, type = 'button', ...props }, ref) => {
     return (
-      <button
-        ref={ref}
-        type={type}
-        className={concatClassNames(style.button, className)}
-        data-size={size}
-        data-variant={variant}
-        onClick={onClick}
-        {...props}
-      >
-        {icon && iconPosition === 'left' && <Icon name={icon} size={buttonIconSize[size]} />}
-        <span data-size={size} className={style.label}>
-          {label}
-        </span>
-        {icon && iconPosition === 'right' && <Icon name={icon} size={buttonIconSize[size]} />}
-      </button>
+      <>
+        <button
+          ref={ref}
+          type={type}
+          className={concatClassNames(style.button, className)}
+          data-size={size}
+          data-variant={variant}
+          onClick={onClick}
+          {...props}
+        >
+          <span data-size={size} className={style.label}>
+            {label}
+          </span>
+        </button>
+        <Icon name="mode-dark" size={24} color="red" appearance="filled" />
+      </>
     );
   },
 );
--- packages/react/src/components/icon/Icon.tsx
+++ packages/react/src/components/icon/Icon.tsx
@@ -1,22 +1,25 @@
 import { IconWrapperProps as CoreIconWrapperProps } from '@design-system-rte/core/components/icon/icon.interface';
+
 import { IconIds, TogglableIcons } from './IconMap';
 
 export type RegularIconIdKey = keyof typeof IconIds;
 export type TogglableIconIdKey = keyof typeof TogglableIcons;
 
-interface IconWrapperProps extends CoreIconWrapperProps {
+interface IconWrapperProps
+  extends CoreIconWrapperProps,
+    Omit<React.SVGProps<SVGSVGElement>, 'name' | 'width' | 'height'> {
   name: RegularIconIdKey | TogglableIconIdKey;
   appearance?: 'outlined' | 'filled';
 }
 
-const Icon = ({ name, size, color, appearance }: IconWrapperProps) => {
+const Icon = ({ name, size, color, appearance, ...props }: IconWrapperProps) => {
   const toggableIcon = TogglableIcons[name as TogglableIconIdKey];
   if (toggableIcon) {
     const [OutlinedIcon, FilledIcon] = toggableIcon;
     return appearance === 'filled' ? (
-      <FilledIcon width={size} height={size} fill={color} />
+      <FilledIcon width={size} height={size} fill={color} {...props} />
     ) : (
-      <OutlinedIcon width={size} height={size} fill={color} />
+      <OutlinedIcon width={size} height={size} fill={color} {...props} />
     );
   } else {
     const Icon = IconIds[name as RegularIconIdKey];
@@ -24,7 +27,7 @@ const Icon = ({ name, size, color, appearance }: IconWrapperProps) => {
       console.warn(`Icon with name "${name}" not found.`);
       return null;
     }
-    return <Icon width={size} height={size} fill={color} />;
+    return <Icon width={size} height={size} fill={color} {...props} />;
   }
 };
 
