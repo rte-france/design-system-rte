b81046914dc1ec91f857adbd3e5cf69f86b567b7
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-05-26 16:18:18 +0200

    refacto: use signal for angular icon component

--- packages/angular/projects/ds-rte-lib/src/lib/components/icon/icon.component.ts
+++ packages/angular/projects/ds-rte-lib/src/lib/components/icon/icon.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, input, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { IconService, RegularIconIdKey, TogglableIconIdKey } from './icon.service';
 import { Observable } from 'rxjs';
@@ -14,25 +14,24 @@ import { CommonModule } from '@angular/common';
   encapsulation: ViewEncapsulation.None
 })
 export class IconComponent {
-  @Input() name!: RegularIconIdKey | TogglableIconIdKey;
-  @Input() size: number = 20;
-  @Input() color: string = 'currentColor';
-  @Input() classes: string ='';
-  @Input() appearance!: 'outlined' | 'filled';
+  name = input.required<RegularIconIdKey | TogglableIconIdKey>();
+  size = input(20);
+  color = input('currentColor');
+  classes = input('');
+  appearance = input<'outlined' | 'filled'>();
 
   svgContent: SafeHtml | null = null;
   svg!: Observable<string>;
 
   constructor(private sanitizer: DomSanitizer,  private iconService: IconService, private cdr: ChangeDetectorRef) {}
 
-  ngOnInit(): void {
-    this.setSvgContent(this.name);
+  ngOnChanges(): void {
+    this.setSvgContent(this.name());
   }
 
   private setSvgContent(svgName: RegularIconIdKey | TogglableIconIdKey) {
 
-    const svgFile = this.iconService.getSvg(svgName)
-    console.log('svgName', svgName);
+    const svgFile = this.iconService.getSvg(svgName, this.appearance() || 'outlined');
 
     svgFile.subscribe((res) => {
 
