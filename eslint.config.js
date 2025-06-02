1c90686efefae0054ab54ae4abd4cb35b8bba14b
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-06-02 12:18:34 +0200

    chore: extract react eslint rules in own eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-plugin-prettier";
-import vitest from "eslint-plugin-vitest";
 
 export default tseslint.config(
   { ignores: ["**/dist", "**/storybook-static"] },
@@ -17,30 +13,9 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
       prettier,
-      vitest: vitest,
-      pluginReact: {
-        ...pluginReact.configs.flat.recommended,
-        settings: {
-          react: {
-            version: "detect",
-          },
-        },
-      },
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "vitest/max-nested-describe": [
-        "error",
-        {
-          max: 3,
-        },
-      ],
-      "react/prop-types": "off", // Désactive les prop-types si tu utilises TypeScript
-      "react/react-in-jsx-scope": "off", // React 17+ n'a plus besoin d'importer React pour JSX
       "prettier/prettier": [
         "error",
         {
