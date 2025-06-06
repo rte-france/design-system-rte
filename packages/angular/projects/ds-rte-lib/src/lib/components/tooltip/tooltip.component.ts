b93e826ee762fe1703a5b63532d9f3f0210388d6
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-06-06 10:14:18 +0200

    refactor: apply linter rules to tooltip

--- packages/angular/projects/ds-rte-lib/src/lib/components/tooltip/tooltip.component.ts
+++ packages/angular/projects/ds-rte-lib/src/lib/components/tooltip/tooltip.component.ts
@@ -1,17 +1,16 @@
-import { Component, input, model} from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Component, input, model } from "@angular/core";
 
 @Component({
-  selector: 'rte-tooltip',
-  standalone: true,
+  selector: "rte-tooltip",
   imports: [CommonModule],
-  templateUrl: './tooltip.component.html',
-  styleUrls: ['./tooltip.component.scss'],
+  standalone: true,
+  templateUrl: "./tooltip.component.html",
+  styleUrl: "./tooltip.component.scss",
 })
-
 export class TooltipComponent {
   label = input.required<string>();
-  position = model<string>('auto');
-  alignment = input<string>('center');
+  position = model<string>("auto");
+  alignment = input<string>("center");
   arrow = input<boolean>(true);
 }
--- packages/core/components/tooltip/tooltip.interface.d.ts
+++ packages/core/components/tooltip/tooltip.interface.d.ts
@@ -1,9 +1,9 @@
-export type Position = 'auto' | 'top' | 'bottom' | 'left' | 'right';
-export type Alignment = 'start' | 'center' | 'end';
+export type Position = "auto" | "top" | "bottom" | "left" | "right";
+export type Alignment = "start" | "center" | "end";
 
 export interface TooltipProps {
-    position?: Position;
-    alignment?: Alignment;
-    label?: string;
-    arrow?: boolean;
+  position?: Position;
+  alignment?: Alignment;
+  label?: string;
+  arrow?: boolean;
 }
