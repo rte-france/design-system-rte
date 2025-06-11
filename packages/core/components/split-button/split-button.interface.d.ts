84089d66440c52e2102dbba6898321f862263b1c
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-06-11 17:07:34 +0200

    feat: add keyboad interaction behavior for splitButton

--- packages/core/components/split-button/split-button.interface.d.ts
+++ packages/core/components/split-button/split-button.interface.d.ts
@@ -1,15 +1,15 @@
-import { Disabled } from './../../../react/src/components/radioButtonGroup/RadioButtonGroup.stories';
-export type SplitButtonAppearance = 'primary' | 'secondary'
-export type SplitButtonSize = 's' | 'm' | 'l'
-export type SplitButtonPosition = 'bottom-start' | 'bottom-end' | 'top-start' | 'top-end'
+export type SplitButtonAppearance = "primary" | "secondary";
+export type SplitButtonSize = "s" | "m" | "l";
+export type SplitButtonPosition = "bottom-start" | "bottom-end" | "top-start" | "top-end";
 
 export interface SplitButtonProps {
-    appearance?: SplitButtonAppearance;
-    size?: SplitButtonSize;
-    label: string;
-    compactSpacing?: boolean;
-    selected?: boolean;
-    position?: SplitButtonPosition;
-    icon?: string;
-    Disabled?: boolean;
+  appearance?: SplitButtonAppearance;
+  ariaLabelRight?: string;
+  size?: SplitButtonSize;
+  label: string;
+  compactSpacing?: boolean;
+  selected?: boolean;
+  position?: SplitButtonPosition;
+  icon?: string;
+  Disabled?: boolean;
 }
