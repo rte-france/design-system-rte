a9b257e214b7519f69078766df794172a47dcd95
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-06-13 16:40:17 +0200

    refactor(primitives-generator): extract instructions in distinct function

--- packages/core/tokens/primitives-generator.ts
+++ packages/core/tokens/primitives-generator.ts
@@ -5,6 +5,11 @@ enum Collection {
   TYPOGRAPHY = "Primitive : Type",
 }
 
+interface CollectionItem {
+  collection: Collection;
+  variables: Record<string, Record<string, { $value: string }>>;
+}
+
 const inputPath = "./tokens/sourceFiles/primitives.json";
 const outputDir = "./tokens/primitives/";
 const UNIT = "px";
@@ -13,46 +18,56 @@ if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
 
-function extractScssVariables(
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  json: { collection: Collection; variables: Record<string, any> }[],
-) {
-  let scss = "";
-  let filename = "";
-
+function extractScssVariables(json: CollectionItem[]) {
   for (const tokenItem of json) {
+    let scss = "";
+    let filename = "";
     switch (tokenItem.collection) {
       case Collection.COLORS:
         filename = "_colors.scss";
-        for (const color in tokenItem.variables) {
-          for (const colorValue in tokenItem.variables[color]) {
-            const colorVariable = tokenItem.variables[color][colorValue];
-            scss += `$${color}-${colorValue}: ${colorVariable.$value};\n`.toLowerCase();
-          }
-        }
+        scss += extractColors(tokenItem);
+        generateScssFile(scss, filename);
         break;
 
       case Collection.TYPOGRAPHY:
         filename = "_typography.scss";
-        for (const category in tokenItem.variables) {
-          for (const subCategory in tokenItem.variables[category]) {
-            const typographyVariable = tokenItem.variables[category][subCategory];
-
-            if (category === "font-weight") {
-              scss += `$${category}-${subCategory}: ${typographyVariable.$value};\n`.toLowerCase();
-            } else if (category === "font-family") {
-              scss += `$${category}-${subCategory}: "${typographyVariable.$value}";\n`;
-            } else {
-              scss += `$${category}-${subCategory}: ${typographyVariable.$value}${UNIT};\n`.toLowerCase();
-            }
-          }
-        }
+        scss += extractTypography(tokenItem);
+        generateScssFile(scss, filename);
         break;
     }
-    fs.writeFileSync(outputDir + filename, scss);
-    scss = "";
-    filename = "";
   }
+}
+
+function extractColors(colorCollection: CollectionItem): string {
+  let scss = "";
+  for (const color in colorCollection.variables) {
+    for (const colorValue in colorCollection.variables[color]) {
+      const colorVariable = colorCollection.variables[color][colorValue];
+      scss += `$${color}-${colorValue}: ${colorVariable.$value};\n`.toLowerCase();
+    }
+  }
+  return scss;
+}
+
+function extractTypography(typographyCollection: CollectionItem): string {
+  let scss = "";
+  for (const category in typographyCollection.variables) {
+    for (const subCategory in typographyCollection.variables[category]) {
+      const typographyVariable = typographyCollection.variables[category][subCategory];
+      if (category === "font-weight") {
+        scss += `$${category}-${subCategory}: ${typographyVariable.$value};\n`.toLowerCase();
+      } else if (category === "font-family") {
+        scss += `$${category}-${subCategory}: "${typographyVariable.$value}";\n`;
+      } else {
+        scss += `$${category}-${subCategory}: ${typographyVariable.$value}${UNIT};\n`.toLowerCase();
+      }
+    }
+  }
+  return scss;
+}
+
+function generateScssFile(scssValue: string, filename: string) {
+  fs.writeFileSync(outputDir + filename, scssValue);
 }
 
 const raw = JSON.parse(fs.readFileSync(inputPath, "utf8"));
