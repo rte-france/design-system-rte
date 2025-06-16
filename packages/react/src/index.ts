44ffe85125c9e2b371a39e04fe98735f083ffa1a
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-06-16 16:04:34 +0200

    fix: correct aria-labelledby attribute spelling in icon button component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,8 +3,22 @@ import Button from "./components/button/Button";
 import Checkbox from "./components/checkbox/Checkbox.tsx";
 import CheckboxGroup from "./components/checkboxGroup/CheckboxGroup.tsx";
 import Grid from "./components/grid/Grid.tsx";
+import Icon from "./components/icon/Icon.tsx";
+import IconButton from "./components/iconButton/IconButton.tsx";
+import IconButtonToggle from "./components/iconButtonToggle/IconButtonToggle.tsx";
 import Link from "./components/link/Link.tsx";
 import RadioButton from "./components/radioButton/RadioButton.tsx";
 import RadioButtonGroup from "./components/radioButtonGroup/RadioButtonGroup.tsx";
 
-export { Button, Grid, Checkbox, CheckboxGroup, Link, RadioButton, RadioButtonGroup };
+export {
+  Button,
+  Grid,
+  Checkbox,
+  CheckboxGroup,
+  Link,
+  RadioButton,
+  RadioButtonGroup,
+  Icon,
+  IconButton,
+  IconButtonToggle,
+};
