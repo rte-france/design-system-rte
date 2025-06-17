7a44ef7633894d1699ca3d37427438fb7a1bb456
Tommahs <thomascandille0508@gmail.com>
2025-06-17 14:03:32 +0200

    feat: add Usage component and integrate it into Button documentation; enhance StorybookTabs with new tab content and styling

--- packages/react/.storybook/template/StorybookTabs.tsx
+++ packages/react/.storybook/template/StorybookTabs.tsx
@@ -11,26 +11,25 @@ const StorybookTabs = ({ children }: StorybookTabsProps) => {
 
   return (
     <div className="storybook_tabs">
-      <div className="storybook_tabs_header" style={{ display: "flex", borderBottom: "1px solid #ccc" }}>
-        {TabsTitle.map((title, index) => (
-          <h3
-            key={index}
-            className={`storybook_tab_title${activeTab === index ? " active" : ""}`}
-            style={{
-              margin: 0,
-              padding: "8px 16px",
-              cursor: "pointer",
-              borderBottom: activeTab === index ? "2px solid #007bff" : "2px solid transparent",
-              color: activeTab === index ? "#007bff" : "#333",
-              background: "none",
-              fontWeight: activeTab === index ? "bold" : "normal",
-            }}
-            onClick={() => setActiveTab(index)}
-          >
-            {title}
-          </h3>
-        ))}
-      </div>
+    <div className="storybook_tabs_header" style={{ display: "flex",alignItems: "center", gap: "1.5rem" }}>
+      {TabsTitle.map((title, index) => (
+        <h3
+        key={index}
+        className={`storybook_tab_title${activeTab === index ? " active" : ""}`}
+        style={{
+          margin: 0,
+          paddingBlock: "1rem",
+          cursor: "pointer",
+          borderBottom: activeTab === index ? "2px solid #001D6C" : "2px solid transparent",
+          color: activeTab === index ? "#001D6C" : "#21272A",
+          background: "none"
+        }}
+        onClick={() => setActiveTab(index)}
+        >
+        {title}
+        </h3>
+      ))}
+    </div>
       <div className="storybook_tab_content" style={{ padding: "16px" }}>
         {React.Children.toArray(children)[activeTab]}
       </div>
