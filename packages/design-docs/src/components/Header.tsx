d867888664f2b7cd6a863a3fc36d83a3b81414d3
Tommahs <thomascandille0508@gmail.com>
2025-06-17 11:12:41 +0200

    feat: main storybook page created

---
+++ packages/design-docs/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// @ts-expect-error storybook don't import img if error is not ignored
+import header from "../img/header.png";
+import "../styles/Header.css";
+
+const Header = () => (
+  <header className="header">
+    <div className="header_left_container">
+      <h1 className="title"> Design System RTE </h1>
+      <p className="text">
+        Un socle solide pour concevoir des expériences cohérentes, accessibles et durables.
+        <br />
+        Ce design system rassemble tous les fondements visuels, techniques et éditoriaux pour concevoir plus vite,
+        mieux, et ensemble.{" "}
+      </p>
+      <button className="button"> Kit de démarrage </button>
+    </div>
+
+    <div className="header_right_container">
+      <img className="img" src={header} alt="Logo" />
+    </div>
+  </header>
+);
+
+export default Header;
