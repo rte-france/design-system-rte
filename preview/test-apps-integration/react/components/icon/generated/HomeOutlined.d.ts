cac22e10bec16f273ced5be171b6928b97153906
DiazAilan <DiazAilan@users.noreply.github.com>
2025-06-23 09:26:54 +0000

    deploy: a1f69ba6dcaa6db38a87f7f7b60025ce426042fa

---
+++ preview/test-apps-integration/react/components/icon/generated/HomeOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '@design-system-rte/react/src/components/icon/Icon';
+declare const SvgHomeOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgHomeOutlined;
