44f9cafde86623620ee2df80f69175298a46b805
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-06-23 10:39:21 +0200

    chore: test iterative push for storybook preview

--- packages/design-docs/src/components/MainContent.tsx
+++ packages/design-docs/src/components/MainContent.tsx
@@ -50,6 +50,7 @@ const ExplanationCard = ({ img, title, content, link }: ExplanationCardProps) =>
 const MainContent = () => (
   <main className="main-content">
     <h2 className="main_title">Storybook Preview Instance</h2>
+    <h3 className="main_title">2nd Iteration</h3>
     <h2 className="main_title">Concevoir et construire ensemble</h2>
     <div className="main_user_card_container">
       <UserCard
