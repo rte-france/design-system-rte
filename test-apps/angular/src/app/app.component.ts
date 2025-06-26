9e26c7e08566e1eb1ede271877ef59766af1f0b7
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-06-26 17:34:40 +0200

    feat: integrate icon-component test to angular-test-app

--- test-apps/angular/src/app/app.component.ts
+++ test-apps/angular/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { ButtonComponent } from "@design-system-rte/angular";
+import { ButtonComponent, IconComponent } from "@design-system-rte/angular";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet, ButtonComponent, IconComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
