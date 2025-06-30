41df4abf88c6f1a4ab209296ceb890871197d53c
ThomasCandille <ThomasCandille@users.noreply.github.com>
2025-06-30 14:29:01 +0000

    deploy: 756c00867cc69eda5326b455b28090b005ed0427

---
+++ preview/storybook-component-page-angular/react/components/icon/generated/DeleteFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '@design-system-rte/react/src/components/icon/Icon';
+declare const SvgDeleteFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgDeleteFilled;
