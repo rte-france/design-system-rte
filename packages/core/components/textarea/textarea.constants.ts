18b377f68b60d30ee70c53f90bfec9890e804ac1
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-07-01 15:42:07 +0200

    chore: extract constants in core

---
+++ packages/core/components/textarea/textarea.constants.ts
@@ -0,0 +1,7 @@
+export const TEXTAREA_ICON_SIZE = 16;
+
+export const TEXTAREA_REQUIRED_LABEL: Record<string, string> = {
+  required: "(obligatoire)",
+  icon: "*",
+  optional: "(facultatif)",
+};
