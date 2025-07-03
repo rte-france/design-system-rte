5a5bdddd3f3d8f604d28c6fad3a2efe84996e46a
DiazAilan <DiazAilan@users.noreply.github.com>
2025-07-03 15:47:44 +0000

    deploy: 2563cf76d1313a63cf8c2c8e98f42c2e4d5afefe

---
+++ preview/change-detection-angular/react/components/icon/generated/LightbulbCircleFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '@design-system-rte/react/src/components/icon/Icon';
+declare const SvgLightbulbCircleFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgLightbulbCircleFilled;
