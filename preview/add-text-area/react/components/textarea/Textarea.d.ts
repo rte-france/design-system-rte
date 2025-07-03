fffd6d29871d57d4500bae7e08b08052a26567f5
jeremyrts <jeremyrts@users.noreply.github.com>
2025-07-03 14:52:29 +0000

    deploy: 5cc13fa91126db850780f84bf4bc269c40f601c0

--- preview/add-text-area/react/components/textarea/Textarea.d.ts
+++ preview/add-text-area/react/components/textarea/Textarea.d.ts
@@ -1,6 +1,6 @@
 import { TextareaProps as CoreTextareaProps } from '../../../../core/components/textarea/textarea.interface';
 import { ChangeEvent, TextareaHTMLAttributes } from '../../../../../node_modules/react';
-interface TextareaProps extends CoreTextareaProps, Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, "onChange" | "value" | "defaultValue" | "onChange" | "placeholder"> {
+interface TextareaProps extends CoreTextareaProps, Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, "onChange" | "value" | "defaultValue" | "placeholder"> {
     onChange?: (event: ChangeEvent<HTMLTextAreaElement>) => void;
 }
 declare const Textarea: import('../../../../../node_modules/react').ForwardRefExoticComponent<TextareaProps & import('../../../../../node_modules/react').RefAttributes<HTMLTextAreaElement>>;
