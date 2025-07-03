70a1a1398a71891d52622fe10749f25d9d34be2e
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-07-03 16:49:05 +0200

    fix:(textarea) set better props handling + handle resizing logic on angular comp

--- packages/core/components/textarea/textarea.constants.ts
+++ packages/core/components/textarea/textarea.constants.ts
@@ -1,6 +1,8 @@
+import { TextareaRequiredAppearance } from "./textarea.interface";
+
 export const TEXTAREA_ICON_SIZE = 16;
 
-export const TEXTAREA_REQUIRED_LABEL: Record<string, string> = {
+export const TEXTAREA_REQUIRED_LABEL: Record<TextareaRequiredAppearance, string> = {
   required: "(obligatoire)",
   icon: "*",
   optional: "(facultatif)",
