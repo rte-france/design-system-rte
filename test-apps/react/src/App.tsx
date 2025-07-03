ff77c6a4963405c38c45edefbdb469feeb25ca52
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-07-03 15:01:41 +0200

    fix: repair react public API to include SplitButton & Tooltip

--- test-apps/react/src/App.tsx
+++ test-apps/react/src/App.tsx
@@ -1,11 +1,32 @@
 import "./App.css";
-import { Button, Icon } from "@design-system-rte/react";
+import { Button, Icon, SplitButton, Tooltip } from "@design-system-rte/react";
 
 function App() {
   return (
     <div data-theme="bleu_iceberg" data-mode="light">
-      <Icon name="check" />
-      <Button label="Click me" />
+      <div>
+        <h3>Icon</h3>
+        <Icon name="check" />
+      </div>
+      <hr />
+      <div>
+        <h3>Button</h3>
+        <Button label="Click me" />
+      </div>
+      <hr />
+      <div>
+        <h3>SplitButton</h3>
+        <SplitButton appearance="primary" ariaLabelRight="Open menu" label="Button Label" position="bottom-start">
+          <Button label="Click me" />
+        </SplitButton>
+      </div>
+      <hr />
+      <div>
+        <h3>Tooltip</h3>
+        <Tooltip label="Tooltip">
+          <Button label="Click me" />
+        </Tooltip>
+      </div>
     </div>
   );
 }
