4abfe478d7fe8715d2741e08d3506d0daecb54f3
Thomas Candille <114737865+ThomasCandille@users.noreply.github.com>
2025-07-04 11:33:54 +0200

    feat: add babel for angular storybook
    fix: streamline Babel presets configuration and improve formatting in test component
    
    feat: added progress on branch - NOT WORKING - prolem with dependencies

---
+++ packages/angular/projects/ds-rte-lib/src/lib/components/button/test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SimpleComponent = ({ title, text }) => {
+  return (
+    <div className="simple-container">
+      <h2 className="simple-title">{title}</h2>
+      <p className="simple-text">{text}</p>
+    </div>
+  );
+};
+
+export default SimpleComponent;
