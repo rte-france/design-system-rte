aa98e8bf59cfd0b4c60a922d52a2535732a32a62
Tommahs <thomascandille0508@gmail.com>
2025-07-04 11:33:55 +0200

    feat: add StorybookTabs, Usage, and UtilisationExemple components with styles
    fix: improve formatting and add descriptive text in UtilisationExemple component

---
+++ packages/angular/.storybook/template/StorybookTabs.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface StorybookTabsProps {
+  children: React.ReactNode;
+}
+
+const StorybookTabs = ({ children }: StorybookTabsProps) => {
+  const [activeTab, setActiveTab] = React.useState(0);
+
+  const TabsTitle = ["Overview", "Guideline", "Code", "Accessibilité"];
+
+  return (
+    <div className="storybook_tabs">
+      <div className="storybook_tabs_header" style={{ display: "flex", alignItems: "center", gap: "1.5rem" }}>
+        {TabsTitle.map((title, index) => (
+          <h3
+            key={index}
+            className={`storybook_tab_title${activeTab === index ? " active" : ""}`}
+            style={{
+              margin: 0,
+              paddingBlock: "1rem",
+              cursor: "pointer",
+              borderBottom: activeTab === index ? "2px solid #001D6C" : "2px solid transparent",
+              color: activeTab === index ? "#001D6C" : "#21272A",
+              background: "none",
+            }}
+            onClick={() => setActiveTab(index)}
+          >
+            {title}
+          </h3>
+        ))}
+      </div>
+      <div className="storybook_tab_content">{React.Children.toArray(children)[activeTab]}</div>
+    </div>
+  );
+};
+
+export default StorybookTabs;
---
+++ packages/angular/.storybook/template/UtilisationExemple/UtilisationExemple.tsx
@@ -0,0 +1,41 @@
+interface UtilisationExempleProps {
+  toDo: boolean;
+  imgPath: string;
+}
+
+const UtilisationExemple = ({ toDo, imgPath }: UtilisationExempleProps) => {
+  return (
+    <div>
+      <div className="storybook_utilisation_exemple">
+        {toDo ? (
+          <div className="storybook_utilisation_exemple_content_todo_true">
+            <img
+              src="/assets/icons/check.svg"
+              alt="icone_check"
+              className="rte-icon"
+              style={{ width: "24px", height: "24px" }}
+            />
+            <p className="storybook_utilisation_exemple"> À faire</p>
+          </div>
+        ) : (
+          <div className="storybook_utilisation_exemple_content_todo_false">
+            <img
+              src="/assets/icons/warning_filled.svg"
+              alt="icone_warning"
+              className="rte-icon"
+              style={{ width: "24px", height: "24px" }}
+            />
+            <p className="storybook_utilisation_exemple">À ne pas faire</p>
+          </div>
+        )}
+        <img
+          src={imgPath}
+          alt={toDo ? "Exemple à faire" : "Exemple à ne pas faire"}
+          className="storybook_utilisation_exemple_image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UtilisationExemple;
