c3cef2ee1b8e2ac5199f55aa463499624a3db6ff
ThomasCandille <ThomasCandille@users.noreply.github.com>
2025-07-04 09:24:58 +0000

    deploy: bde25d8525af8b67dce6b4d14298a07fbb3f6f82

---
+++ preview/text-input-react/react/components/textInput/TextInput.d.ts
@@ -0,0 +1,3 @@
+import { TextInputProps } from '../../../../core/components/text-input/text-input.interface';
+declare const TextInput: ({ showLabel, label, labelPosition, required_optional, showCounter, value, showLeftIcon, showRightIcon, rightIconAppearance, requiredAppearance, showAssistiveText, assistiveAppearance, showAssistiveIcon, assistiveLabel, disabled, error, readOnly, onChange, onRightIconClick, }: TextInputProps) => import("react/jsx-runtime").JSX.Element;
+export default TextInput;
