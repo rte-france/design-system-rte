23b46f5b1b87ee5c2cf2a855ef125a80d55c1b03
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-07-04 15:20:14 +0200

    fix: naming typo

---
+++ packages/react/.storybook/template/UtilisationExemple/UtilisationExemple.tsx
@@ -0,0 +1,35 @@
+import Icon from "../../../src/components/icon/Icon";
+
+import style from "./UtilisationExemple.module.scss";
+
+interface UtilisationExempleProps {
+  toDo: boolean;
+  imgPath: string;
+}
+
+const UtilisationExemple = ({ toDo, imgPath }: UtilisationExempleProps) => {
+  return (
+    <div>
+      <div className={style.storybook_utilisation_exemple}>
+        {toDo ? (
+          <div className={style.storybook_utilisation_exemple_content_todo_true}>
+            <Icon className={style.storybook_utilisation_exemple_icon} name="check" />
+            <p className={style.storybook_utilisation_exemple}> À faire</p>
+          </div>
+        ) : (
+          <div className={style.storybook_utilisation_exemple_content_todo_false}>
+            <Icon className={style.storybook_utilisation_exemple_icon} name="warning" />
+            <p className={style.storybook_utilisation_exemple}>À ne pas faire</p>
+          </div>
+        )}
+        <img
+          src={imgPath}
+          alt={toDo ? "Exemple à faire" : "Exemple à ne pas faire"}
+          className={style.storybook_utilisation_exemple_image}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UtilisationExemple;
