360165597935509a91f20d064cbc120f57295e9b
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-07-04 15:20:14 +0200

    fix: naming typo

---
+++ packages/react/.storybook/template/Usage/Usage.tsx
@@ -0,0 +1,23 @@
+import style from "./Usage.module.scss";
+
+interface UsageProps {
+  children: React.ReactNode[];
+  title: string;
+}
+
+const Usage = ({ children, title }: UsageProps) => {
+  return (
+    <div className={style.storybook_usage}>
+      <h2 className={style.storybook_usage_title}>Quand utiliser un {title}</h2>
+      <div className={style.storybook_usage_content}>
+        {children.map((child, index) => (
+          <div key={index} className={style.storybook_usage_item}>
+            {child}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Usage;
