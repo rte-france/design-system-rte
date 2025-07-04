c66a3edf339e0ef96a3074540c37b31e86bcc5d7
Tommahs <thomascandille0508@gmail.com>
2025-07-04 16:41:10 +0200

    feat: add TextInput types and prepared file for component

---
+++ packages/core/components/text-input/text-input.interface.d.ts
@@ -0,0 +1,26 @@
+export type LabelPosition = "top" | "side";
+export type RightIconAppearance = "clear" | "visibilityOn" | "visibilityOff";
+export type RequiredAppearance = "required" | "requiredIconOnly";
+export type AssistiveAppearance = "description" | "error" | "succes" | "link";
+
+export interface TextInputProps {
+    showLabel?: boolean;
+    label?: string;
+    labelPosition?: LabelPosition;
+    required?: boolean;
+    optional?: boolean;
+    showCounter?: boolean;
+    value?: string;
+    showLeftIcon?: boolean;
+    showRightIcon?: boolean;
+    RightIconAppearance?: RightIconAppearance;
+    RequiredAppearance?: RequiredAppearance;
+    showAssistiveText?: boolean;
+    assistiveText?: string;
+    showAssistiveIcon?: boolean;
+    assistiveLabel?: string;
+    disabled?: boolean;
+    readOnly?: boolean;
+    onChange?: (value: string) => void;
+    onRightIconClick?: () => void;
+}
