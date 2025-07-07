ea992d8d62f564a8a2bf5cb575d91294b311d1bd
DiazAilan <DiazAilan@users.noreply.github.com>
2025-07-07 09:04:57 +0000

    deploy: e07ebe6611f702ab969212253c245b16bf2f9859

--- react/index.d.ts
+++ react/index.d.ts
@@ -8,4 +8,6 @@ import { default as IconButtonToggle } from './components/iconButtonToggle/IconB
 import { default as Link } from './components/link/Link.tsx';
 import { default as RadioButton } from './components/radioButton/RadioButton.tsx';
 import { default as RadioButtonGroup } from './components/radioButtonGroup/RadioButtonGroup.tsx';
-export { Button, Grid, Checkbox, CheckboxGroup, Link, RadioButton, RadioButtonGroup, Icon, IconButton, IconButtonToggle, };
+import { default as SplitButton } from './components/splitButton/SplitButton.tsx';
+import { default as Tooltip } from './components/tooltip/Tooltip.tsx';
+export { Button, Grid, Checkbox, CheckboxGroup, Link, RadioButton, RadioButtonGroup, Icon, IconButton, IconButtonToggle, SplitButton, Tooltip, };
