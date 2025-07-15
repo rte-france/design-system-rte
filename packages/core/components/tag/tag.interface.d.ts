108d624f4e5413a14a714bec64a369dac4d3d054
Tommahs <thomascandille0508@gmail.com>
2025-07-15 14:55:38 +0200

    refactor: improve formatting and consistency in Tag component and interface

--- packages/core/components/tag/tag.interface.d.ts
+++ packages/core/components/tag/tag.interface.d.ts
@@ -1,13 +1,34 @@
-export type TagType = 'status' | 'decorative'
-export type Status = 'success' | 'information' | 'warning' | 'alert'
-export type Color = 'brand' | 'neutral' | 'azur' | 'jade' | 'lavande' | 'ciel' | 'nuage' | 'givre' | 'brume' | 'glacier' | 'turquoise' | 'anis' | 'menthe' | 'citronnelle' | 'sable' | 'abricot' | 'coral' | 'rose' | 'petale' | 'quartz' | 'cendre'
+export type TagType = "status" | "decorative";
+export type Status = "success" | "information" | "warning" | "alert";
+export type Color =
+  | "brand"
+  | "neutral"
+  | "azur"
+  | "jade"
+  | "lavande"
+  | "ciel"
+  | "nuage"
+  | "givre"
+  | "brume"
+  | "glacier"
+  | "turquoise"
+  | "anis"
+  | "menthe"
+  | "citronnelle"
+  | "sable"
+  | "abricot"
+  | "coral"
+  | "rose"
+  | "petale"
+  | "quartz"
+  | "cendre";
 
 export interface TagProps {
-    tagType?: TagType
-    status?: Status
-    color?: Color
-    CompactSpacing?: boolean
-    label?: string
-    showLeftIcon?: boolean
-    iconName?: string
+  tagType?: TagType;
+  status?: Status;
+  color?: Color;
+  CompactSpacing?: boolean;
+  label?: string;
+  showLeftIcon?: boolean;
+  iconName?: string;
 }
