9a66a468bd77c820e4f2a22b7d8bad3c6f5b21ac
ThomasCandille <ThomasCandille@users.noreply.github.com>
2025-07-15 13:36:45 +0000

    deploy: 5c610dc9c891289f48e67115d940244cfab9e7e0

---
+++ preview/tag-component/angular/components/tag/tag.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import { TagProps } from "@design-system-rte/core/components/tag/tag.interface";
+
+import { isValidIconName } from "../icon/icon-map";
+import { IconComponent } from "../icon/icon.component";
+import { RegularIconIdKey, TogglableIconIdKey } from "../icon/icon.service";
+
+@Component({
+  selector: "rte-tag",
+  imports: [CommonModule, IconComponent],
+  standalone: true,
+  templateUrl: "./tag.component.html",
+  styleUrl: "./tag.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TagComponent {
+  readonly tagType = input<TagProps["tagType"]>("status");
+  readonly status = input<TagProps["status"]>("success");
+  readonly color = input<TagProps["color"]>("brand");
+  readonly compactSpacing = input<boolean>(false);
+  readonly label = input<string>("");
+  readonly showLeftIcon = input<boolean>(false);
+  readonly iconName = input<RegularIconIdKey | TogglableIconIdKey>();
+
+  get computedIconName(): RegularIconIdKey | TogglableIconIdKey {
+    if (this.tagType() === "status") {
+      const statusIconMap: Record<string, RegularIconIdKey | TogglableIconIdKey> = {
+        success: "check",
+        information: "info",
+        warning: "warning",
+        alert: "dangerous",
+      };
+      const statusValue = this.status();
+      if (statusValue && statusIconMap[statusValue]) {
+        return statusIconMap[statusValue];
+      }
+    }
+    return this.iconName() ?? "check";
+  }
+
+  isValidIconName(iconName: string): boolean {
+    return iconName ? isValidIconName(iconName) : false;
+  }
+}
