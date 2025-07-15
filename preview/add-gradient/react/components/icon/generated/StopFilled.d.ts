f3b8b690f15aeca0a42d079cb5a953f26a65fb17
jeremyrts <jeremyrts@users.noreply.github.com>
2025-07-15 14:11:18 +0000

    deploy: 07a006e2bf656a23ae26b413d3cbc5e349e57e95

---
+++ preview/add-gradient/react/components/icon/generated/StopFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '@design-system-rte/react/src/components/icon/Icon';
+declare const SvgStopFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgStopFilled;
