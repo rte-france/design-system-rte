af2901a97d9d18ba7a3ded55ab59b070576eb1a8
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-07-16 15:41:08 +0200

    feat: add script to generate icon maps

---
+++ packages/angular/projects/ds-rte-lib/generateIconMap.js
@@ -0,0 +1,76 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const assetsDir = path.resolve(__dirname, "src/lib/assets/icons");
+const iconDir = path.resolve(__dirname, "src/lib/components/icon");
+const ALL_ICONS = getIconsName();
+const { togglableIcons, regularIcons } = extractRegularAndTogglableIcons();
+
+generateIconMaps();
+
+function getIconsName() {
+  const files = fs.readdirSync(assetsDir);
+  const componentNames = files.filter((file) => file.endsWith(".svg")).map((file) => path.basename(file, ".svg"));
+  return componentNames;
+}
+
+function extractRegularAndTogglableIcons() {
+  const togglableIcons = [];
+  const regularIcons = [];
+  ALL_ICONS.forEach((iconName) => {
+    if (iconName.includes("outlined") || iconName.includes("filled")) {
+      const baseName = iconName.replace(/(_outlined|_filled)$/, "");
+      if (!togglableIcons.includes(baseName)) {
+        togglableIcons.push(baseName);
+      }
+    } else {
+      regularIcons.push(iconName);
+    }
+  });
+  return {
+    togglableIcons,
+    regularIcons,
+  };
+}
+
+function generateIconMaps() {
+  let string = `// This file is auto-generated. Do not edit manually.\n\n`;
+
+  string += generateImportsIconsMap();
+  string += generateRegularIconsMap();
+  string += generateTogglableIconsMap();
+  string += generateIsValidIconNameFunction();
+
+  fs.writeFileSync(path.resolve(iconDir, "icon-map.ts"), string);
+}
+
+function generateImportsIconsMap() {
+  return `import { isValidIconName as isValidIconNameShared } from "@design-system-rte/core/components/icon/icon-utils";\n\n`;
+}
+
+function generateRegularIconsMap() {
+  let string = `export const RegularIcons = {\n`;
+  regularIcons.forEach((iconName) => {
+    const snakeCaseName = iconName.replace(/_/g, "-");
+    string += `  "${snakeCaseName}": "${iconName}",\n`;
+  });
+  string += `};\n\n`;
+  return string;
+}
+
+function generateTogglableIconsMap() {
+  let string = `export const TogglableIcons = {\n`;
+  togglableIcons.forEach((iconName) => {
+    const snakeCaseName = iconName.replace(/_/g, "-");
+    string += `  "${snakeCaseName}": ["${iconName}_outlined", "${iconName}_filled"],\n`;
+  });
+  string += `};\n`;
+  return string;
+}
+
+function generateIsValidIconNameFunction() {
+  return `\nexport function isValidIconName(iconName: string): boolean {\n  return isValidIconNameShared(iconName, RegularIcons, TogglableIcons);\n}\n`;
+}
---
+++ packages/react/generateIconMap.js
@@ -0,0 +1,98 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const ALL_ICONS = getIconsName();
+const { togglableIcons, regularIcons } = extractRegularAndTogglableIcons();
+
+generateIndexFile();
+generateIconMaps();
+
+function getIconsName() {
+  const iconsDir = path.resolve(__dirname, "src/components/icon/generated");
+  const files = fs.readdirSync(iconsDir);
+  const componentNames = files.filter((file) => file.endsWith(".tsx")).map((file) => path.basename(file, ".tsx"));
+  return componentNames;
+}
+
+function extractRegularAndTogglableIcons() {
+  const togglableIcons = [];
+  const regularIcons = [];
+  ALL_ICONS.forEach((iconName) => {
+    if (iconName.includes("Outlined") || iconName.includes("Filled")) {
+      const baseName = iconName.replace(/(Outlined|Filled)$/, "");
+      if (!togglableIcons.includes(baseName)) {
+        togglableIcons.push(baseName);
+      }
+    } else {
+      regularIcons.push(iconName);
+    }
+  });
+  return {
+    togglableIcons,
+    regularIcons,
+  };
+}
+
+function generateIndexFile() {
+  let string = `// This file is auto-generated. Do not edit manually.\n\n`;
+  ALL_ICONS.forEach((iconName) => {
+    const iconNameCamelCase = iconName.charAt(0).toUpperCase() + iconName.slice(1);
+    string += `export { default as ${iconNameCamelCase} } from './${iconNameCamelCase}';\n`;
+  });
+  fs.writeFileSync(path.resolve(__dirname, "src/components/icon/generated/index.ts"), string);
+}
+
+function generateIconMaps() {
+  let string = `// This file is auto-generated. Do not edit manually.\n\n`;
+
+  string += generateImportsIconsMap();
+  string += generateRegularIconsMap();
+  string += generateTogglableIconsMap();
+  string += generateIsValidIconNameFunction();
+
+  fs.writeFileSync(path.resolve(__dirname, "src/components/icon/IconMap.ts"), string);
+}
+
+function generateImportsIconsMap() {
+  let string = `import { isValidIconName as isValidIconNameShared } from "@design-system-rte/core/components/icon/icon-utils";\n\n`;
+  string += `import {\n`;
+  ALL_ICONS.forEach((iconName) => {
+    const iconNameCamelCase = iconName.charAt(0).toUpperCase() + iconName.slice(1);
+    string += `  ${iconNameCamelCase},\n`;
+  });
+  string += `} from "./generated";\n\n`;
+  return string;
+}
+
+function generateRegularIconsMap() {
+  let string = `export const RegularIcons = {\n`;
+  regularIcons.forEach((iconName) => {
+    const snakeCaseName = iconName
+      .replace(/([a-z0-9])([A-Z])/g, "$1-$2")
+      .replace(/([A-Z])([A-Z][a-z])/g, "$1-$2")
+      .toLowerCase();
+    string += `  "${snakeCaseName}": ${iconName},\n`;
+  });
+  string += `};\n\n`;
+  return string;
+}
+
+function generateTogglableIconsMap() {
+  let string = `export const TogglableIcons = {\n`;
+  togglableIcons.forEach((iconName) => {
+    const snakeCaseName = iconName
+      .replace(/([a-z0-9])([A-Z])/g, "$1-$2")
+      .replace(/([A-Z])([A-Z][a-z])/g, "$1-$2")
+      .toLowerCase();
+    string += `  "${snakeCaseName}": [${iconName}Outlined, ${iconName}Filled],\n`;
+  });
+  string += `};\n`;
+  return string;
+}
+
+function generateIsValidIconNameFunction() {
+  return `\nexport function isValidIconName(name: string): boolean {\n  return isValidIconNameShared(name, RegularIcons, TogglableIcons);\n}\n`;
+}
