182696a4920e211ebaaa186e490f02d2daaf2e93
Tommahs <thomascandille0508@gmail.com>
2025-07-18 14:06:17 +0200

    feat: add Badge interface

---
+++ packages/core/components/badge/badge.interface.d.ts
@@ -0,0 +1,11 @@
+export type BadgeType = "brand" | "neutral" | "indicator";
+export type Size = "XS" | "S" | "M" | "L";
+export type Appearance = "text" | "icon";
+
+export interface BadgeProps {
+  badgeType?: BadgeType;
+  size?: Size;
+  appearance?: Appearance;
+  count?: string;
+  icon?: string;
+}
