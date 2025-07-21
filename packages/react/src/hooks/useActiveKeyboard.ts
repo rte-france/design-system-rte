2794afc002e36f01e4ea176b058a0ad5503f2928
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-07-21 16:28:13 +0200

    feat: add useActiveKeyboard hook to centralise keyboard interaction

---
+++ packages/react/src/hooks/useActiveKeyboard.ts
@@ -0,0 +1,42 @@
+import { KeyboardEvent, useRef, useState } from "react";
+
+export const SPACEBAR_INPUT = "Space";
+export const ENTER_INPUT = "Enter";
+
+type OptionsActiveKeyboard = {
+  id?: string;
+  interactiveKeyCodes?: string[];
+};
+
+const noop = () => {};
+
+export const useActiveKeyboard = <T extends HTMLElement>(
+  handlerKeyup: (e: KeyboardEvent<T>) => void = noop,
+  options: OptionsActiveKeyboard = {},
+) => {
+  const { id, interactiveKeyCodes } = options;
+  const [isActiveKeyboard, setIsActiveKeyboard] = useState<boolean>(false);
+  const interactiveKeysRef = useRef<string[]>(interactiveKeyCodes ?? [SPACEBAR_INPUT, ENTER_INPUT]);
+
+  const onKeyDown = (e: KeyboardEvent<T>) => {
+    if (interactiveKeysRef.current?.includes(e.code) && (!id || (e.target as T).id === id)) {
+      e.preventDefault();
+      console.log(`Key down: ${e.code}`);
+      setIsActiveKeyboard(true);
+    }
+  };
+
+  const onKeyUp = (e: KeyboardEvent<T>) => {
+    if (interactiveKeysRef.current?.includes(e.code) && (!id || (e.target as T).id === id)) {
+      console.log(`Key up: ${e.code}`);
+      handlerKeyup(e);
+      setIsActiveKeyboard(false);
+    }
+  };
+
+  const onBlur = () => {
+    setIsActiveKeyboard(false);
+  };
+
+  return { onKeyDown, onKeyUp, onBlur, isActiveKeyboard };
+};
