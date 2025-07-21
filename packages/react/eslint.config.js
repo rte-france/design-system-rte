845e6cac77461f9fd8e8a6198e8109c4b7417820
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-07-21 16:29:03 +0200

    feat(chip): add React single chip component

--- packages/react/eslint.config.js
+++ packages/react/eslint.config.js
@@ -5,24 +5,33 @@ import tseslint from "typescript-eslint";
 
 import baseConfig from "../../eslint.config.js";
 
-export default tseslint.config(...baseConfig, {
-  files: ["**/*.{js,ts,jsx,tsx}"],
-  plugins: {
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-    pluginReact: {
-      ...pluginReact.configs.flat.recommended,
-      settings: {
-        react: {
-          version: "detect",
+export default tseslint.config(
+  ...baseConfig,
+  {
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      pluginReact: {
+        ...pluginReact.configs.flat.recommended,
+        settings: {
+          react: {
+            version: "detect",
+          },
         },
       },
     },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+    },
   },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
+  {
+    files: ["**/*.stories.tsx"],
+    rules: {
+      "react-hooks/rules-of-hooks": "off",
+    },
   },
-});
+);
