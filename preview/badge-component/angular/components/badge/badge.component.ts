7983f4c1fbcbc260a1452bcc7489c28ea443463c
DiazAilan <DiazAilan@users.noreply.github.com>
2025-07-25 14:04:48 +0000

    deploy: d25598acd12975c4ebd9680982462a20022d0504

--- preview/badge-component/angular/components/badge/badge.component.ts
+++ preview/badge-component/angular/components/badge/badge.component.ts
@@ -19,12 +19,16 @@ export class BadgeComponent {
   readonly badgeType = input<BadgeType>("brand");
   readonly badgeSize = input<BadgeSize>("m");
   readonly badgeAppearance = input<BadgeAppearance>("text");
-  readonly count = input<number>(1);
+  readonly count = input<number | undefined>();
   readonly icon = input<RegularIconIdKey | TogglableIconIdKey>("settings");
 
   readonly isValidIconName = computed(() => isValidIconName(this.icon()));
   readonly showIcon = computed(() => this.badgeAppearance() === "icon" && this.isValidIconName());
-  readonly showText = computed(() => this.badgeAppearance() === "text" && this.count() > 0);
-  readonly displayCount = computed(() => (this.count() < 1000 ? this.count() : "999+"));
+  readonly showText = computed(
+    () => this.badgeAppearance() === "text" && typeof this.count() === "number" && this.count()! > 0,
+  );
+  readonly displayCount = computed(() =>
+    typeof this.count() === "number" && this.count()! < 1000 ? this.count() : "999+",
+  );
   readonly iconSize = computed(() => badgeIconSize[this.badgeSize()]);
 }
