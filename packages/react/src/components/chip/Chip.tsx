651f07ace3cc59cb83769107ce64573b8f7fadd9
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-07-25 17:13:05 +0200

    refactor: add keyboard test constants file

--- packages/react/src/components/chip/Chip.tsx
+++ packages/react/src/components/chip/Chip.tsx
@@ -1,6 +1,11 @@
 import { CHIP_TYPE_TO_ARIA_ROLE_MAP } from "@design-system-rte/core/components/chip/chip.constants";
 import { ChipProps as CoreChipProps } from "@design-system-rte/core/components/chip/chip.interface";
-import { BACKSPACE_KEY, DELETE_KEY, ENTER_KEY, SPACE_KEY } from "@design-system-rte/core/constants/keyboard.constants";
+import {
+  BACKSPACE_KEY,
+  DELETE_KEY,
+  ENTER_KEY,
+  SPACE_KEY,
+} from "@design-system-rte/core/constants/keyboard/keyboard.constants";
 import React, { forwardRef } from "react";
 
 import { useActiveKeyboard } from "../../hooks/useActiveKeyboard";
