5c455b3c21dea312db8136ee1fda858f5d7c866d
jeremyrts <jeremyrts@users.noreply.github.com>
2025-07-30 13:47:40 +0000

    deploy: 8a47c465008dc17f0fd434e35803489b241b9ab0

---
+++ preview/update-tokens-5/react/components/icon/generated/ViewTimelineFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '@design-system-rte/react/src/components/icon/Icon';
+declare const SvgViewTimelineFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgViewTimelineFilled;
