52d95d3b8e6e8fe86ae809037bc95d236b7e8b57
DiazAilan <DiazAilan@users.noreply.github.com>
2025-07-31 15:29:25 +0000

    deploy: 3f718268e82453d9ec5f9131d785944940d39304

---
+++ preview/latest-changes/angular/68932.986b1fc6.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_design_system_rte_angular_builder=self.webpackChunk_design_system_rte_angular_builder||[]).push([[68932],{"./projects/ds-rte-lib/src/lib/assets/icons/assignment_filled.svg":(module,__unused_webpack_exports,__webpack_require__)=>{module.exports=__webpack_require__.p+"static/media/assignment_filled.126e4840.svg"}}]);
