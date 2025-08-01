df8d2ce5fc6ea8167107fe83f936dab7905248ec
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-08-01 17:18:55 +0200

    feat: (segmented control) wip add react component

--- packages/core/constants/keyboard/keyboard.constants.ts
+++ packages/core/constants/keyboard/keyboard.constants.ts
@@ -4,3 +4,5 @@ export const TAB_KEY = "Tab";
 export const DOWN_KEY = "ArrowDown";
 export const BACKSPACE_KEY = "Backspace";
 export const DELETE_KEY = "Delete";
+export const ARROW_LEFT = "ArrowLeft";
+export const ARROW_RIGHT = "ArrowRight";
