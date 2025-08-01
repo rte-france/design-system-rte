5664f783f46a61fbf36c5c466c5837a43b09f5d3
Tommahs <thomascandille0508@gmail.com>
2025-08-01 14:14:05 +0200

    feat: implement Divider component with orientation and thickness props for react

---
+++ packages/react/src/components/divider/Divider.tsx
@@ -0,0 +1,18 @@
+import { DividerProps } from "@design-system-rte/core/components/divider/divider.interface";
+import { forwardRef } from "react";
+
+import style from "./Divider.module.scss";
+
+
+const Divider = forwardRef<HTMLDivElement, DividerProps>(
+    ({ orientation = "horizontal", thickness = "light", ...props }, ref) => {
+
+        return orientation === "horizontal" ? (
+            <hr ref={ref as React.Ref<HTMLHRElement>} className={style.horizontal} data-thickness={thickness} {...props} />
+        ) : (
+            <div ref={ref as React.Ref<HTMLDivElement>} role="separator" className={style.vertical} data-thickness={thickness} {...props} />
+        );
+    },
+);
+
+export default Divider;
