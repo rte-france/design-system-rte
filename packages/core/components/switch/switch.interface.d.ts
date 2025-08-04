7b4becf0f7c840a78197b314d66e55913b3f3470
Tommahs <thomascandille0508@gmail.com>
2025-08-04 17:32:25 +0200

    feat (Switch): added switch component to React with style and and documentation

---
+++ packages/core/components/switch/switch.interface.d.ts
@@ -0,0 +1,9 @@
+export type SwitchAppearance = "brand" | "neutral";
+
+export interface SwitchProps {
+    appearance?: SwitchAppearance;
+    showLabel?: boolean;
+    showIcon?: boolean;
+    label?: string;
+    showIcon?: boolean;
+}
