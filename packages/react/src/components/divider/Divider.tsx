676d1914d1e6aabcaa81510417d8d60e961456f2
Tommahs <thomascandille0508@gmail.com>
2025-08-12 14:58:55 +0200

    refactor: improve formatting and consistency in Divider component files

--- packages/react/src/components/divider/Divider.tsx
+++ packages/react/src/components/divider/Divider.tsx
@@ -3,16 +3,20 @@ import { forwardRef } from "react";
 
 import style from "./Divider.module.scss";
 
-
 const Divider = forwardRef<HTMLDivElement, DividerProps>(
-    ({ orientation = "horizontal", thickness = "light", ...props }, ref) => {
-
-        return orientation === "horizontal" ? (
-            <hr ref={ref as React.Ref<HTMLHRElement>} className={style.horizontal} data-thickness={thickness} {...props} />
-        ) : (
-            <div ref={ref as React.Ref<HTMLDivElement>} role="separator" className={style.vertical} data-thickness={thickness} {...props} />
-        );
-    },
+  ({ orientation = "horizontal", thickness = "light", ...props }, ref) => {
+    return orientation === "horizontal" ? (
+      <hr ref={ref as React.Ref<HTMLHRElement>} className={style.horizontal} data-thickness={thickness} {...props} />
+    ) : (
+      <div
+        ref={ref as React.Ref<HTMLDivElement>}
+        role="separator"
+        className={style.vertical}
+        data-thickness={thickness}
+        {...props}
+      />
+    );
+  },
 );
 
 export default Divider;
