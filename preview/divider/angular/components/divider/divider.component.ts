8e7b4bbb8b02d1caf613d879f15bd1770c6ebb6f
ThomasCandille <ThomasCandille@users.noreply.github.com>
2025-08-12 14:58:07 +0000

    deploy: 0e7439f7f9e6d30fcc602b8210f7bb1267c2160b

--- preview/divider/angular/components/divider/divider.component.ts
+++ preview/divider/angular/components/divider/divider.component.ts
@@ -13,7 +13,8 @@ import { DividerProps } from "@design-system-rte/core/components/divider/divider
 export class DividerComponent {
   readonly orientation = input<DividerProps["orientation"]>("horizontal");
   readonly thickness = input<DividerProps["thickness"]>("light");
-  readonly color = input<DividerProps["color"]>("default");
+  readonly appearance = input<DividerProps["appearance"]>("default");
+  readonly endPoint = input<DividerProps["endPoint"]>("round");
 
   readonly isHorizontal = computed(() => this.orientation() === "horizontal");
 }
