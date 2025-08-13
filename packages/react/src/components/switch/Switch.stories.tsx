110bc81aaf253f80367d540c69ef6b2ebcc97890
Tommahs <thomascandille0508@gmail.com>
2025-08-13 11:06:28 +0200

    refactor(Switch): improve code formatting and consistency across components

--- packages/react/src/components/switch/Switch.stories.tsx
+++ packages/react/src/components/switch/Switch.stories.tsx
@@ -1,47 +1,48 @@
-import {Meta, StoryObj} from '@storybook/react';
-import Switch from './Switch';
+import { Meta, StoryObj } from "@storybook/react";
+
+import Switch from "./Switch";
 
 const meta = {
-  title: 'Switch',
+  title: "Switch",
   component: Switch,
-decorators: [
+  decorators: [
     (Story) => (
-        <div style={{ height: '50px', width: '200px' }}>
-            <Story />
-        </div>
+      <div style={{ height: "50px", width: "200px" }}>
+        <Story />
+      </div>
     ),
-],
-  tags: ['autodocs'],
+  ],
+  tags: ["autodocs"],
   argTypes: {
     label: {
-      control: 'text',
-      description: 'Label for the switch',
-      defaultValue: 'Label',
+      control: "text",
+      description: "Label for the switch",
+      defaultValue: "Label",
     },
     appearance: {
-      control: 'select',
-      options: ['brand', 'neutral'],
-      description: 'Appearance of the switch',
-      defaultValue: 'brand',
+      control: "select",
+      options: ["brand", "neutral"],
+      description: "Appearance of the switch",
+      defaultValue: "brand",
     },
     showLabel: {
-      control: 'boolean',
-      description: 'Whether to show the label',
+      control: "boolean",
+      description: "Whether to show the label",
       defaultValue: true,
     },
     showIcon: {
-      control: 'boolean',
-      description: 'Whether to show the icon',
+      control: "boolean",
+      description: "Whether to show the icon",
       defaultValue: true,
     },
     disabled: {
-      control: 'boolean',
-      description: 'Whether the switch is disabled',
+      control: "boolean",
+      description: "Whether the switch is disabled",
       defaultValue: false,
     },
     readOnly: {
-      control: 'boolean',
-      description: 'Whether the switch is read-only',
+      control: "boolean",
+      description: "Whether the switch is read-only",
       defaultValue: false,
     },
   },
@@ -53,7 +54,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    label: 'Label',
+    label: "Label",
     showLabel: true,
     disabled: false,
     readOnly: false,
