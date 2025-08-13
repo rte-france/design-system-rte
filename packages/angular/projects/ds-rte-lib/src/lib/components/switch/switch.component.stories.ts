ae66c65e4c41cd46feca9e54faea678c7f736787
Tommahs <thomascandille0508@gmail.com>
2025-08-13 11:06:29 +0200

    feat(Switch): add aria-label for accessibility and update storybook interactions

--- packages/angular/projects/ds-rte-lib/src/lib/components/switch/switch.component.stories.ts
+++ packages/angular/projects/ds-rte-lib/src/lib/components/switch/switch.component.stories.ts
@@ -1,38 +1,51 @@
 import { Meta, StoryObj } from "@storybook/angular";
-
+import { expect, within, userEvent } from "@storybook/test";
 import { SwitchComponent } from "./switch.component";
 
+
 const meta: Meta<SwitchComponent> = {
-  title: "Switch",
-  component: SwitchComponent,
-  tags: ["autodocs"],
-  argTypes: {
-    label: {
-      control: "text",
-      defaultValue: "Switch",
+    title: "Switch",
+    component: SwitchComponent,
+    tags: ["autodocs"],
+    argTypes: {
+        label: {
+            control: "text",
+            defaultValue: "Switch",
+        },
+        appearance: {
+            control: "select",
+            options: ["brand", "neutral"],
+            defaultValue: "brand",
+        },
+        showLabel: {
+            control: "boolean",
+            defaultValue: true,
+        },
+        showIcon: {
+            control: "boolean",
+            defaultValue: true,
+        },
+        disabled: {
+            control: "boolean",
+            defaultValue: false,
+        },
+        readOnly: {
+            control: "boolean",
+            defaultValue: false,
+        },
+        checked: {
+            table: { disable: true }
+        },
+        handleClick: {
+            table: { disable: true }
+        },
+        switchHeight: {
+            table: { disable: true }
+        },
+        switchWidth: {
+            table: { disable: true }
+        }
     },
-    appearance: {
-      control: "select",
-      options: ["brand", "neutral"],
-      defaultValue: "brand",
-    },
-    showLabel: {
-      control: "boolean",
-      defaultValue: true,
-    },
-    showIcon: {
-      control: "boolean",
-      defaultValue: true,
-    },
-    disabled: {
-      control: "boolean",
-      defaultValue: false,
-    },
-    readOnly: {
-      control: "boolean",
-      defaultValue: false,
-    },
-  },
 };
 
 export default meta;
@@ -47,4 +60,43 @@ export const Default: Story = {
     disabled: false,
     readOnly: false,
   },
+play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const switchElement = canvas.getByRole('switch');
+    
+    await userEvent.tab();
+    await expect(switchElement).toHaveFocus();
+    
+    await userEvent.keyboard(' ');
+    await expect(switchElement).toBeChecked();
+    
+    await userEvent.keyboard(' ');
+    await expect(switchElement).not.toBeChecked();
+    
+    await userEvent.keyboard('{Enter}');
+    await expect(switchElement).not.toBeChecked();
+}
 };
+
+export const Disabled: Story = {
+  args: {
+    label: "Switch",
+    appearance: "brand",
+    showLabel: true,
+    showIcon: true,
+    disabled: true,
+    readOnly: false,
+  }
+};
+
+export const ReadOnly: Story = {
+  args: {
+    label: "Switch",
+    appearance: "brand",
+    showLabel: true,
+    showIcon: true,
+    disabled: false,
+    readOnly: true,
+  }
+};
+
