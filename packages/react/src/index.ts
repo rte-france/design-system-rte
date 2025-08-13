c34917db757beb4ee6552526e9cff6405ec01401
Tommahs <thomascandille0508@gmail.com>
2025-08-13 11:06:29 +0200

    feat (switch): added switch component to the test app

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -16,6 +16,7 @@ import SplitButton from "./components/splitButton/SplitButton.tsx";
 import Textarea from "./components/textarea/Textarea.tsx";
 import TextInput from "./components/textInput/TextInput.tsx";
 import Tooltip from "./components/tooltip/Tooltip.tsx";
+import Switch from "./components/switch/Switch.tsx";
 
 export {
   Button,
@@ -35,4 +36,5 @@ export {
   Badge,
   Textarea,
   Divider,
+  Switch,
 };
