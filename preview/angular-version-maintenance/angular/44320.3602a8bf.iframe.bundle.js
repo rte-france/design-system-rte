864ed861309195547b7a67fd043297187cb7b597
DiazAilan <DiazAilan@users.noreply.github.com>
2025-08-14 08:20:38 +0000

    deploy: 5c064694f963c055893df8c03217cc58fb0474df

---
+++ preview/angular-version-maintenance/angular/44320.3602a8bf.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_design_system_rte_angular_builder=self.webpackChunk_design_system_rte_angular_builder||[]).push([[44320],{"./projects/ds-rte-lib/src/lib/assets/icons/bookmarks_filled.svg":(module,__unused_webpack_exports,__webpack_require__)=>{module.exports=__webpack_require__.p+"static/media/bookmarks_filled.f370025c.svg"}}]);
