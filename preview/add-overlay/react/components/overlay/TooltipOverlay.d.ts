7b4b144f06e8b6786f055d85817a4ab4a3829e2f
jeremyrts <jeremyrts@users.noreply.github.com>
2025-08-19 15:23:46 +0000

    deploy: 46e284b0c35e24fa1acc4532e330f2c79509df63

--- preview/add-overlay/react/components/overlay/TooltipOverlay.d.ts
+++
@@ -1,7 +0,0 @@
-type TooltipProps = {
-    label: string;
-    position?: "top" | "bottom" | "left" | "right" | "auto";
-    children: React.ReactNode;
-};
-export declare const TooltipOverlay: import('../../../../../node_modules/react').ForwardRefExoticComponent<TooltipProps & import('../../../../../node_modules/react').RefAttributes<HTMLDivElement>>;
-export {};
