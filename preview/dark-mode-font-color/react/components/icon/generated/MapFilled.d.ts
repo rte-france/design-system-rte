efadb42809317a6ce34f9669c51f2da2086d7823
DiazAilan <DiazAilan@users.noreply.github.com>
2025-08-28 15:45:03 +0000

    deploy: dda6fe3dc5f28a490cc5b0380c895325b71dce5d

---
+++ preview/dark-mode-font-color/react/components/icon/generated/MapFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '@design-system-rte/react/src/components/icon/Icon';
+declare const SvgMapFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgMapFilled;
