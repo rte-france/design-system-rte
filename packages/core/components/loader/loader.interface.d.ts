b9ff0aeded2cccbf7847077ef4ca8776285372ae
Tommahs <thomascandille0508@gmail.com>
2025-09-01 16:25:43 +0200

    feat(loader): add loader component for react and angular

--- packages/core/components/loader/loader.interface.d.ts
+++ packages/core/components/loader/loader.interface.d.ts
@@ -1,11 +1,11 @@
-export type LoaderAppearance = 'brand' | 'reverse'
-export type LoaderSize = 'small' | 'medium' | 'large'
-export type LoaderLabelPosition = 'right' | 'under'
+export type LoaderAppearance = "brand" | "reverse";
+export type LoaderSize = "small" | "medium" | "large";
+export type LoaderLabelPosition = "right" | "under";
 
 export interface LoaderProps {
-    appearance?: LoaderAppearance
-    size?: LoaderSize
-    showLabel?: boolean
-    label?: string
-    labelPosition?: LoaderLabelPosition
+  appearance?: LoaderAppearance;
+  size?: LoaderSize;
+  showLabel?: boolean;
+  label?: string;
+  labelPosition?: LoaderLabelPosition;
 }
--- packages/react/src/components/loader/Loader.stories.tsx
+++ packages/react/src/components/loader/Loader.stories.tsx
@@ -1,61 +1,61 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import Loader from './Loader';
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Loader from "./Loader";
 
 const meta = {
-    title: 'Loader',
-    component: Loader,
-    parameters: {
-        layout: 'centered',
+  title: "Loader",
+  component: Loader,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    appearance: {
+      control: "select",
+      options: ["brand", "reverse"],
+      description: "appearance of the Loader",
+      defaultValue: "brand",
     },
-    tags: ['autodocs'],
-    argTypes: {
-        appearance: {
-            control: 'select',
-            options: ['brand', 'reverse'],
-            description: "appearance of the Loader",
-            defaultValue: "brand"
-        },
-        size: {
-            control: 'select',
-            options: ['small', 'medium', 'large'],
-            description: "size of the Loader",
-            defaultValue: "medium"
-        },
-        showLabel: {
-            control: 'boolean',
-            description: "show the label of the Loader",
-            defaultValue: "true"
-        },
-        labelPosition: {
-            control: 'select',
-            options: ['right', 'under'],
-            description: "position of the label",
-            defaultValue: "right"
-        },
-        label:{
-            control:"text",
-            description: "label of the Loader",
-            defaultValue: "Chargement"
-        }
+    size: {
+      control: "select",
+      options: ["small", "medium", "large"],
+      description: "size of the Loader",
+      defaultValue: "medium",
     },
-} satisfies Meta<typeof Loader>
+    showLabel: {
+      control: "boolean",
+      description: "show the label of the Loader",
+      defaultValue: "true",
+    },
+    labelPosition: {
+      control: "select",
+      options: ["right", "under"],
+      description: "position of the label",
+      defaultValue: "right",
+    },
+    label: {
+      control: "text",
+      description: "label of the Loader",
+      defaultValue: "Chargement",
+    },
+  },
+} satisfies Meta<typeof Loader>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        appearance: 'brand',
-        size: 'medium',
-        showLabel: true,
-        labelPosition: 'right',
-        label: 'Loading...',
-    },
+  args: {
+    appearance: "brand",
+    size: "medium",
+    showLabel: true,
+    labelPosition: "right",
+    label: "Loading...",
+  },
 };
 
 export const WithoutLabel: Story = {
-    args: {
-        showLabel: false,
-    },
+  args: {
+    showLabel: false,
+  },
 };
-
