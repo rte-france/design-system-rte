6eae995d3a24f7326813e17c3119f31d7628db44
Tommahs <thomascandille0508@gmail.com>
2025-09-04 10:08:12 +0200

    fix(popover): standardize quotes and formatting in Popover interface and component

--- packages/core/components/popover/popover.interface.d.ts
+++ packages/core/components/popover/popover.interface.d.ts
@@ -1,5 +1,5 @@
-export type PopoverPosition = 'auto' | 'top' | 'bottom' | 'left' | 'right';
-export type PopoverAlignment = 'start' | 'center' | 'end';
+export type PopoverPosition = "auto" | "top" | "bottom" | "left" | "right";
+export type PopoverAlignment = "start" | "center" | "end";
 
 export interface PopoverProps {
   position?: PopoverPosition;
