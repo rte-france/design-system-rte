1f32f62d20a619bbaffae3ed38e8981e005d3f05
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-09-10 17:22:28 +0200

    feat(dropdown): handle keyboard interaction for angular component

--- packages/angular/projects/ds-rte-lib/src/lib/components/dropdown/dropdownMenu/dropdown-menu.component.ts
+++ packages/angular/projects/ds-rte-lib/src/lib/components/dropdown/dropdownMenu/dropdown-menu.component.ts
@@ -1,5 +1,16 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject, input, output, ViewContainerRef, AfterViewInit, OnDestroy, viewChild } from "@angular/core";
+import {
+  Component,
+  inject,
+  input,
+  output,
+  ViewContainerRef,
+  AfterViewInit,
+  OnDestroy,
+  viewChild,
+  ElementRef,
+} from "@angular/core";
+import { ARROW_DOWN_KEY, ARROW_UP_KEY, TAB_KEY } from "@design-system-rte/core/constants/keyboard/keyboard.constants";
 
 import { DropdownItemComponent } from "../dropdownItem/dropdown-item.component";
 import { DropdownService } from "../service/dropdown.service";
@@ -16,6 +27,7 @@ export class DropdownMenuComponent implements AfterViewInit, OnDestroy {
   readonly positionY = input<number>(0);
 
   readonly vcr = viewChild("container", { read: ViewContainerRef });
+  readonly dropdownRef = viewChild<ElementRef>("menu");
   private pendingItems?: ReadonlyArray<DropdownItemComponent>;
   private dropdownService: DropdownService;
   readonly childrenDropdownId = input<string>("");
@@ -67,5 +79,54 @@ export class DropdownMenuComponent implements AfterViewInit, OnDestroy {
 
   handleClick(event: MouseEvent) {
     this.close.emit(event);
+  }
+
+  private focusNextElement = (dropdown: HTMLElement) => {
+    const menuItems = this.getDropdownMenuItems(dropdown);
+    const activeElementIndex = menuItems.findIndex((el) => el === document.activeElement);
+
+    if (activeElementIndex < menuItems.length - 1) {
+      const nextElement = menuItems[activeElementIndex + 1];
+      nextElement.focus();
+    } else {
+      menuItems[0]?.focus();
+    }
+  };
+
+  private focusPreviousElement = (dropdown: HTMLElement) => {
+    const menuItems = this.getDropdownMenuItems(dropdown);
+    const activeElementIndex = menuItems.findIndex((el) => el === document.activeElement);
+
+    if (activeElementIndex > 0) {
+      const previousElement = menuItems[activeElementIndex - 1];
+      previousElement.focus();
+    } else {
+      menuItems[menuItems.length - 1]?.focus();
+    }
+  };
+
+  private getDropdownMenuItems = (dropdown: HTMLElement) => {
+    const allDropdownElement = dropdown?.querySelectorAll<HTMLElement>('li[role="menuitem"]');
+    return allDropdownElement ? Array.from(allDropdownElement) : [];
+  };
+
+  handleKeydown(event: KeyboardEvent) {
+    if (event.key === TAB_KEY) {
+      if (this.isOpen()) {
+        event.preventDefault();
+      }
+    } else if (event.key === ARROW_DOWN_KEY || event.key === ARROW_UP_KEY) {
+      event.preventDefault();
+    }
+  }
+
+  handleKeyup(event: KeyboardEvent) {
+    if (event.key === ARROW_DOWN_KEY) {
+      this.focusNextElement(this.dropdownRef()?.nativeElement);
+    } else if (event.key === ARROW_UP_KEY) {
+      this.focusPreviousElement(this.dropdownRef()?.nativeElement);
+    } else if (event.key === "Escape") {
+      this.close.emit(event);
+    }
   }
 }
