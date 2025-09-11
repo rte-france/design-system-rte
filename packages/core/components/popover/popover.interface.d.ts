5146f4165cb59473382b035e607a7cd1ab880b60
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-09-11 16:18:20 +0200

    feat(popover): handle alignment + add buttons method callback props

--- packages/core/components/popover/popover.interface.d.ts
+++ packages/core/components/popover/popover.interface.d.ts
@@ -2,13 +2,15 @@ export type PopoverPosition = "auto" | "top" | "bottom" | "left" | "right";
 export type PopoverAlignment = "start" | "center" | "end";
 
 export interface PopoverProps {
+  primaryButtonLabel: string;
   position?: PopoverPosition;
   alignment?: PopoverAlignment;
   content?: string;
   arrow?: boolean;
   title?: string;
   showTitle?: boolean;
-  buttonLabel?: string;
-  button2?: boolean;
-  button2Label?: string;
+  secondaryButtonLabel?: string;
+  onClickPrimaryButton?: () => void;
+  onClickSecondaryButton?: () => void;
+  isOpen?: boolean;
 }
