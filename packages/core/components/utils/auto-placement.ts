d915219246fabe5e2de2fd23d0d65870653ae917
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-09-12 17:39:30 +0200

    feat(popover): add keyboard interaction story test

--- packages/core/components/utils/auto-placement.ts
+++ packages/core/components/utils/auto-placement.ts
@@ -115,12 +115,6 @@ export const getCoordinates = (
   const triggerElementRect = triggerElement.getBoundingClientRect();
   const castedElementRect = castedElement.getBoundingClientRect();
 
-  console.log({ position });
-  console.log({ alignment });
-  console.log({ triggerElementRect });
-  console.log({ castedElementRect });
-  console.log({ castedElement: castedElement });
-
   if (position === "bottom") {
     return handleBottomPosition(triggerElementRect, castedElementRect, offset, alignment);
   } else if (position === "left") {
