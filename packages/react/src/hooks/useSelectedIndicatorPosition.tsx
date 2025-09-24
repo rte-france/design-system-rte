1597c17434a1a8ccd3f39282d8d5f6dd98d97804
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-09-24 22:04:44 +0200

    feat(tab): add react component

--- packages/react/src/hooks/useSelectedIndicatorPosition.tsx
+++ packages/react/src/hooks/useSelectedIndicatorPosition.tsx
@@ -2,15 +2,15 @@ import { useCallback, useEffect, useState } from "react";
 
 const useSelectedIndicatorPosition = (
   containerRef: React.MutableRefObject<HTMLDivElement | null>,
-  options: { id: string }[],
   selectedId?: string,
-  position?: "bottom" | "center",
+  position?: "bottom" | "center" | "left",
 ): {
   left: number;
   top: number;
-  width: number;
+  width?: number;
+  height?: number;
 } => {
-  const [indicatorStyle, setIndicatorStyle] = useState<{ left: number; top: number; width: number }>({
+  const [indicatorStyle, setIndicatorStyle] = useState<{ left: number; top: number; width?: number; height?: number }>({
     left: 0,
     top: 0,
     width: 0,
@@ -19,13 +19,20 @@ const useSelectedIndicatorPosition = (
   const updateIndicator = useCallback(() => {
     if (!containerRef.current) return;
     const node = containerRef.current.querySelector(`#${selectedId}`) as HTMLElement | undefined;
+
     if (node)
       if (position === "bottom") {
         setIndicatorStyle({ left: node.offsetLeft, top: node.offsetTop + node.offsetHeight, width: node.offsetWidth });
+      } else if (position === "left") {
+        setIndicatorStyle({
+          left: node.offsetLeft - 2,
+          top: node.offsetTop,
+          height: node.offsetHeight,
+        });
       } else {
         setIndicatorStyle({ left: node.offsetLeft, top: node.offsetTop, width: node.offsetWidth });
       }
-  }, [selectedId, options, containerRef]);
+  }, [selectedId, containerRef, position]);
 
   useEffect(() => {
     updateIndicator();
@@ -33,7 +40,7 @@ const useSelectedIndicatorPosition = (
     return () => {
       window.removeEventListener("resize", updateIndicator);
     };
-  }, [selectedId, options, containerRef, updateIndicator]);
+  }, [selectedId, containerRef, updateIndicator]);
 
   return indicatorStyle;
 };
