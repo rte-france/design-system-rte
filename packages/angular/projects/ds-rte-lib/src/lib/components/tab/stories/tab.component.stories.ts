1517314ee67524485a0fb688a313be5f105fb42c
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-10-09 17:12:39 +0200

    feat(tab): add overflow type stories

--- packages/angular/projects/ds-rte-lib/src/lib/components/tab/stories/tab.component.stories.ts
+++ packages/angular/projects/ds-rte-lib/src/lib/components/tab/stories/tab.component.stories.ts
@@ -348,6 +348,167 @@ export const WithBadge: Story = {
   }),
 };
 
+export const OverflowType: Story = {
+  args: {
+    options: [
+      {
+        id: "tab-1",
+        label: "First Tab",
+        panelId: "panel-1",
+      },
+      {
+        id: "tab-2",
+        label: "Second Tab",
+        panelId: "panel-2",
+      },
+      {
+        id: "tab-3",
+        label: "Third Tab",
+        panelId: "panel-3",
+      },
+      {
+        id: "tab-4",
+        label: "Fourth Tab",
+        panelId: "panel-4",
+        disabled: true,
+      },
+      {
+        id: "tab-5",
+        label: "Fifth Tab",
+        panelId: "panel-5",
+      },
+    ],
+    selectedTabId: "tab-1",
+    alignment: "start",
+    compactSpacing: false,
+    direction: "horizontal",
+    overflowType: "scrollable",
+  },
+  render: (args) => ({
+    props: {
+      ...args,
+      change(id: string) {
+        this["selectedTabId"] = id;
+      },
+    },
+    template: `
+    <div style="display: flex; flexDirection: column; gap: 32px">
+      <div style="width: 400px">
+        <span style="font-family: Arial">Scrollable</span>
+        <rte-tab
+          [options]="options"
+          [selectedTabId]="selectedTabId"
+          (change)="change($event)"
+          [direction]="direction"
+          [alignment]="alignment"
+          [compactSpacing]="compactSpacing"
+          [overflowType]="overflowType"
+          [inverted]="inverted"
+        />
+        <div style="height: 100px; border: 1px solid #ccc; padding: 8px; marginTop: 16px">
+          <div [hidden]="selectedTabId !== 'tab-1'" id="panel-1">
+            Content for First Tab
+          </div>
+          <div [hidden]="selectedTabId !== 'tab-2'" id="panel-2">
+            Content for Second Tab
+          </div>
+          <div [hidden]="selectedTabId !== 'tab-3'" id="panel-3">
+            Content for Third Tab
+          </div>
+          <div [hidden]="selectedTabId !== 'tab-4'" id="panel-4">
+            Content for Fourth Tab
+          </div>
+          <div [hidden]="selectedTabId !== 'tab-5'" id="panel-5">
+            Content for Fifth Tab
+          </div>
+        </div>
+      </div>
+      <div style="width: 400px">
+        <span style="font-family: Arial">Dropdown</span>
+        <rte-tab
+          [options]="options"
+          [selectedTabId]="selectedTabId"
+          (change)="change($event)"
+          [direction]="direction"
+          [alignment]="alignment"
+          [compactSpacing]="compactSpacing"
+          [overflowType]="'dropdown'"
+          [inverted]="inverted"
+        />
+        <div style="height: 100px; border: 1px solid #ccc; padding: 8px; marginTop: 16px">
+          <div [hidden]="selectedTabId !== 'tab-1'" id="panel-1">
+            Content for First Tab
+          </div>
+          <div [hidden]="selectedTabId !== 'tab-2'" id="panel-2">
+            Content for Second Tab
+          </div>
+          <div [hidden]="selectedTabId !== 'tab-3'" id="panel-3">
+            Content for Third Tab
+          </div>
+          <div [hidden]="selectedTabId !== 'tab-4'" id="panel-4">
+            Content for Fourth Tab
+          </div>
+          <div [hidden]="selectedTabId !== 'tab-5'" id="panel-5">
+            Content for Fifth Tab
+          </div>
+        </div>
+      </div>
+    </div>
+    `,
+  }),
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+    const firstTab = await canvas.getByRole("tab", { name: "First Tab" });
+    const secondTab = await canvas.getByRole("tab", { name: "Second Tab" });
+    const thirdTab = await canvas.getByRole("tab", { name: "Third Tab" });
+    const fourthTab = await canvas.getByRole("tab", { name: "Fourth Tab" });
+    const fifthTab = await canvas.getByRole("tab", { name: "Fifth Tab" });
+
+    const expectTabToBeSelected = async (tab: HTMLElement) => {
+      expect(tab).toHaveAttribute("aria-selected", "true");
+      expect(tab).toHaveFocus();
+      expect(canvas.getByText(`Content for ${tab.textContent}`)).toBeVisible();
+    };
+
+    const expectTabToBeNotSelected = async (tab: HTMLElement) => {
+      expect(tab).toHaveAttribute("aria-selected", "false");
+      expect(tab).not.toHaveFocus();
+      expect(canvas.getByText(`Content for ${tab.textContent}`)).not.toBeVisible();
+    };
+
+    await step("Focus on the first tab and select it", async () => {
+      await userEvent.tab();
+      expectTabToBeSelected(firstTab);
+    });
+
+    await step("Navigate to the second tab and select it", async () => {
+      await userEvent.keyboard(TESTING_ARROW_RIGHT_KEY);
+      expectTabToBeSelected(secondTab);
+    });
+
+    await step("Navigate to the second tab and select it", async () => {
+      await userEvent.keyboard(TESTING_ARROW_RIGHT_KEY);
+      expectTabToBeSelected(thirdTab);
+    });
+
+    await step("Navigate directly to the fifth tab and select it because the forth is disabled", async () => {
+      await userEvent.keyboard(TESTING_ARROW_RIGHT_KEY);
+      expectTabToBeSelected(fifthTab);
+      expectTabToBeNotSelected(fourthTab);
+    });
+
+    await step("Navigate back to the first tab when there are no next tabs", async () => {
+      await userEvent.keyboard(TESTING_ARROW_RIGHT_KEY);
+      expectTabToBeSelected(firstTab);
+    });
+
+    await step("Navigate back to the last tab and select it", async () => {
+      await userEvent.keyboard(TESTING_ARROW_LEFT_KEY);
+      expectTabToBeSelected(fifthTab);
+    });
+  },
+};
+
 export const KeyboardInteraction: Story = {
   args: {
     options: [
