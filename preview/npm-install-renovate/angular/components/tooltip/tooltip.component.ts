7389d8542fa11a4ac1350295455f5213d10db916
jeremyrts <jeremyrts@users.noreply.github.com>
2025-10-14 12:40:23 +0000

    deploy: b7859159d2fe5711e734dd1d4bc0623bdf2b3853

---
+++ preview/npm-install-renovate/angular/components/tooltip/tooltip.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from "@angular/common";
+import { Component, input } from "@angular/core";
+import { Alignment, Position } from "@design-system-rte/core/components/common/common-types";
+
+@Component({
+  selector: "rte-tooltip",
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: "./tooltip.component.html",
+  styleUrl: "./tooltip.component.scss",
+})
+export class TooltipComponent {
+  readonly label = input.required<string>();
+  readonly position = input<Exclude<Position, "auto">>("top");
+  readonly alignment = input<Alignment>("center");
+  readonly arrow = input<boolean>(true);
+}
