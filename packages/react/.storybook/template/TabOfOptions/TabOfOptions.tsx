6c15b956df5c282a637df8f6282b66434b342c29
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-10-15 17:11:35 +0200

    feat(storybook): Add TabOfOptions component

---
+++ packages/react/.storybook/template/TabOfOptions/TabOfOptions.tsx
@@ -0,0 +1,35 @@
+import "./tabOfOptions.css";
+
+interface TabOfOptionsProps {
+  options: {
+    property: string;
+    value: string;
+    defaultValue: string;
+    dependance: string;
+  }[];
+}
+
+export const TabOfOptions = ({ options }: TabOfOptionsProps) => {
+  return (
+    <table className="rte-stories-table">
+      <thead>
+        <tr>
+          <th scope="col">Property</th>
+          <th scope="col">Value</th>
+          <th scope="col">Default Value</th>
+          <th scope="col">Dependance</th>
+        </tr>
+      </thead>
+      <tbody>
+        {options.map((option, index) => (
+          <tr key={index}>
+            <th scope="row">{option.property}</th>
+            <td>{option.value}</td>
+            <td>{option.defaultValue}</td>
+            <td>{option.dependance}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
