273373305b30d1ec1bc1c5b89ddbf5fa9c6506d9
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-10-22 16:18:42 +0200

    chore(icon): add icon sizes constants

--- packages/core/components/common/common-types.ts
+++ packages/core/components/common/common-types.ts
@@ -1,3 +1,4 @@
 export type Position = "auto" | "top" | "bottom" | "left" | "right";
 export type Alignment = "start" | "center" | "end";
 export type Direction = "horizontal" | "vertical";
+export type Size = "xs" | "s" | "m" | "l" | "xl";
