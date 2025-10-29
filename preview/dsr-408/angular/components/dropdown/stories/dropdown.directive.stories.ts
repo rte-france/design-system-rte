f17a1a419ba6f6a86af4e9967956a7234d2a12a1
jeremyrts <jeremyrts@users.noreply.github.com>
2025-10-29 14:53:18 +0000

    deploy: ea8026b80b1b186c4b568bf4f3f50ec004c49f3b

--- preview/dsr-408/angular/components/dropdown/stories/dropdown.directive.stories.ts
+++ preview/dsr-408/angular/components/dropdown/stories/dropdown.directive.stories.ts
@@ -1,4 +1,10 @@
+import {
+  TESTING_DOWN_KEY,
+  TESTING_ENTER_KEY,
+  TESTING_UP_KEY,
+} from "@design-system-rte/core/constants/keyboard/keyboard-test.constants";
 import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
+import { expect, userEvent } from "@storybook/test";
 
 import { DropdownDirective } from "../dropdown.directive";
 import { DropdownModule } from "../dropdown.module";
@@ -67,4 +73,89 @@ export const Default: Story = {
     </div>
     `,
   }),
+};
+
+export const KeyboardNavigation: Story = {
+  decorators: [
+    moduleMetadata({
+      imports: [DropdownModule],
+    }),
+  ],
+  args: {
+    rteDropdownPosition: "bottom",
+  },
+  render: (args) => ({
+    props: {
+      ...args,
+      items: MOCKUP_ITEMS,
+      onItemClick: (event: { event: Event; id: string }) => {
+        console.log("Item clicked:", event);
+      },
+    },
+    template: `
+    ${wipWarning}
+    <div rteDropdown [rteDropdownPosition]="rteDropdownPosition" (menuEvent)="onItemClick($event)">
+      <button rteDropdownTrigger>Menu principal ⬇</button>
+      <rte-dropdown-menu [items]="items"/>
+    </div>
+    `,
+  }),
+  play: async () => {
+    await userEvent.tab();
+    await userEvent.keyboard(TESTING_ENTER_KEY);
+    await userEvent.tab();
+    const overlay = document.getElementById("overlay-root");
+    const dropdown = overlay?.querySelector("rte-dropdown-menu");
+    const menuItems = dropdown?.querySelector("ul")?.querySelectorAll("li");
+    expect(dropdown).toBeInTheDocument();
+    expect(menuItems?.[0]).toHaveFocus();
+    await userEvent.keyboard(TESTING_DOWN_KEY);
+    expect(menuItems?.[1]).toHaveFocus();
+    await userEvent.keyboard(TESTING_UP_KEY);
+    expect(menuItems?.[0]).toHaveFocus();
+  },
+};
+
+export const KeyboardNavigationWithLink: Story = {
+  decorators: [
+    moduleMetadata({
+      imports: [DropdownModule],
+    }),
+  ],
+  args: {
+    rteDropdownPosition: "bottom",
+  },
+  render: (args) => ({
+    props: {
+      ...args,
+      items: [
+        { label: "Messages", leftIcon: "mail", link: "/messages" },
+        { label: "Username", leftIcon: "user-circle", link: "/username" },
+      ],
+      onItemClick: (event: { event: Event; id: string }) => {
+        console.log("Item clicked:", event);
+      },
+    },
+    template: `
+    ${wipWarning}
+    <div rteDropdown [rteDropdownPosition]="rteDropdownPosition" (menuEvent)="onItemClick($event)">
+      <button rteDropdownTrigger>Menu principal ⬇</button>
+      <rte-dropdown-menu [items]="items"/>
+    </div>
+    `,
+  }),
+  play: async () => {
+    await userEvent.tab();
+    await userEvent.keyboard(TESTING_ENTER_KEY);
+    await userEvent.tab();
+    const overlay = document.getElementById("overlay-root");
+    const dropdown = overlay?.querySelector("rte-dropdown-menu");
+    const menuItems = dropdown?.querySelector("ul")?.querySelectorAll("li");
+    expect(dropdown).toBeInTheDocument();
+    expect(menuItems?.[0]).toHaveFocus();
+    await userEvent.keyboard(TESTING_DOWN_KEY);
+    expect(menuItems?.[1]).toHaveFocus();
+    await userEvent.keyboard(TESTING_UP_KEY);
+    expect(menuItems?.[0]).toHaveFocus();
+  },
 };
