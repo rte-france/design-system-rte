5a0697843bcd1a810146563b02f8c7292a7635ea
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-10-30 10:08:08 +0100

    feat(breadcrumbs): add tooltip on ellipsed item

--- packages/angular/projects/ds-rte-lib/src/lib/components/link/link.component.ts
+++ packages/angular/projects/ds-rte-lib/src/lib/components/link/link.component.ts
@@ -17,5 +17,5 @@ export class LinkComponent {
   readonly subtle = input<boolean>(false);
   readonly externalLink = input<boolean>(false);
   readonly reverse = input<boolean>(false);
-  readonly maxWidth = input<number | null>(null);
+  readonly maxWidth = input<number>();
 }
--- packages/react/src/components/breadcrumbs/Breadcrumbs.tsx
+++ packages/react/src/components/breadcrumbs/Breadcrumbs.tsx
@@ -4,21 +4,28 @@ import {
   getBreadcrumbsTruncatedItems,
   shouldTruncateBreadcrumbs,
 } from "@design-system-rte/core/components/breadcrumbs/breadcrumbs.utils";
-import { forwardRef, useState } from "react";
+import { forwardRef, useState, Fragment } from "react";
 
 import { Dropdown } from "../dropdown/Dropdown";
 import { DropdownItem } from "../dropdown/dropdownItem/DropdownItem";
 import IconButton from "../iconButton/IconButton";
-import Link from "../link/Link";
 import { concatClassNames } from "../utils";
 
+import BreadcrumbItem from "./breadcrumbItem/BreadcrumbItem.tsx";
 import style from "./Breadcrumbs.module.scss";
 
 export interface BreadcrumbsProps extends BreadcrumbsPropsCore, React.HTMLAttributes<HTMLElement> {}
 
+const Separator = () => (
+  <span aria-hidden="true" className={style.separator}>
+    /
+  </span>
+);
+
 const Breadcrumbs = forwardRef<HTMLDivElement, BreadcrumbsProps>(
   ({ items, ariaLabel = BREADCRUMBS_DEFAULT_ARIA_LABEL, breadcrumbItemMaxWidth, ...props }, ref) => {
     const [isTrucatedListOpened, setIsTruncatedListOpened] = useState<boolean>(false);
+
     if (shouldTruncateBreadcrumbs(items)) {
       const { root, truncated, remaining } = getBreadcrumbsTruncatedItems(items);
 
@@ -30,19 +37,12 @@ const Breadcrumbs = forwardRef<HTMLDivElement, BreadcrumbsProps>(
           role="navigation"
           aria-label={ariaLabel}
         >
-          <Link
-            subtle
-            href={root.link}
-            label={root.label}
-            style={{
-              color: "var(--content-tertiary)",
-              maxWidth: `${breadcrumbItemMaxWidth}px`,
-            }}
-            className={style.breadcrumbItem}
+          <BreadcrumbItem
+            item={{ label: root.label, link: root.link }}
+            isLast={false}
+            breadcrumbItemMaxWidth={breadcrumbItemMaxWidth}
           />
-          <span aria-hidden="true" className={style.separator}>
-            /
-          </span>
+          <Separator />
 
           <span className={style.breadcrumbItem}>
             <Dropdown
@@ -71,31 +71,14 @@ const Breadcrumbs = forwardRef<HTMLDivElement, BreadcrumbsProps>(
             /
           </span>
           {remaining.map((item, index) => (
-            <div key={item.link} className={style.breadcrumbItem}>
-              {index === remaining.length - 1 ? (
-                <a
-                  role="link"
-                  aria-label={item.label}
-                  aria-current="page"
-                  tabIndex={0}
-                  style={{ maxWidth: `${breadcrumbItemMaxWidth}px` }}
-                >
-                  {item.label}
-                </a>
-              ) : (
-                <Link
-                  href={item.link}
-                  label={item.label}
-                  subtle
-                  style={{ color: "var(--content-tertiary)", maxWidth: `${breadcrumbItemMaxWidth}px` }}
-                />
-              )}
-              {index < remaining.length - 1 && (
-                <span aria-hidden="true" className={style.separator}>
-                  /
-                </span>
-              )}
-            </div>
+            <Fragment key={item.label + index}>
+              <BreadcrumbItem
+                item={item}
+                isLast={index === remaining.length - 1}
+                breadcrumbItemMaxWidth={breadcrumbItemMaxWidth}
+              />
+              {index < remaining.length - 1 && <Separator />}
+            </Fragment>
           ))}
         </nav>
       );
@@ -104,31 +87,14 @@ const Breadcrumbs = forwardRef<HTMLDivElement, BreadcrumbsProps>(
     return (
       <nav ref={ref} className={style.breadcrumbsBadge} {...props} role="navigation" aria-label="Fil d'Ariane">
         {items.map((item, index) => (
-          <div key={item.link} className={style.breadcrumbItem}>
-            {index === items.length - 1 ? (
-              <a
-                role="link"
-                aria-label={item.label}
-                aria-current="page"
-                tabIndex={0}
-                style={{ maxWidth: `${breadcrumbItemMaxWidth}px` }}
-              >
-                {item.label}
-              </a>
-            ) : (
-              <Link
-                href={item.link}
-                label={item.label}
-                subtle
-                style={{ color: "var(--content-tertiary)", maxWidth: `${breadcrumbItemMaxWidth}px` }}
-              />
-            )}
-            {index < items.length - 1 && (
-              <span aria-hidden="true" className={style.separator}>
-                /
-              </span>
-            )}
-          </div>
+          <Fragment key={item.label + index}>
+            <BreadcrumbItem
+              item={item}
+              isLast={index === items.length - 1}
+              breadcrumbItemMaxWidth={breadcrumbItemMaxWidth}
+            />
+            {index < items.length - 1 && <Separator />}
+          </Fragment>
         ))}
       </nav>
     );
