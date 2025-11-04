735ea0d6fc250d7e1d08b6107efe3585860cb4cb
DiazAilan <DiazAilan@users.noreply.github.com>
2025-11-04 16:31:54 +0000

    deploy: e189c0a00fa2499a915539217b359f9fa8abd106

--- preview/sidenav-design-dev/react/components/sideNav/navItem/NavItem.d.ts
+++ preview/sidenav-design-dev/react/components/sideNav/navItem/NavItem.d.ts
@@ -2,6 +2,7 @@ import { NavItemProps as CoreNavItemProps } from '../../../../../core/components
 import { HTMLAttributes, ReactNode } from '../../../../../../node_modules/react';
 interface NavItemProps extends CoreNavItemProps, Omit<HTMLAttributes<HTMLLIElement>, "onClick"> {
     children?: ReactNode;
+    parentMenuOpen?: boolean;
 }
 declare const NavItem: import('../../../../../../node_modules/react').ForwardRefExoticComponent<NavItemProps & import('../../../../../../node_modules/react').RefAttributes<HTMLElement | HTMLLIElement>>;
 export default NavItem;
---
+++ preview/sidenav-design-dev/react/components/sideNav/navMenu/NavMenu.d.ts
@@ -0,0 +1,8 @@
+import { NavMenuProps as CoreNavMenuProps } from '../../../../../core/components/side-nav/nav-menu/nav-menu.interface';
+import { HTMLAttributes, ReactNode } from '../../../../../../node_modules/react';
+interface NavMenuProps extends CoreNavMenuProps, Omit<HTMLAttributes<HTMLLIElement>, "onClick"> {
+    children?: ReactNode;
+    parentMenuOpen?: boolean;
+}
+declare const NavMenu: import('../../../../../../node_modules/react').ForwardRefExoticComponent<NavMenuProps & import('../../../../../../node_modules/react').RefAttributes<HTMLElement | HTMLLIElement>>;
+export default NavMenu;
