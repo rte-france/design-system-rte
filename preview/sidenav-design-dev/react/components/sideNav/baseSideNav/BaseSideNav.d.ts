11143fa217e055d91fd2a6d9e4f6111d3637df21
DiazAilan <DiazAilan@users.noreply.github.com>
2025-11-10 15:47:07 +0000

    deploy: 0ebef8602f5750caf0ff7bbf083ca4d3b2bcf954

--- preview/sidenav-design-dev/react/components/sideNav/baseSideNav/BaseSideNav.d.ts
+++ preview/sidenav-design-dev/react/components/sideNav/baseSideNav/BaseSideNav.d.ts
@@ -1,10 +1,10 @@
 import { SideNavProps as CoreSideNavProps } from '../../../../../core/components/side-nav/side-nav.interface';
-import { default as React, ReactNode } from '../../../../../../node_modules/react';
+import { ReactNode } from '../../../../../../node_modules/react';
 interface BaseSideNavProps extends Partial<Omit<CoreSideNavProps, "items">>, Omit<React.HTMLAttributes<HTMLDivElement>, "content"> {
     header?: ReactNode;
     body?: ReactNode;
     footer?: ReactNode;
     children?: ReactNode;
 }
-declare const BaseSideNav: React.ForwardRefExoticComponent<BaseSideNavProps & React.RefAttributes<HTMLDivElement | HTMLElement>>;
+declare const BaseSideNav: import('../../../../../../node_modules/react').ForwardRefExoticComponent<BaseSideNavProps & import('../../../../../../node_modules/react').RefAttributes<HTMLDivElement | HTMLElement>>;
 export default BaseSideNav;
