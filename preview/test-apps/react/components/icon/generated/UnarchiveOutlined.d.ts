f6468245b66a9b7cf35eb08fe70d5476f3eadfdc
DiazAilan <DiazAilan@users.noreply.github.com>
2025-11-12 13:24:03 +0000

    deploy: cc64a7f62a4095be846f7814c6de09c4fb155f53

---
+++ preview/test-apps/react/components/icon/generated/UnarchiveOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '@design-system-rte/react/src/components/icon/Icon';
+declare const SvgUnarchiveOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgUnarchiveOutlined;
