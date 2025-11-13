2f5d3cf59259bbf39ac068574cd7d68ffc76dd37
DiazAilan <DiazAilan@users.noreply.github.com>
2025-11-13 15:29:11 +0000

    deploy: 026c483005dfb2a5cee8d15f75c87efbc147dc8f

--- preview/sidenav-design-dev/react/components/sideNav/navItem/NavItem.d.ts
+++ preview/sidenav-design-dev/react/components/sideNav/navItem/NavItem.d.ts
@@ -2,6 +2,7 @@ import { NavItemProps as CoreNavItemProps } from '../../../../../core/components
 import { HTMLAttributes, ReactNode } from '../../../../../../node_modules/react';
 interface NavItemProps extends CoreNavItemProps, Omit<HTMLAttributes<HTMLDivElement>, "onClick" | "id"> {
     children?: ReactNode;
+    isNested?: boolean;
     parentMenuOpen?: boolean;
 }
 declare const NavItem: import('../../../../../../node_modules/react').ForwardRefExoticComponent<NavItemProps & import('../../../../../../node_modules/react').RefAttributes<HTMLDivElement>>;
