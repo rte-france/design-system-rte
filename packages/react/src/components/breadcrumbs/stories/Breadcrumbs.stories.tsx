0013e36d694d4850394c0271d462ab69299b38a9
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-11-13 16:37:24 +0100

    feat(storybook): add breadcrumbs - overview page

--- packages/react/src/components/breadcrumbs/stories/Breadcrumbs.stories.tsx
+++ packages/react/src/components/breadcrumbs/stories/Breadcrumbs.stories.tsx
@@ -8,10 +8,10 @@ import { expect, userEvent, waitFor, within } from "@storybook/test";
 
 import { focusElementBeforeComponent } from "../../../.storybook/testing/testing.utils";
 
-import Breadcrumbs from "./Breadcrumbs";
+import Breadcrumbs from "../Breadcrumbs";
 
 const meta = {
-  title: "Composants/Breadcrumbs",
+  title: "Composants/Breadcrumbs/Breadcrumbs",
   component: Breadcrumbs,
   tags: ["autodocs"],
   argTypes: {
