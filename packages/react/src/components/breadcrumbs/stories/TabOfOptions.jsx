52d80eeec2ac38bb3d7c6f2e4c7badbca4e59d0d
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-11-13 16:36:50 +0100

    feat(storybook): add new tab of options, heading, availability and LastUpdate component

---
+++ packages/react/src/components/breadcrumbs/stories/TabOfOptions.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { TabOfOptions as CoreTabOfOptions } from "../../../../.storybook/template/TabOfOptions/TabOfOptions";
+
+const options = [
+  { property: "numberOfPages", value: "1 / 2 / 3 / 3+", defaultValue: "2", dependance: "-" },
+  { property: "activePageLabel", value: "Text", defaultValue: "Active page", dependance: "-" },
+  { property: "showBadge", value: "True / False", defaultValue: "False", dependance: "-" },
+  {
+    property: "mode",
+    value: "bleu-iceberg_light / bleu-iceberg_dark / violet_light / violet_dark / vert-foret_light / vert-foret_dark",
+    defaultValue: "bleu-iceberg_light",
+    dependance: "-",
+  },
+];
+
+export const TabOfOptions = () => {
+  return <CoreTabOfOptions options={options} />;
+};
