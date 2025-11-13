f1b172691a6f11fa4d8032ae035e624ef5f7a269
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-11-13 16:36:50 +0100

    feat(storybook): add new tab of options, heading, availability and LastUpdate component

---
+++ packages/react/.storybook/template/Heading/Heading.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Heading.scss";
+
+interface HeadingProps {
+  id: string;
+  level: number;
+  children: React.ReactNode;
+}
+
+const Heading = ({ id, level, children }: HeadingProps) => {
+  const style = level === 1 ? { fontFamily: "Nunito" } : { fontFamily: "Nunito", border: "none", margin: 0 };
+
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+  return (
+    <Tag id={id} style={style}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Heading;
