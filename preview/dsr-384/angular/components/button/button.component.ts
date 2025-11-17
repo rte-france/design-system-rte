aa42dc7c400b258ea37dae2a422b6f305724c9b0
jeremyrts <jeremyrts@users.noreply.github.com>
2025-11-17 10:21:02 +0000

    deploy: 6f93ec9b0e15402ed2824e5a620474f6fe3987d0

--- preview/dsr-384/angular/components/button/button.component.ts
+++ preview/dsr-384/angular/components/button/button.component.ts
@@ -1,27 +1,12 @@
 import { ChangeDetectionStrategy, Component, computed, HostBinding, input } from "@angular/core";
-import { BadgeContent, BadgeType } from "@design-system-rte/core/components/badge/badge.interface";
+import { BadgeContent } from "@design-system-rte/core/components/badge/badge.interface";
 import { ButtonSize, ButtonVariant } from "@design-system-rte/core/components/button/common/common-button";
-
-import { BadgeDirective } from "../badge/badge.directive";
-import { RegularIconIdKey, TogglableIconIdKey } from "../icon/icon.service";
 
 @Component({
   selector: "button[rteButton]",
   standalone: true,
   templateUrl: "./button.component.html",
   styleUrl: "./button.component.scss",
-  hostDirectives: [
-    {
-      directive: BadgeDirective,
-      inputs: [
-        "rteBadgeType: rteBadgeType",
-        "rteBadgeSize: rteButtonSize",
-        "rteBadgeContent: rteBadgeContent",
-        "rteBadgeCount: rteBadgeCount",
-        "rteBadgeIcon: rteBadgeIcon",
-      ],
-    },
-  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
@@ -29,8 +14,6 @@ export class ButtonComponent {
   readonly rteButtonSize = input<ButtonSize>("m");
   readonly rteBadgeCount = input<number>();
   readonly rteBadgeContent = input<BadgeContent>();
-  readonly rteBadgeType = input<BadgeType>();
-  readonly rteBadgeIcon = input<RegularIconIdKey | TogglableIconIdKey>("settings");
 
   @HostBinding("class") get classes() {
     return `rte-button ${this.rteButtonVariant()} size-${this.rteButtonSize()}`;
