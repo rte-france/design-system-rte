4176623ce094da20af5af391dcb13d072f71ee2c
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-11-19 17:10:17 +0100

    fix(toast): add useCallback to prevent infinite rendering

--- packages/react/src/components/toast/toastQueue/ToastQueueProvider.tsx
+++ packages/react/src/components/toast/toastQueue/ToastQueueProvider.tsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import { InputToast, ToastQueueContext, ToastQueueItem } from "./ToastQueueContext";
 
 const ToastQueueProvider = ({ children }: { children: React.ReactNode }) => {
   const [queue, setQueue] = useState<ToastQueueItem[]>([]);
 
-  const addToQueue = (toast: InputToast) => {
+  const addToQueue = useCallback((toast: InputToast) => {
     const priority = getPriority(toast);
 
     setQueue((prevQueue) => {
       return [...prevQueue, { id: toast.id, priority, isOpen: false }].sort((a, b) => a.priority - b.priority);
     });
-  };
+  }, []);
 
-  const removeFromQueue = (id: string) => {
+  const removeFromQueue = useCallback((id: string) => {
     setQueue((prevQueue) => prevQueue.filter((toast) => toast.id !== id));
-  };
+  }, []);
 
   const isFirstInQueue = (id: string) => {
     return queue.length > 0 && queue[0].id === id;
