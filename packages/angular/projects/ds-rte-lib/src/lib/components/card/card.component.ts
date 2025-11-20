6f3880c224cb547e62db27df7f42f6be9d232f88
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-11-20 15:11:37 +0100

    feat (card): add card component

---
+++ packages/angular/projects/ds-rte-lib/src/lib/components/card/card.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, computed, input, output } from "@angular/core";
+import { cardSize } from "@design-system-rte/core/components/card/card.constats";
+import { CardType } from "@design-system-rte/core/components/card/card.interface";
+import { Size } from "@design-system-rte/core/components/common/common-types";
+
+@Component({
+  selector: "rte-card",
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: "./card.component.html",
+  styleUrl: "./card.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardComponent {
+  readonly size = input<Size>("m");
+  readonly cardType = input<CardType>("default");
+  readonly clickable = input<boolean>(false);
+  readonly disabled = input<boolean>(false);
+
+  readonly cardClicked = output<void>();
+
+  readonly cardWidth = computed(() => `${cardSize[this.size()]}px`);
+
+  onClick(): void {
+    if (!this.disabled() && this.clickable()) {
+      this.cardClicked.emit();
+    }
+  }
+}
---
+++ packages/react/src/components/card/Card.tsx
@@ -0,0 +1,26 @@
+import { cardSize } from "@design-system-rte/core/components/card/card.constats";
+import { CardProps as CardPropsCore } from "@design-system-rte/core/components/card/card.interface";
+
+import styles from "./Card.module.scss";
+
+interface CardProps extends CardPropsCore {
+  children?: React.ReactNode;
+  onClick?: () => void;
+}
+
+const Card = ({ size, cardType, clickable, disabled, children, onClick }: CardProps) => {
+  return (
+    <div
+      className={styles.card}
+      style={{ width: cardSize[size] }}
+      data-card-type={cardType}
+      data-clickable={clickable}
+      data-disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
