0ef94bf793da25e7229d20fee1360a8c70962c97
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-11-24 14:33:44 +0100

    chore (card): add proper typing and forward html element ref

--- packages/react/src/components/card/Card.tsx
+++ packages/react/src/components/card/Card.tsx
@@ -1,56 +1,51 @@
 import { cardSize } from "@design-system-rte/core/components/card/card.constats";
 import { CardProps as CardPropsCore } from "@design-system-rte/core/components/card/card.interface";
 import { ENTER_KEY, SPACE_KEY } from "@design-system-rte/core/constants/keyboard/keyboard.constants";
+import { HTMLAttributes, forwardRef, KeyboardEvent } from "react";
 
 import { useActiveKeyboard } from "../../hooks/useActiveKeyboard";
 
 import styles from "./Card.module.scss";
 
-interface CardProps extends CardPropsCore {
-  children?: React.ReactNode;
+interface CardProps extends CardPropsCore, Omit<HTMLAttributes<HTMLDivElement>, "onClick"> {
   onClick?: () => void;
 }
 
-const Card = ({
-  size = "m",
-  cardType = "default",
-  clickable = false,
-  disabled = false,
-  children,
-  onClick,
-  ...props
-}: CardProps) => {
-  const keyboardHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (!disabled && clickable && (event.key === SPACE_KEY || event.key === ENTER_KEY)) {
-      event.preventDefault();
-      onClick?.();
-    }
-  };
+const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({ size = "m", cardType = "default", clickable = false, disabled = false, children, onClick, ...props }, ref) => {
+    const keyboardHandler = (event: KeyboardEvent<HTMLDivElement>) => {
+      if (!disabled && clickable && (event.key === SPACE_KEY || event.key === ENTER_KEY)) {
+        event.preventDefault();
+        onClick?.();
+      }
+    };
 
-  const { onKeyDown, onKeyUp, onBlur } = useActiveKeyboard<HTMLDivElement>(
-    { onKeyUp: keyboardHandler },
-    {
-      interactiveKeyCodes: [SPACE_KEY, ENTER_KEY],
-    },
-  );
+    const { onKeyDown, onKeyUp, onBlur } = useActiveKeyboard<HTMLDivElement>(
+      { onKeyUp: keyboardHandler },
+      {
+        interactiveKeyCodes: [SPACE_KEY, ENTER_KEY],
+      },
+    );
 
-  return (
-    <div
-      className={styles.card}
-      style={{ width: cardSize[size] }}
-      data-card-type={cardType}
-      data-clickable={clickable}
-      data-disabled={disabled}
-      tabIndex={clickable ? 0 : -1}
-      onClick={onClick}
-      onKeyDown={onKeyDown}
-      onKeyUp={onKeyUp}
-      onBlur={onBlur}
-      {...props}
-    >
-      {children}
-    </div>
-  );
-};
+    return (
+      <div
+        ref={ref}
+        className={styles.card}
+        style={{ width: cardSize[size] }}
+        data-card-type={cardType}
+        data-clickable={clickable}
+        data-disabled={disabled}
+        tabIndex={clickable ? 0 : -1}
+        onClick={onClick}
+        onKeyDown={onKeyDown}
+        onKeyUp={onKeyUp}
+        onBlur={onBlur}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  },
+);
 
 export default Card;
