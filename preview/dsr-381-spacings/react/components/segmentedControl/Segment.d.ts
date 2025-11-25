c666599d0464bdb53bf2f385dbae02467ad4bcd7
jeremyrts <jeremyrts@users.noreply.github.com>
2025-11-25 15:10:03 +0000

    deploy: f22d3ee6e890f28ee6319e634fe42eefdebc6022

---
+++ preview/dsr-381-spacings/react/components/segmentedControl/Segment.d.ts
@@ -0,0 +1,3 @@
+import { SegmentProps } from './SegmentedControl';
+declare const Segment: ({ id, icon, label, position, isSelected, onClick, ...props }: SegmentProps) => import("react/jsx-runtime").JSX.Element;
+export default Segment;
