d032db73e57287039e455797dfbfe03ce1ba3549
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-11-25 11:50:05 +0100

    docs: add displayConfig for design docs

--- packages/design-docs/.storybook/manager.ts
+++ packages/design-docs/.storybook/manager.ts
@@ -1,6 +1,7 @@
 import { addons } from "@storybook/manager-api";
 
 import { customThemes } from "./customTheme";
+import DISPLAY_CONFIG from "./displayConfig.json";
 
 function applyTheme(themeName = "bleu_iceberg_light"): void {
   try {
@@ -37,4 +38,61 @@ window.addEventListener("message", (event) => {
 
 function _emitThemeChange(theme: string, color: string): void {
   addons.getChannel().emit("THEME_CHANGED", { theme, color });
+}
+
+function _hideStories(storySections: Record<string, string[]>): void {
+  if (!Object.keys(storySections).length) {
+    return;
+  }
+
+  const selectors = Object.entries(storySections)
+    .map(([storySection, stories]) => _extractStorySelectorsFromSection({ sectionId: storySection, stories }))
+    .join(", ");
+
+  if (!selectors.length) {
+    return;
+  }
+
+  const styleId = "hidden-stories-style";
+  let styleElement = document.getElementById(styleId) as HTMLStyleElement | null;
+
+  if (!styleElement) {
+    styleElement = document.createElement("style");
+    styleElement.id = styleId;
+    document.head.appendChild(styleElement);
+  }
+
+  styleElement.textContent = `${selectors} { display: none !important; }`;
+}
+
+function _extractStorySelectorsFromSection(storySection: { sectionId: string; stories: string[] }): string[] {
+  return storySection.stories.map(
+    (story) => `#storybook-explorer-tree [data-item-id="${storySection.sectionId}-${story}--docs"]`,
+  );
+}
+
+function _hideSections(sections: string[]): void {
+  if (!sections.length) {
+    return;
+  }
+
+  const selectors = sections.map((section) => `#storybook-explorer-tree [data-item-id="${section}"]`).join(", ");
+
+  const styleId = "hidden-sections-style";
+  let styleElement = document.getElementById(styleId) as HTMLStyleElement | null;
+  if (!styleElement) {
+    styleElement = document.createElement("style");
+    styleElement.id = styleId;
+    document.head.appendChild(styleElement);
+  }
+
+  styleElement.textContent = `${selectors} { display: none !important; }`;
+}
+
+if (DISPLAY_CONFIG.hiddenStories) {
+  _hideStories(DISPLAY_CONFIG.hiddenStories);
+}
+
+if (DISPLAY_CONFIG.hiddenSections) {
+  _hideSections(DISPLAY_CONFIG.hiddenSections);
 }
