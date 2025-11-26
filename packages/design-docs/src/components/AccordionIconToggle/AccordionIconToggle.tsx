f0afe440a362bf5ca8465450264586973360c94a
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-11-26 10:09:05 +0100

    fix(doc): change closed accordion icon

---
+++ packages/design-docs/src/components/AccordionIconToggle/AccordionIconToggle.tsx
@@ -0,0 +1,14 @@
+import Icon from "@design-system-rte/react/src/components/icon/Icon";
+
+interface AccordionIconToggleProps {
+  isAccordionOpen: boolean;
+}
+
+const AccordionIconToggle = ({ isAccordionOpen = true }: AccordionIconToggleProps) => {
+  if (isAccordionOpen) {
+    return <Icon name="arrow-chevron-down" />;
+  }
+  return <Icon name="arrow-chevron-right" />;
+};
+
+export default AccordionIconToggle;
