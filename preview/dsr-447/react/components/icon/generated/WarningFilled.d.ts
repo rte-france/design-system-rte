3925af4bfb136fbc619e10377a33c1652f4a8645
DiazAilan <DiazAilan@users.noreply.github.com>
2025-11-26 10:46:25 +0000

    deploy: 740270ae8471596f7a0c40af921b1751398ac8df

---
+++ preview/dsr-447/react/components/icon/generated/WarningFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '@design-system-rte/react/src/components/icon/Icon';
+declare const SvgWarningFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgWarningFilled;
