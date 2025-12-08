d2058471bd9a6eaad312e88aa5f3814331dc8e49
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-12-08 16:38:17 +0100

    feat(select): harmonise readonly and disable behavior + add stories scenario

--- packages/core/components/select/select.interface.d.ts
+++ packages/core/components/select/select.interface.d.ts
@@ -3,4 +3,5 @@ import { InputProps } from "../common/input-props";
 export interface SelectProps extends InputProps {
   onClear?: () => void;
   options: { value: string; label: string }[];
+  showResetButton?: boolean;
 }
