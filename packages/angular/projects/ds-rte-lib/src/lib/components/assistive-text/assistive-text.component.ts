b1493778921dd20ce83ee9aa4918240b184d2361
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-12-11 14:49:30 +0100

    feat(assistive-text): add angular component

---
+++ packages/angular/projects/ds-rte-lib/src/lib/components/assistive-text/assistive-text.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from "@angular/common";
+import { Component, input, computed, ChangeDetectionStrategy } from "@angular/core";
+import { ASSISTIVE_TEXT_ICON_SIZE } from "@design-system-rte/core/components/assistive-text/assistive-text.constants";
+import { AssistiveTextProps } from "@design-system-rte/core/components/assistive-text/assistive-text.interface";
+import { getIconNameFromAppearance } from "@design-system-rte/core/components/assistive-text/assistive-text.utils";
+
+import { IconComponent } from "../icon/icon.component";
+import { LinkComponent } from "../link/link.component";
+
+@Component({
+  selector: "rte-assistive-text",
+  imports: [CommonModule, IconComponent, LinkComponent],
+  standalone: true,
+  templateUrl: "./assistive-text.component.html",
+  styleUrl: "./assistive-text.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AssistiveTextComponent {
+  readonly label = input<string>();
+  readonly appearance = input<AssistiveTextProps["appearance"]>("description");
+  readonly showIcon = input<boolean>(false);
+  readonly href = input<string>();
+
+  readonly isLink = computed(() => {
+    return this.appearance() === "link" && this.href() !== undefined;
+  });
+
+  readonly assistiveTextIconSize = ASSISTIVE_TEXT_ICON_SIZE;
+
+  readonly iconName = computed(() => {
+    return getIconNameFromAppearance(this.appearance()!);
+  });
+}
--- packages/react/src/components/assistivetext/AssistiveText.tsx
+++ packages/react/src/components/assistivetext/AssistiveText.tsx
@@ -1,5 +1,6 @@
 import { ASSISTIVE_TEXT_ICON_SIZE } from "@design-system-rte/core/components/assistive-text/assistive-text.constants";
 import { AssistiveTextProps } from "@design-system-rte/core/components/assistive-text/assistive-text.interface";
+import { getIconNameFromAppearance } from "@design-system-rte/core/components/assistive-text/assistive-text.utils";
 
 import Icon from "../icon/Icon";
 import Link from "../link/Link";
@@ -7,31 +8,19 @@ import Link from "../link/Link";
 import styles from "./AssistiveText.module.scss";
 
 const AssistiveText = ({ label, appearance, showIcon, href }: AssistiveTextProps) => {
+  const icon = getIconNameFromAppearance(appearance);
+
   return (
-    <div className={styles["assistive-text"]}>
-      {showIcon && appearance === "error" && (
+    <div className={styles["assistive-text"]} data-appearance={appearance}>
+      {showIcon && icon && (
         <Icon
-          name="error"
+          name={icon}
           appearance="outlined"
-          className={styles["assistive-icon-error"]}
+          className={styles[`assistive-icon-${appearance}`]}
           size={ASSISTIVE_TEXT_ICON_SIZE}
         />
       )}
-      {showIcon && appearance === "success" && (
-        <Icon
-          name="check-circle"
-          appearance="outlined"
-          className={styles["assistive-icon-success"]}
-          size={ASSISTIVE_TEXT_ICON_SIZE}
-        />
-      )}
-      {appearance === "link" ? (
-        <Link label={label} href={href} />
-      ) : (
-        <p className={styles["assistive-label"]} data-appearance={appearance}>
-          {label}
-        </p>
-      )}
+      {appearance === "link" ? <Link label={label} href={href} externalLink /> : <span>{label}</span>}
     </div>
   );
 };
