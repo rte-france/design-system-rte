3f8a67fcaa125ab4e99fdcfbf80b9edfb8726abb
jeremyrts <jeremyrts@users.noreply.github.com>
2025-12-17 10:48:30 +0000

    deploy: 0e5bb55b4ed54983233046fce147a829d06ecd04

---
+++ preview/dsr-117/react/components/icon/generated/SubtitlesOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgSubtitlesOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgSubtitlesOutlined;
