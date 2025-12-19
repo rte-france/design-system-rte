7c24c92789b2989e97765af5fb69568f58a43361
jeremyrts <jeremyrts@users.noreply.github.com>
2025-12-19 16:19:12 +0000

    deploy: 5854d262de0b7b78ba052059d62b78f887552bd8

---
+++ preview/fixfonts/react/components/icon/generated/ArrowAltDownLeft.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgArrowAltDownLeft: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgArrowAltDownLeft;
