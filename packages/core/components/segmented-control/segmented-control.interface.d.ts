073cd7e9fdcdfbec5c3f3657a0a016992399cce1
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2025-12-29 16:32:54 +0100

    feat(segmented control): add badge

--- packages/core/components/segmented-control/segmented-control.interface.d.ts
+++ packages/core/components/segmented-control/segmented-control.interface.d.ts
@@ -1,16 +1,18 @@
+import { BadgeHolderProps } from "../badge/badge.interface";
+
 export interface SegmentedControlProps {
   options: SegmentOptions[];
   onChange: (id: string) => void;
   selectedSegment?: string;
 }
 
-export interface SegmentOptions {
+export interface SegmentOptions extends BadgeHolderProps {
   id: string;
   label: string;
   icon?: string;
 }
 
-export interface SegmentProps {
+export interface SegmentProps extends BadgeHolderProps {
   id: string;
   position: "left" | "middle" | "right";
   label?: string;
