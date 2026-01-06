dfadacd454f8457dbbcc4335f1f736635e5c7d6f
jeremyrts <jeremyrts@users.noreply.github.com>
2026-01-06 10:32:52 +0000

    deploy: cb62c96705d8ad390a34964066e64b038d23ffb2

---
+++ preview/dsr-487/react/components/icon/generated/CloudOffFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgCloudOffFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgCloudOffFilled;
