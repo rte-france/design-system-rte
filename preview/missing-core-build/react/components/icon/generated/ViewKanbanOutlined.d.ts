9fb29bfeee667e61d20888dd956ed0a656aff996
jeremyrts <jeremyrts@users.noreply.github.com>
2026-01-08 08:58:16 +0000

    deploy: 62e5fb8fb875fde0f109d6384ac9d76fd8ec9966

---
+++ preview/missing-core-build/react/components/icon/generated/ViewKanbanOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgViewKanbanOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgViewKanbanOutlined;
