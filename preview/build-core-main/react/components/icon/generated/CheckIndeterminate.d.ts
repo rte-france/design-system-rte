32c7cf1343cd037cfa44868b16e4da002ba466f5
jeremyrts <jeremyrts@users.noreply.github.com>
2026-01-08 10:20:19 +0000

    deploy: 75cb2b1bc88491da5b2c6544ca5b0a0316112bbc

---
+++ preview/build-core-main/react/components/icon/generated/CheckIndeterminate.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgCheckIndeterminate: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgCheckIndeterminate;
