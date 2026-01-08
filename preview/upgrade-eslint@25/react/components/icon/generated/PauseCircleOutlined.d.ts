e8745eaaa161d2ad1d9532db38ba0ea2a17e6ca0
jeremyrts <jeremyrts@users.noreply.github.com>
2026-01-08 09:53:36 +0000

    deploy: 7dfcd1cd5b544b985a7f1196147a6753b7038e94

---
+++ preview/upgrade-eslint@25/react/components/icon/generated/PauseCircleOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgPauseCircleOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgPauseCircleOutlined;
---
+++ preview/upgrade-eslint@25/react/components/icon/generated/ShareFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgShareFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgShareFilled;
