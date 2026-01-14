37102716d3ca850bb977e9f7c6224b2a24e32d38
DiazAilan <DiazAilan@users.noreply.github.com>
2026-01-14 15:12:31 +0000

    deploy: 7919524274b40980e424442147858a1b4ffa6076

--- preview/dsr-139/assets/List-cvfsaEjS.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BsU69eyl.js";const o=({children:s})=>e.jsx("ul",{className:"colors-list",children:s});o.__docgenInfo={description:"",methods:[],displayName:"List",props:{children:{required:!1,tsType:{name:"ReactNode"},description:""}}};export{o as L};
