5b1612a837332b1e8971b8e945843f3a757b71ee
jeremyrts <jeremyrts@users.noreply.github.com>
2026-01-21 14:51:52 +0000

    deploy: 834f488c4f7720158bb9afd97bac265897aa03f4

---
+++ preview/dsr-422/assets/List-CvyzfldU.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DuMNXcSq.js";const o=({children:s})=>e.jsx("ul",{className:"colors-list",children:s});o.__docgenInfo={description:"",methods:[],displayName:"List",props:{children:{required:!1,tsType:{name:"ReactNode"},description:""}}};export{o as L};
