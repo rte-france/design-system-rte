611c95fba8c0dd6f61e488a1b2bf9d7c59295de5
jeremyrts <jeremyrts@users.noreply.github.com>
2026-01-21 15:54:35 +0000

    deploy: 19f5e4054afa761de0aeb18c5bd8508bc58ce3a9

---
+++ preview/dsr-501/react/components/icon/generated/DraftOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgDraftOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgDraftOutlined;
