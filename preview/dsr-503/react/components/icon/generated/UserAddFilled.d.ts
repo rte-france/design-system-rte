b205e28eeccd669eeaaf9be288f2f5980085f4a3
jeremyrts <jeremyrts@users.noreply.github.com>
2026-01-23 16:10:25 +0000

    deploy: 34cb912dbb5c1cca376735dcf9e0f6fa4a6cc935

---
+++ preview/dsr-503/react/components/icon/generated/UserAddFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgUserAddFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgUserAddFilled;
