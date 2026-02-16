42f6013635c4bcfab8b8c2f6c1442fd8a554e57a
DiazAilan <DiazAilan@users.noreply.github.com>
2026-02-16 16:37:37 +0000

    deploy: add84d3a5b6b5cdf9aff3470e7666bfd55ee7b77

---
+++ preview/dsr-466/react/components/icon/generated/ArrowAltDown.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgArrowAltDown: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgArrowAltDown;
