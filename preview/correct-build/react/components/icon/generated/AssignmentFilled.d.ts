c811b86e0f6b8a6f22eb950c02dd01e28a842bcf
DiazAilan <DiazAilan@users.noreply.github.com>
2026-02-23 12:27:10 +0000

    deploy: 76c58b2cea90e298478cea3c9f6b69f89a3489d5

---
+++ preview/correct-build/react/components/icon/generated/AssignmentFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgAssignmentFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgAssignmentFilled;
