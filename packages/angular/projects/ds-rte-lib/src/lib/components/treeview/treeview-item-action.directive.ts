2b0824ca2be611f4a669f5d030e72d72b0f3ddf6
Gonzalo Diaz Ailan <diaz.ailan.gonzalo@gmail.com>
2026-03-09 15:38:22 +0100

    feat (treeview): set base structure for Treeview in Angular

---
+++ packages/angular/projects/ds-rte-lib/src/lib/components/treeview/treeview-item-action.directive.ts
@@ -0,0 +1,7 @@
+import { Directive } from "@angular/core";
+
+@Directive({
+  selector: "[rteTreeviewItemAction]",
+  standalone: true,
+})
+export class TreeviewItemActionDirective {}
---
+++ packages/core/components/treeview/treeview-item.interface.d.ts
@@ -0,0 +1,23 @@
+import { BadgeProps } from "../badge/badge.interface";
+
+export type TreeviewBorderType = "vertical" | "branch" | "corner" | "horizontal" | "spacer";
+
+export interface TreeviewItemProps {
+  id?: string;
+  labelText: string;
+  icon?: string;
+  link?: string;
+  disabled?: boolean;
+  isCompact?: boolean;
+  hasCheckbox?: boolean;
+  isSelected?: boolean;
+  isOpen?: boolean;
+  hasIcon?: boolean;
+  hasAction?: boolean;
+  hasBadge?: boolean;
+  newLine?: boolean;
+  items?: TreeviewItemProps[];
+  badge?: BadgeProps;
+  borderType?: TreeviewBorderType;
+  dottedLine?: boolean;
+}
