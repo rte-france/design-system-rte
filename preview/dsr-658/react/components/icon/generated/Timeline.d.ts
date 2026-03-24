b042d1f57f5b844a98e0d42d21f6566e02572d18
DiazAilan <DiazAilan@users.noreply.github.com>
2026-03-24 09:15:13 +0000

    deploy: 66dc81e02030924293d70a7fc1b0640a5de3f4da

---
+++ preview/dsr-658/react/components/icon/generated/Timeline.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgTimeline: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgTimeline;
