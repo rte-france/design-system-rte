886a710fa16559cceee714f9b88121637642a530
jeremyrts <jeremyrts@users.noreply.github.com>
2026-03-24 11:18:21 +0000

    deploy: 6772bbc055e1d8e2e60b24ee01c376d56a2653a1

---
+++ preview/dsr-272-react/angular/components/treeview/treeview-item/treeview-item-border/treeview-item-border.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, computed, input } from "@angular/core";
+import type { SvgConfig } from "@design-system-rte/core/common/svg";
+import { TREEVIEW_BORDER_SVG_MAP, TREEVIEW_BORDER_SVG_MAP_COMPACT } from "@design-system-rte/core/components/treeview";
+import type { TreeviewBorderType } from "@design-system-rte/core/components/treeview/treeview-item.interface";
+
+@Component({
+  selector: "rte-treeview-item-border",
+  standalone: true,
+  templateUrl: "./treeview-item-border.component.html",
+  styleUrl: "./treeview-item-border.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TreeviewItemBorderComponent {
+  readonly borderType = input.required<TreeviewBorderType>();
+  readonly dottedLine = input<boolean>(false);
+  readonly isCompact = input<boolean>(false);
+
+  readonly svgConfig = computed<SvgConfig>(() => {
+    const map = this.isCompact() ? TREEVIEW_BORDER_SVG_MAP_COMPACT : TREEVIEW_BORDER_SVG_MAP;
+    return map[this.borderType()];
+  });
+}
