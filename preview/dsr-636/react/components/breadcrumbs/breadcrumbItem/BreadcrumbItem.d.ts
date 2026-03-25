4b7316cb9f1f7928d06424f5ebf8936b5fc85ea2
jeremyrts <jeremyrts@users.noreply.github.com>
2026-03-25 13:58:44 +0000

    deploy: 778f04b6c84e2017b622b9c8c1c280b5b1e73dc4

---
+++ preview/dsr-636/react/components/breadcrumbs/breadcrumbItem/BreadcrumbItem.d.ts
@@ -0,0 +1,3 @@
+import { BreadcrumbProps } from '../../../../../core/components/breadcrumbs/breadcrumbs.interface';
+declare const BreadcrumbItem: ({ item, isLast, breadcrumbItemMaxWidth }: BreadcrumbProps) => import("react/jsx-runtime").JSX.Element;
+export default BreadcrumbItem;
