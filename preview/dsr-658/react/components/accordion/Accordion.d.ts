8dd53823a116639d42ec1bf53638eede911e07c5
DiazAilan <DiazAilan@users.noreply.github.com>
2026-03-25 16:47:01 +0000

    deploy: ad47e4d0c3407d6a6eff731c91cb1bd3e1c0d0cd

--- preview/dsr-658/react/components/accordion/Accordion.d.ts
+++ preview/dsr-658/react/components/accordion/Accordion.d.ts
@@ -1,6 +1,6 @@
 import { AccordionProps as coreAccordionProps } from '../../../../core/components/accordion/accordion.interface';
-interface AccordionProps extends coreAccordionProps, React.HTMLAttributes<HTMLButtonElement> {
+interface AccordionProps extends coreAccordionProps, Omit<React.HTMLAttributes<HTMLButtonElement>, "title"> {
     children?: React.ReactNode | React.ReactNode[];
 }
-declare const Accordion: ({ id, children, icon, alignement, disabled, isOpen, appearance, compactSpacing, size, onClick, }: AccordionProps) => import("react/jsx-runtime").JSX.Element;
+declare const Accordion: import('../../../../../node_modules/react').ForwardRefExoticComponent<AccordionProps & import('../../../../../node_modules/react').RefAttributes<HTMLButtonElement>>;
 export default Accordion;
