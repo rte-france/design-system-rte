afeeddf5749f667c692d45a32e13792e888b2c82
DiazAilan <DiazAilan@users.noreply.github.com>
2026-03-26 10:34:09 +0000

    deploy: 62f7253fbb931afb7229136993bdf81db5efa3b2

---
+++ preview/dsr-635/react/components/icon/generated/SmartphoneFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgSmartphoneFilled: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgSmartphoneFilled;
