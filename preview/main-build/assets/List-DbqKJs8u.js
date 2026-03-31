5e8dea64abe32069b6dba5cc1b721246622bf66b
DiazAilan <DiazAilan@users.noreply.github.com>
2026-03-31 11:23:51 +0000

    deploy: 38231c9538e31acb637d5da7d8c2df249b0ea584

--- preview/main-build/assets/List-DbqKJs8u.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Ge3Bp8km.js";const o=({children:s})=>e.jsx("ul",{className:"colors-list",children:s});o.__docgenInfo={description:"",methods:[],displayName:"List",props:{children:{required:!1,tsType:{name:"ReactNode"},description:""}}};export{o as L};
