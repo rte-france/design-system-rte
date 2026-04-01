b379093b9c67f5ba5bcfc4be58d4686319d31464
DiazAilan <DiazAilan@users.noreply.github.com>
2026-04-01 14:48:42 +0000

    deploy: bb04e4951fcc07a94ce4ad9e0a35865f858b0359

--- react/index.d.ts
+++ react/index.d.ts
@@ -1,3 +1,5 @@
+import { default as Accordion } from './components/accordion/Accordion.tsx';
+import { default as Avatar } from './components/avatar/Avatar.tsx';
 import { default as Badge } from './components/badge/Badge.tsx';
 import { default as Banner } from './components/banner/Banner.tsx';
 import { default as Breadcrumbs } from './components/breadcrumbs/Breadcrumbs.tsx';
@@ -7,6 +9,7 @@ import { default as Checkbox } from './components/checkbox/Checkbox.tsx';
 import { default as CheckboxGroup } from './components/checkboxGroup/CheckboxGroup.tsx';
 import { default as Chip } from './components/chip/Chip.tsx';
 import { default as Divider } from './components/divider/Divider.tsx';
+import { default as Drawer } from './components/drawer/Drawer.tsx';
 import { default as Grid } from './components/grid/Grid.tsx';
 import { default as Icon } from './components/icon/Icon.tsx';
 import { default as IconButton } from './components/iconButton/IconButton.tsx';
@@ -32,4 +35,5 @@ import { default as BaseTextInput } from './components/textInput/baseTextuInput/
 import { default as Toast } from './components/toast/Toast.tsx';
 import { default as ToastQueueProvider } from './components/toast/toastQueue/ToastQueueProvider.tsx';
 import { default as Tooltip } from './components/tooltip/Tooltip.tsx';
-export { Button, Grid, Checkbox, CheckboxGroup, Link, RadioButton, RadioButtonGroup, Icon, IconButton, IconButtonToggle, Loader, SplitButton, Tooltip, BaseTextInput as TextInput, Chip, Badge, Textarea, Divider, Switch, SegmentedControl, Breadcrumbs, Banner, Popover, Modal, SideNav, Stepper, BaseSideNav, Card, Toast, ToastQueueProvider, Tag, Tab, Searchbar, Select, };
+import { default as Treeview } from './components/treeview/Treeview.tsx';
+export { Avatar, Accordion, Button, Grid, Checkbox, CheckboxGroup, Link, RadioButton, RadioButtonGroup, Icon, IconButton, IconButtonToggle, Loader, SplitButton, Tooltip, BaseTextInput as TextInput, Chip, Badge, Textarea, Divider, Drawer, Switch, SegmentedControl, Breadcrumbs, Banner, Popover, Modal, SideNav, Stepper, BaseSideNav, Card, Toast, ToastQueueProvider, Tag, Tab, Treeview, Searchbar, Select, };
