1d8347c8ef1f23e3c13f6c788d7ac6722c7cd7df
jeremyrts <jeremyrts@users.noreply.github.com>
2026-04-13 15:01:48 +0000

    deploy: 7e6105c316b3fb04b62d40fea30ffed6205a2bcd

---
+++ preview/dsr-208/react/components/fileupload/FileUpload.d.ts
@@ -0,0 +1,3 @@
+import { FileUploadProps } from '../../../../core';
+declare const FileUpload: ({ id, compactSpacing, label, showLabel, required, showLabelRequirement, disabled, assistiveTextLabel, assistiveAppearance, showAssistiveIcon, assistiveTextLink, isError, multiple, buttonLabel, accept, onChange, onUpload, errorFilesMap, onRemovingFile, }: FileUploadProps) => import("react/jsx-runtime").JSX.Element;
+export default FileUpload;
--- preview/dsr-208/react/react/src/components/fileupload/FileUpload.d.ts
+++
@@ -1,3 +0,0 @@
-import { FileUploadProps } from '../../../../core';
-declare const FileUpload: ({ id, compactSpacing, label, showLabel, required, showLabelRequirement, disabled, assistiveTextLabel, assistiveAppearance, showAssistiveIcon, assistiveTextLink, isError, multiple, buttonLabel, accept, onChange, errorFilesMap, onRemovingFile, }: FileUploadProps) => import("react/jsx-runtime").JSX.Element | null;
-export default FileUpload;
