52acf1f2f78b01252f2383aa95257fc9c5b8c1ee
DiazAilan <DiazAilan@users.noreply.github.com>
2026-04-22 15:13:01 +0000

    deploy: 7b5eafa2ad269d37fd61a6dc0cc5e2598a48abfd

---
+++ preview/dsr-180/react/components/icon/generated/VolumeDownOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgVolumeDownOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgVolumeDownOutlined;
