06a2cae2c385985bbfca56d26ca4e066b824e00d
DiazAilan <DiazAilan@users.noreply.github.com>
2026-04-23 12:23:36 +0000

    deploy: 181160edcddc0d4a0fea73bb19928d8de2476667

--- preview/dsr-663/angular/components/time-picker/time-picker.component.ts
+++ preview/dsr-663/angular/components/time-picker/time-picker.component.ts
@@ -333,7 +333,7 @@ export class TimePickerComponent implements ControlValueAccessor {
       return;
     }
     const key = event.key;
-    if ([ARROW_UP_KEY, ARROW_DOWN_KEY, ARROW_LEFT_KEY, ARROW_RIGHT_KEY, BACKSPACE_KEY, DELETE_KEY].includes(key)) {
+    if ([ARROW_LEFT_KEY, ARROW_RIGHT_KEY, BACKSPACE_KEY, DELETE_KEY].includes(key)) {
       event.preventDefault();
       this.handleFunctionKey(key);
       return;
@@ -381,32 +381,6 @@ export class TimePickerComponent implements ControlValueAccessor {
         this.moveToNextSegment();
         this.scheduleSelectionRangeForSegment(this.activeTimeSegment());
       }
-    } else if ([ARROW_UP_KEY, ARROW_DOWN_KEY].includes(key)) {
-      this.adjustActiveSegmentByArrow(key === ARROW_UP_KEY ? 1 : -1);
-    }
-  }
-
-  private adjustActiveSegmentByArrow(direction: 1 | -1): void {
-    if (this.isCurrentSegmentReadOnly()) {
-      return;
-    }
-    const activeSegment = this.activeTimeSegment();
-    if (activeSegment === TimeSegmentEnum.HOURS) {
-      if (direction === 1) {
-        this.increaseHours();
-      } else {
-        this.decreaseHours();
-      }
-    } else if (activeSegment === TimeSegmentEnum.MINUTES) {
-      if (direction === 1) {
-        this.increaseMinutes();
-      } else {
-        this.decreaseMinutes();
-      }
-    } else if (direction === 1) {
-      this.increaseSeconds();
-    } else {
-      this.decreaseSeconds();
     }
   }
 
