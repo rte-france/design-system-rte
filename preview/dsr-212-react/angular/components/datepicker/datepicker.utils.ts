f4847219ea99b4b24cc3cd5960321882e14b04e6
jeremyrts <jeremyrts@users.noreply.github.com>
2026-04-29 10:21:00 +0000

    deploy: 9a7042d0efa210f399a8b0291e75c884f9b78718

--- preview/dsr-212-react/angular/components/datepicker/datepicker.utils.ts
+++ preview/dsr-212-react/angular/components/datepicker/datepicker.utils.ts
@@ -1,29 +1,9 @@
-export type DatepickerCalendarType = "day" | "month" | "year";
-
-export type DatepickerDayCellType = "default" | "today" | "selected" | "prev/next" | "empty";
-
-export interface DatepickerDayCell {
-  readonly date: Date;
-  readonly label: string;
-  readonly cellType: DatepickerDayCellType;
-  readonly isDisabled: boolean;
-}
-
-export interface DatepickerMonthCell {
-  readonly monthIndex: number;
-  readonly label: string;
-  readonly isDisabled: boolean;
-  readonly isCurrent: boolean;
-  readonly isSelected: boolean;
-}
-
-export interface DatepickerYearCell {
-  readonly year: number;
-  readonly label: string;
-  readonly isDisabled: boolean;
-  readonly isCurrent: boolean;
-  readonly isSelected: boolean;
-}
+import {
+  DatepickerDayCell,
+  DatepickerDayCellType,
+  DatepickerMonthCell,
+  DatepickerYearCell,
+} from "@design-system-rte/core";
 
 function getTodayStart(): Date {
   return startOfDay(new Date());
