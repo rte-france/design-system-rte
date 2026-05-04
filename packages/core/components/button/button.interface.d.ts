50d18224f660162421bcb50b85be28102c2b0a4e
Gonzalo Diaz Ailan <diaz.ailan.gonzalo@gmail.com>
2026-05-04 17:30:20 +0200

    test (ci): test default lane ci actions

--- packages/core/components/button/button.interface.d.ts
+++ packages/core/components/button/button.interface.d.ts
@@ -12,4 +12,5 @@ export interface ButtonProps extends BadgeHolderProps {
   iconPosition?: ButtonIconPosition;
   icon?: string;
   iconAppearance?: ButtonIconAppearance;
+  testProperty?: string;
 }
