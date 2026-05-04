626622a1c7127dd007ef3eb9defc6df9701b8171
jeremyrts <jeremyrts@users.noreply.github.com>
2026-05-04 14:00:10 +0000

    deploy: 762d089157ce350e2098bc567b34a6994952b955

---
+++ preview/ci-maintenance/angular/components/text-input/base-text-input/base-text-input.component.ts
@@ -0,0 +1,86 @@
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, computed, effect, input, signal } from "@angular/core";
+
+import { IconComponent } from "../../icon/icon.component";
+import { RegularIconIdKey, TogglableIconIdKey } from "../../icon/icon.service";
+import { IconButtonComponent } from "../../icon-button/icon-button.component";
+import { BaseInputComponent } from "../../input/base-input/base-input.component";
+import { RteBaseInputControlDirective } from "../../input/rte-base-input-control.directive";
+import { LinkComponent } from "../../link/link.component";
+
+@Component({
+  selector: "rte-base-text-input",
+  imports: [CommonModule, IconComponent, IconButtonComponent, LinkComponent, RteBaseInputControlDirective],
+  standalone: true,
+  templateUrl: "../../input/base-input/base-input.component.html",
+  styleUrl: "../../input/base-input/base-input.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BaseTextInputComponent extends BaseInputComponent {
+  readonly rightIconAction = input<"clean" | "visibilityOn" | "visibilityOff">("clean");
+  readonly isHiddenInput = signal<boolean>(false);
+  readonly visibilityIcons = computed(() => ["visibilityOn", "visibilityOff"].includes(this.rightIconAction()));
+
+  constructor() {
+    super();
+
+    effect(
+      () => {
+        if (this.visibilityIcons()) {
+          this.isHiddenInput.set(this.rightIconAction() === "visibilityOn");
+        } else {
+          this.isHiddenInput.set(false);
+        }
+      },
+      { allowSignalWrites: true },
+    );
+  }
+
+  protected override computeInputType(): "text" | "password" {
+    return this.isHiddenInput() ? "password" : "text";
+  }
+
+  protected override computeRightIconName(): RegularIconIdKey | TogglableIconIdKey | null {
+    if (this.visibilityIcons()) {
+      return this.isHiddenInput() ? "visibility-show" : "visibility-hide";
+    }
+    if (this.rightIconAction() === "clean") {
+      return "cancel";
+    }
+    return null;
+  }
+
+  protected override computeRightIconAriaLabel(): string {
+    if (this.visibilityIcons()) {
+      return this.isHiddenInput() ? "show text" : "hide text";
+    }
+    if (this.rightIconAction() === "clean") {
+      return "clear";
+    }
+    return "";
+  }
+
+  protected override handleRightIconClick(): void {
+    if (this.rightIconAction() === "clean") {
+      this.internalValue.set("");
+      this.valueChange.emit("");
+      this.characterCount.set(0);
+    }
+    if (this.visibilityIcons()) {
+      this.isHiddenInput.set(!this.isHiddenInput());
+    }
+  }
+
+  protected override computeShouldShowRightIcon(): boolean {
+    if (this.readOnly() || this.disabled()) {
+      return false;
+    }
+    if (!this.showRightIcon()) {
+      return false;
+    }
+    if (this.rightIconAction() === "clean") {
+      return !!this.internalValue()?.length;
+    }
+    return this.visibilityIcons();
+  }
+}
---
+++ preview/ci-maintenance/react/react/src/components/icon/generated/LeftPanelOpenOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgLeftPanelOpenOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgLeftPanelOpenOutlined;
