85cd1905956788e402c4fa54415f0a2bf719418d
DiazAilan <DiazAilan@users.noreply.github.com>
2026-05-06 14:05:33 +0000

    deploy: bcf90f59d04d4b09ae5f1680109b61bab40d4398

---
+++ preview/dsr-713/react/react/src/components/icon/generated/SwapHoriz.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgSwapHoriz: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgSwapHoriz;
