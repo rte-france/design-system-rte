41457456d94836e84e418858b986326622910895
DiazAilan <DiazAilan@users.noreply.github.com>
2026-06-04 09:46:43 +0000

    deploy: 7d7de4b2878beff9459de3b7c0d15d9bb16ea974

---
+++ preview/main/angular/components/checkbox-group/checkbox-group.component.stories.ts
@@ -0,0 +1,116 @@
+import { Meta, StoryObj } from "@storybook/angular";
+import { userEvent, within, expect } from "@storybook/test";
+
+import { CheckboxGroupComponent } from "./checkbox-group.component";
+
+const meta: Meta<CheckboxGroupComponent> = {
+  title: "Composants/CheckboxGroup",
+  component: CheckboxGroupComponent,
+  tags: ["autodocs"],
+  argTypes: {
+    items: {
+      control: "object",
+      defaultValue: ["Option 1", "Option 2", "Option 3"],
+    },
+    direction: {
+      control: "select",
+      options: ["horizontal", "vertical"],
+      defaultValue: "horizontal",
+    },
+    showItemsLabel: {
+      control: "boolean",
+      defaultValue: true,
+    },
+    groupTitle: {
+      control: "text",
+      defaultValue: "Checkbox Group Title",
+    },
+    showGroupTitle: {
+      control: "boolean",
+      defaultValue: true,
+    },
+    groupHelpText: {
+      control: "text",
+      defaultValue: "This is a help text for the checkbox group.",
+    },
+    showHelpText: {
+      control: "boolean",
+      defaultValue: true,
+    },
+    errorMessage: {
+      control: "text",
+      defaultValue: "This is an error message. Please select an option.",
+    },
+    error: {
+      control: "boolean",
+      defaultValue: false,
+    },
+    disabled: {
+      control: "boolean",
+      defaultValue: false,
+    },
+    readOnly: {
+      control: "boolean",
+      defaultValue: false,
+    },
+  },
+};
+export default meta;
+type Story = StoryObj<CheckboxGroupComponent>;
+
+export const Default: Story = {
+  args: {
+    items: ["Option 1", "Option 2", "Option 3"],
+    direction: "horizontal",
+    showItemsLabel: true,
+    groupTitle: "Checkbox Title",
+    showGroupTitle: true,
+    groupHelpText: "This is a help text for the checkbox group.",
+    showHelpText: true,
+    errorMessage: "This is an error message. Please select an option.",
+    error: false,
+    disabled: false,
+    readOnly: false,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const radioButton = canvas.getByLabelText("Option 1");
+    await userEvent.click(radioButton);
+    expect(radioButton).toBeChecked();
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
+};
+
+export const Error: Story = {
+  args: {
+    ...Default.args,
+    error: true,
+  },
+};
+
+export const ReadOnly: Story = {
+  args: {
+    ...Default.args,
+    readOnly: true,
+  },
+};
+
+export const Vertical: Story = {
+  args: {
+    ...Default.args,
+    direction: "vertical",
+  },
+};
+
+export const Horizontal: Story = {
+  args: {
+    ...Default.args,
+    direction: "horizontal",
+  },
+};
