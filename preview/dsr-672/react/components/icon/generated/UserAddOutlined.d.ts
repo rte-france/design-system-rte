546672b0c1b3cc1f647986ab210d34237f089f13
jeremyrts <jeremyrts@users.noreply.github.com>
2026-06-09 12:49:01 +0000

    deploy: 2807c12703495916326b7ac8ffa69371a4748091

---
+++ preview/dsr-672/react/components/icon/generated/UserAddOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgUserAddOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgUserAddOutlined;
