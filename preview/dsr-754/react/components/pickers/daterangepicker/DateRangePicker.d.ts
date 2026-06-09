5c9be0d44be2ccd86d117bce29eb2589980cac5d
jeremyrts <jeremyrts@users.noreply.github.com>
2026-06-09 16:13:03 +0000

    deploy: ac8ad5f9f840db2baaf5625194903a910ae26abd

---
+++ preview/dsr-754/react/components/pickers/daterangepicker/DateRangePicker.d.ts
@@ -0,0 +1,3 @@
+import { DateRangePickerProps } from '../../../../../core';
+declare const DateRangePicker: ({ id, label, labelId, required, showLabelRequirement, value, onChange, hasAction, onValidate, onCancel, minDate, maxDate, disabledDates, disabled, assistiveTextLabel, assistiveAppearance, showAssistiveIcon, assistiveTextLink, isError, readonly, }: DateRangePickerProps) => import("react/jsx-runtime").JSX.Element;
+export default DateRangePicker;
