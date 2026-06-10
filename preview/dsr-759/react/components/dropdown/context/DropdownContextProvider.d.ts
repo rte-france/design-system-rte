2fea014cb40dad9bc8e38cb144b2d21c2984dddb
jeremyrts <jeremyrts@users.noreply.github.com>
2026-06-10 10:45:14 +0000

    deploy: eb5af40909269398113f3882bf64f73fea89a62c

---
+++ preview/dsr-759/react/components/dropdown/context/DropdownContextProvider.d.ts
@@ -0,0 +1,6 @@
+export declare const DropdownContextProvider: ({ dropdownId, closeRoot, autoClose, children, }: {
+    dropdownId: string;
+    autoClose?: boolean;
+    closeRoot: () => void;
+    children: React.ReactNode;
+}) => import("react/jsx-runtime").JSX.Element;
---
+++ preview/dsr-759/react/components/segmentedControl/SegmentedControlUtils.d.ts
@@ -0,0 +1,4 @@
+export declare const focusNextNotSegmentElement: (currentFocusedElementIndex: number, allFocusableElement: HTMLElement[], criteria: string) => void;
+export declare const focusPreviousNotSegmentElement: (currentFocusedElementIndex: number, allFocusableElement: HTMLElement[], criteria: string) => void;
+export declare const focusNextSegmentElement: (currentActiveSegmentElementIndex: number, allSegmentElements: HTMLElement[]) => void;
+export declare const focusPreviousSegmentElement: (currentActiveSegmentElementIndex: number, allSegmentElements: HTMLElement[]) => void;
