78bece673ed97b736600f866b0bdc1b5e80e7102
jeremyrts <jeremyrts@users.noreply.github.com>
2026-06-11 15:49:51 +0000

    deploy: 72d708310ab800b4ef5091e1ea559a44cce26dba

---
+++ preview/dsr-779/react/components/icon/generated/FormatAlignLeft.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgFormatAlignLeft: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgFormatAlignLeft;
---
+++ preview/dsr-779/react/components/icon/generated/VolumeMuteOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgVolumeMuteOutlined: ({ width, height, fill, ...props }: IconProps) => import("react/jsx-runtime").JSX.Element;
+export default SvgVolumeMuteOutlined;
