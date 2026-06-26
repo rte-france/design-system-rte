cf8847543b2858b8c5e53f4607d7148d4930c085
jeremyrts <jeremyrts@users.noreply.github.com>
2026-06-26 14:21:25 +0000

    deploy: 34c1126262205dfaf7b02bb4c712ea0e5908ff3c

--- preview/dsr-733/angular/components/select/select.component.ts
+++ preview/dsr-733/angular/components/select/select.component.ts
@@ -13,6 +13,7 @@ import {
   TemplateRef,
 } from "@angular/core";
 import { NG_VALUE_ACCESSOR } from "@angular/forms";
+import { IconSize } from "@design-system-rte/core";
 import {
   SELECT_DROPDOWN_OFFSET,
   THRESHOLD_BOTTOM_POSITION,
@@ -80,6 +81,9 @@ export class SelectComponent implements AfterViewInit {
   readonly withSelectAll = input<boolean>(false);
   readonly optionToDisplay = input<SelectProps["optionToDisplay"]>("first-selected");
   readonly width = input<number | string>("350px");
+  readonly compactSpacing = input<boolean>(false);
+  readonly placeholder = input<string>();
+  readonly variant = input<SelectProps["variant"]>("default");
 
   readonly selectRef = viewChild<ElementRef<HTMLElement>>("selectRef");
   readonly buttonsContainerRef = viewChild<ElementRef<HTMLElement>>("buttonsContainerRef");
@@ -143,6 +147,16 @@ export class SelectComponent implements AfterViewInit {
 
   readonly shouldDisplayErrorIcon = computed(() => this.isError() && !this.disabled() && !this.readOnly());
 
+  readonly shouldDisplaySelectedIcon = computed(() => {
+    return (
+      this.variant() === "visibly-selected" &&
+      !this.shouldDisplayErrorIcon() &&
+      !this.disabled() &&
+      !this.readOnly() &&
+      !!this.internalValue()
+    );
+  });
+
   readonly isActive = signal(false);
 
   readonly selectWidth = computed(() => {
@@ -160,6 +174,8 @@ export class SelectComponent implements AfterViewInit {
     }
     return `${this.width()}px`;
   });
+
+  readonly iconSize = computed(() => (this.compactSpacing() ? IconSize["s"] : IconSize["m"]));
 
   ngAfterViewInit() {
     this.regenerateOptionsFormatted();
