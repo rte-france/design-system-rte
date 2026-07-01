92e4f883d1db6c3ac8d2d3d8d46cab04ee80e424
Gonzalo Diaz Ailan <diaz.ailan.gonzalo@gmail.com>
2026-07-01 17:38:13 +0200

    fix (Textarea): remove placeholder props insertion

--- packages/react/src/components/utils/index.ts
+++ packages/react/src/components/utils/index.ts
@@ -1,5 +1,15 @@
 import clsx from "clsx";
+import { TextareaHTMLAttributes } from "react";
 
 export function concatClassNames(...classes: (string | undefined | null | false)[]): string {
   return clsx(...classes);
+}
+
+export function deleteFromProps<Props extends TextareaHTMLAttributes<HTMLTextAreaElement>>(
+  props: Props,
+  target: string,
+): Props {
+  const newProps = { ...props };
+  delete newProps[target as keyof Props];
+  return newProps;
 }
