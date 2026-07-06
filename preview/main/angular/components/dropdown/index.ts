bf0bb1ba14cb8910792644fb6ff22fd1771e996e
DiazAilan <DiazAilan@users.noreply.github.com>
2026-07-06 08:19:23 +0000

    deploy: 26f2bc01170506b5576172f6cae6f171cd28490c

---
+++ preview/main/angular/components/dropdown/index.ts
@@ -0,0 +1,7 @@
+export { DropdownModule } from "./dropdown.module";
+
+export { DropdownDirective } from "./dropdown.directive";
+export { DropdownTriggerDirective } from "./dropdown-trigger/dropdown-trigger.directive";
+export { DropdownMenuComponent } from "./dropdown-menu/dropdown-menu.component";
+export { DropdownItemComponent } from "./dropdown-item/dropdown-item.component";
+export type { DropdownItemConfig } from "./dropdown.types";
