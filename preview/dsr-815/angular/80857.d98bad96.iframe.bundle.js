072dc133ac8b5ac04863bebff9f7e08624b22af1
DiazAilan <DiazAilan@users.noreply.github.com>
2026-07-15 16:00:57 +0000

    deploy: 15d925ebb532f5083b53be53c336ad2d01af791d

---
+++ preview/dsr-815/angular/80857.d98bad96.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_design_system_rte_angular_builder=self.webpackChunk_design_system_rte_angular_builder||[]).push([[80857],{"./projects/ds-rte-lib/src/lib/assets/icons/clock_outlined.svg"(module,__unused_webpack_exports,__webpack_require__){module.exports=__webpack_require__.p+"static/media/clock_outlined.5308339b.svg"}}]);
---
+++ preview/dsr-815/react/components/icon/generated/TrendingDown.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgTrendingDown: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgTrendingDown;
