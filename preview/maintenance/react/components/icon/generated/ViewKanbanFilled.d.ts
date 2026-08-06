c3f5c48872e2029ef4d2b58f9cd2999c32cee363
jeremyrts <jeremyrts@users.noreply.github.com>
2026-08-06 15:35:10 +0000

    deploy: 9bcc73673492cdd7e85ebe15b456a2114c5f3a1b

---
+++ preview/maintenance/react/components/icon/generated/ViewKanbanFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgViewKanbanFilled: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgViewKanbanFilled;
