2f76cf8ce89058b509f9fed097d9dbf6e444b4f1
DiazAilan <DiazAilan@users.noreply.github.com>
2026-08-06 09:02:34 +0000

    deploy: 87c57cf82e9e71604353346baa43db8750e21aa9

---
+++ preview/dsr-630/react/components/dynamicTab/dynamictab.utils.d.ts
@@ -0,0 +1,2 @@
+import { DynamicTabItemOption } from '../../../../core';
+export declare const buildNewTab: (newTabConfig: DynamicTabItemOption | undefined, internalOptions: DynamicTabItemOption[]) => DynamicTabItemOption | null;
