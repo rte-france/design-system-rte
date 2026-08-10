621cc6a85ffa42b6d5a93e35f479ae983440dfa4
jeremyrts <jeremyrts@users.noreply.github.com>
2026-08-10 07:38:03 +0000

    deploy: f4cb7ee904691fc0dfc6d1f565b2804c13470cc7

---
+++ preview/add-tag/react/components/utils/index.d.ts
@@ -0,0 +1,3 @@
+import { TextareaHTMLAttributes } from '../../../../../node_modules/react';
+export declare function concatClassNames(...classes: (string | undefined | null | false)[]): string;
+export declare function deleteFromProps<Props extends TextareaHTMLAttributes<HTMLTextAreaElement>>(props: Props, target: string): Props;
