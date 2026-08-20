134d940ce6f74577e8c6431812f1ebfc5eaa58b6
DiazAilan <DiazAilan@users.noreply.github.com>
2026-08-20 13:44:07 +0000

    deploy: cc7f996117b99a967f5ccbd28d623c8cf3759fc3

---
+++ preview/dsr-874/react/assets/IconButton.module-DsipBz7u.js
@@ -0,0 +1,1 @@
+const t={"icon-button":"_icon-button_1w2tu_1"};export{t as i};
---
+++ preview/dsr-874/react/components/icon/generated/TextSnippetFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgTextSnippetFilled: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgTextSnippetFilled;
