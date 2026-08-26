18974de0b91858fcbf5a398f62b67164b1a93942
jeremyrts <jeremyrts@users.noreply.github.com>
2026-08-26 13:32:49 +0000

    deploy: 8fc0e3ab01036e7fc18f88a0913010edc4bcf251

---
+++ preview/dsr-897/react/components/icon/generated/CalendarMonthFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgCalendarMonthFilled: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgCalendarMonthFilled;
