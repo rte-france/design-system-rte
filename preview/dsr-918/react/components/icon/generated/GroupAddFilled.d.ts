6214aa6db226a5f83bdac6b7e425a4bb199c0f70
jeremyrts <jeremyrts@users.noreply.github.com>
2026-08-27 15:17:33 +0000

    deploy: 4f43ccf193f29e9f8a5ee63a87882a9a1623b284

---
+++ preview/dsr-918/react/components/icon/generated/GroupAddFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgGroupAddFilled: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgGroupAddFilled;
