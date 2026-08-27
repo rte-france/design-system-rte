d93dfed78b1beb3f09291c80565202ce6ad329b9
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2026-08-27 17:09:36 +0200

    fix(banner): hide decorative status icon + allow banner to be announced on screen reader

--- packages/react/src/components/banner/Banner.tsx
+++ packages/react/src/components/banner/Banner.tsx
@@ -1,7 +1,12 @@
-import { CloseButtonAriaLabel } from "@design-system-rte/core";
-import { BannerProps as CoreBannerProps } from "@design-system-rte/core/components/banner/banner.interface";
-import { IconSize } from "@design-system-rte/core/components/icon/icon.constants";
-import { forwardRef, useEffect, useState } from "react";
+import {
+  CloseButtonAriaLabel,
+  bannerTypeLabel,
+  BannerProps as CoreBannerProps,
+  IconSize,
+  generateId,
+  iconTypeMap,
+} from "@design-system-rte/core";
+import { forwardRef, useEffect, useRef, useState } from "react";
 
 import useAnimatedMount from "../../hooks/useAnimatedMount";
 import Button from "../button/Button";
@@ -13,12 +18,12 @@ import style from "./Banner.module.scss";
 
 interface BannerProps extends CoreBannerProps, React.HTMLAttributes<HTMLDivElement> {}
 
-const Banner = forwardRef<HTMLDivElement, BannerProps>(
+const Banner = forwardRef<HTMLElement, BannerProps>(
   (
     {
       title,
       message,
-      type = "alert",
+      type = "info",
       closable = false,
       onClose,
       position = "push",
@@ -35,46 +40,68 @@ const Banner = forwardRef<HTMLDivElement, BannerProps>(
     const ariaRole = type === "error" ? "alert" : "status";
     const { shouldRender, isAnimating } = useAnimatedMount(isInternalOpen, 200);
 
-    const iconTypeMap: Record<string, string> = {
-      info: "info",
-      error: "dangerous",
-      success: "check-circle",
-      warning: "warning",
-    };
+    const bannerRef = useRef<HTMLElement | null>(null);
+    const customId = generateId();
+
+    const titleId = `banner-title-${customId}`;
+    const messageId = `banner-message-${customId}`;
 
     useEffect(() => {
       setIsInternalOpen(isOpen);
     }, [isOpen]);
 
-    if (closable === false && position === "overlay") {
-      console.warn("Banner: 'closable' and 'onClose' props must be set when 'position' has 'overlay' value.");
-      return null;
-    }
+    useEffect(() => {
+      if (type === "error" && shouldRender) {
+        bannerRef.current?.focus();
+      }
+    }, [shouldRender, type]);
 
     const handleOnClose = () => {
       setIsInternalOpen(false);
       onClose?.();
     };
 
+    if (closable === false && position === "overlay") {
+      console.warn("Banner: 'closable' and 'onClose' props must be set when 'position' has 'overlay' value.");
+      return null;
+    }
+
     return (
       shouldRender && (
         <section
           role={ariaRole}
-          className={concatClassNames(style["banner"], className)}
-          ref={ref}
+          className={concatClassNames(style["rte-banner"], className)}
+          ref={(node) => {
+            bannerRef.current = node;
+            if (typeof ref === "function") {
+              ref(node);
+            } else if (ref) {
+              ref.current = node;
+            }
+          }}
+          aria-labelledby={title ? titleId : undefined}
+          aria-describedby={message ? messageId : undefined}
           data-type={type}
           data-position={position}
           data-compact={isCompact ? "true" : undefined}
           data-open={isAnimating || undefined}
+          tabIndex={-1}
           {...props}
         >
-          <div className={style["banner-icon"]}>
-            <Icon name={iconTypeMap[type]} size={32} />
+          <div className={style["rte-banner-icon"]}>
+            <Icon name={iconTypeMap[type]} size={32} aria-hidden="true" />
           </div>
-          <div className={style["banner-content"]}>
-            {title && <span className={style["banner-title"]}>{title}</span>}
+          <div className={style["rte-banner-content"]}>
+            {title && (
+              <>
+                <span id={titleId} className={style["rte-banner-title"]}>
+                  <span className={style["rte-sr-only"]}>{bannerTypeLabel[type]} </span>
+                  {title}
+                </span>
+              </>
+            )}
             {message && (
-              <div className={style["banner-message"]}>
+              <div id={messageId} className={style["rte-banner-message"]}>
                 <span>{message}</span>
               </div>
             )}
@@ -91,7 +118,7 @@ const Banner = forwardRef<HTMLDivElement, BannerProps>(
           {closable && (
             <button
               type="button"
-              className={concatClassNames(iconButtonStyle["icon-button"], style["banner-close-button"])}
+              className={concatClassNames(iconButtonStyle["icon-button"], style["rte-banner-close-button"])}
               data-size="m"
               data-variant="neutral"
               data-compact-spacing={true}
