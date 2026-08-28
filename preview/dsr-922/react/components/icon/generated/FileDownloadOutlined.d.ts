57d29b35a491d7eb0773b036540d30f81506d357
DiazAilan <DiazAilan@users.noreply.github.com>
2026-08-28 15:38:31 +0000

    deploy: 7628a6dd4fe4d8ed026e03cc7dd5707aad39909e

---
+++ preview/dsr-922/react/components/icon/generated/FileDownloadOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgFileDownloadOutlined: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgFileDownloadOutlined;
