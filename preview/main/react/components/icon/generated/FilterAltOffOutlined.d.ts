579d45e66997e39fbd07f58dfd37ade4c6edb632
DiazAilan <DiazAilan@users.noreply.github.com>
2026-08-31 08:43:41 +0000

    deploy: 7bf8a19330929856a47bd687f512ffe03b1290b5

---
+++ preview/main/react/components/icon/generated/FilterAltOffOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgFilterAltOffOutlined: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgFilterAltOffOutlined;
