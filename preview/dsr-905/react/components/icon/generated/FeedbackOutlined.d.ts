8cabbde13544ffe297d1caf45958b3a52d1291c1
DiazAilan <DiazAilan@users.noreply.github.com>
2026-08-31 13:53:20 +0000

    deploy: b634702176008f1ab429b5b7f173d12d6ef289f7

---
+++ preview/dsr-905/react/components/icon/generated/FeedbackOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgFeedbackOutlined: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgFeedbackOutlined;
