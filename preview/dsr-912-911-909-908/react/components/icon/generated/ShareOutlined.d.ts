0d90d9a1c35c7cd2cc6af26e7a08a396fcb57da9
jeremyrts <jeremyrts@users.noreply.github.com>
2026-08-31 14:37:03 +0000

    deploy: 6268069779d8ecda9d0214f2c4c36d831ca71f99

---
+++ preview/dsr-912-911-909-908/react/components/icon/generated/ShareOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgShareOutlined: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgShareOutlined;
