da03094f656c8699863e9d4be167226d7be5e244
DiazAilan <DiazAilan@users.noreply.github.com>
2026-08-31 15:16:52 +0000

    deploy: 68a3afbd9c38f59effc17e9bb7c03afb41a3ca35

---
+++ preview/dsr-906/react/components/icon/generated/CalendarAvailableOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgCalendarAvailableOutlined: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgCalendarAvailableOutlined;
