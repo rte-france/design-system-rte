8835d2ea63fbc17482de5c136bbe4017ead96e13
DiazAilan <DiazAilan@users.noreply.github.com>
2026-09-01 09:47:44 +0000

    deploy: 67ab0843a1cee38ae0067ec5c2970e3cc90f5ae8

---
+++ preview/dsr-924/react/components/icon/generated/FolderMoveOutlined.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgFolderMoveOutlined: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgFolderMoveOutlined;
---
+++ preview/dsr-924/react/components/pickers/baseInputPicker/BaseInputPicker.d.ts
@@ -0,0 +1,20 @@
+import { BaseInputProps } from '../../../../../core/components/common/input/input.interface';
+export interface BaseInputPickerProps extends BaseInputProps<string> {
+    onChange?: () => void;
+    icon?: string;
+    isFocused?: boolean;
+    pickerInputRef?: React.RefObject<HTMLInputElement> | ((element: HTMLInputElement) => void);
+    pickerRef?: React.RefObject<HTMLDivElement>;
+    onFocus?: () => void;
+    onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+    onMouseUp?: (event: React.MouseEvent<HTMLInputElement>) => void;
+    onMouseDown?: () => void;
+    onOpenPicker?: () => void;
+    onBlur?: () => void;
+    ariaLabelledBy?: string;
+    ariaDescribedBy?: string;
+    openButtonAriaLabel?: string;
+    fixedWidth?: boolean;
+}
+declare const BaseInputPicker: ({ id, value, readOnly, icon, onChange, onFocus, onKeyDown, onMouseUp, onMouseDown, onOpenPicker, onBlur, isFocused, pickerInputRef, pickerRef, ariaLabelledBy, ariaDescribedBy, isError, disabled, openButtonAriaLabel, fixedWidth, }: BaseInputPickerProps) => import("react").JSX.Element;
+export default BaseInputPicker;
