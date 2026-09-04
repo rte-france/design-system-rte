70698c8f73a75c4809164eadcd9b93f87baa4dc4
DiazAilan <DiazAilan@users.noreply.github.com>
2026-09-04 13:53:40 +0000

    deploy: 990dec4f924ffd3e696d5966e0ecfd99732c746a

---
+++ preview/dsr-931/react/components/breadcrumbs/Breadcrumbs.d.ts
@@ -0,0 +1,5 @@
+import { BreadcrumbsProps as BreadcrumbsPropsCore } from '../../../../core/components/breadcrumbs/breadcrumbs.interface';
+export interface BreadcrumbsProps extends BreadcrumbsPropsCore, React.HTMLAttributes<HTMLElement> {
+}
+declare const Breadcrumbs: import('../../../../../node_modules/react').ForwardRefExoticComponent<BreadcrumbsProps & import('../../../../../node_modules/react').RefAttributes<HTMLDivElement>>;
+export default Breadcrumbs;
