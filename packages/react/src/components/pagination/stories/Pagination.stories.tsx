8bf529886601a255970f037cc7cf02ce50846a66
Gonzalo Diaz Ailan <diaz.ailan.gonzalo@gmail.com>
2026-09-09 15:22:22 +0200

    docs (Pagination): add documentation

--- packages/react/src/components/pagination/stories/Pagination.stories.tsx
+++ packages/react/src/components/pagination/stories/Pagination.stories.tsx
@@ -159,6 +159,7 @@ export const SinglePage: Story = {
   },
 };
 export const Interactions: Story = {
+  tags: ["!autodocs"],
   args: {
     ...Default.args,
     appearance: "brand",
@@ -197,6 +198,7 @@ export const Interactions: Story = {
 };
 
 export const KeyboardNavigation: Story = {
+  tags: ["!autodocs"],
   args: {
     ...Default.args,
     appearance: "neutral",
