9f164e553b38ee3763dee23fa6721b01a9a0235a
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2026-09-10 15:09:07 +0200

    fix(file-item) : add descriptive text for status icon

--- packages/react/src/components/loader/Loader.tsx
+++ packages/react/src/components/loader/Loader.tsx
@@ -1,5 +1,6 @@
 import { loaderSize } from "@design-system-rte/core/components/loader/loader.constants";
 import { LoaderProps as CoreLoaderProps } from "@design-system-rte/core/components/loader/loader.interface";
+import { generateId } from "@design-system-rte/core/dist";
 import { forwardRef } from "react";
 
 import { concatClassNames } from "../utils";
@@ -9,7 +10,21 @@ import style from "./Loader.module.scss";
 interface LoaderProps extends CoreLoaderProps, React.HTMLAttributes<HTMLDivElement> {}
 
 const Loader = forwardRef<HTMLDivElement, LoaderProps>(
-  ({ appearance = "brand", size = "medium", label, labelPosition = "right", className, ...props }, ref) => {
+  (
+    {
+      appearance = "brand",
+      size = "medium",
+      label,
+      labelPosition = "right",
+      className,
+      "aria-label": ariaLabel,
+      "aria-hidden": ariaHidden,
+      ...props
+    },
+    ref,
+  ) => {
+    const loaderId = "loader" + generateId();
+
     return (
       <div
         className={concatClassNames(style.loader, className)}
@@ -18,6 +33,11 @@ const Loader = forwardRef<HTMLDivElement, LoaderProps>(
         ref={ref}
         {...props}
       >
+        {!!label && (
+          <span id={`${loaderId}-label`} className={style.loaderLabel}>
+            {label}
+          </span>
+        )}
         {appearance == "brand" ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -26,6 +46,9 @@ const Loader = forwardRef<HTMLDivElement, LoaderProps>(
             className={style.svgLoader}
             viewBox="0 0 48 48"
             fill="none"
+            aria-labelledby={label ? `${loaderId}-label` : undefined}
+            aria-label={label ?? ariaLabel}
+            aria-hidden={ariaHidden}
           >
             <g clipPath="url(#paint0_angular_4438_67_clip_path)" data-figma-skip-parse="true">
               <g transform="matrix(0.0240793 7.6297e-08 -7.62968e-08 0.0240017 24.0001 24.0003)">
@@ -87,7 +110,14 @@ const Loader = forwardRef<HTMLDivElement, LoaderProps>(
           </svg>
         )}
         {label && (
-          <span role="status" aria-live="polite" className={style.label} data-size={size} data-appearance={appearance}>
+          <span
+            id={`${loaderId}-label`}
+            role="status"
+            aria-live="polite"
+            className={style.label}
+            data-size={size}
+            data-appearance={appearance}
+          >
             {label}
           </span>
         )}
