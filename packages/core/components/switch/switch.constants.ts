953236bc23b2a797c09d1216b7a5533648596968
Jérémy Ratsimandresy <jeremy.ratsimandresy@gmail.com>
2026-09-21 11:56:42 +0200

    fix(switch): enforce accessibility

--- packages/core/components/switch/switch.constants.ts
+++ packages/core/components/switch/switch.constants.ts
@@ -1,2 +1,5 @@
 export const switchHeight = 24;
 export const switchWidth = 40;
+
+export const SWITCH_MISSING_ACCESSIBLE_NAME_ERROR =
+  "Switch requires a label, an aria-labelledby, or an aria-label to provide an accessible name.";
