5b574a929e5c9aafffa9d87eb75752db63a229ff
Gonzalo Diaz Ailan <diaz.ailan.gonzalo@gmail.com>
2026-09-21 10:19:05 +0200

    fix (release): correct missplaced import statements from react

--- packages/react/src/components/fileupload/file-item/FileItem.tsx
+++ packages/react/src/components/fileupload/file-item/FileItem.tsx
@@ -1,6 +1,6 @@
 import { FileItemProps, generateId, getTextWidth } from "@design-system-rte/core";
 import { extractFileNameParts, formatFileSize } from "@design-system-rte/core/components/file-upload/file-upload.util";
-import { FILE_UPLOAD_ITEM_ARIA_LABELS } from "@design-system-rte/core/dist/components/file-upload/file-upload.constants";
+import { FILE_UPLOAD_ITEM_ARIA_LABELS } from "@design-system-rte/core/components/file-upload/file-upload.constants";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 import AssistiveText from "../../assistivetext/AssistiveText";
--- packages/react/src/components/loader/Loader.tsx
+++ packages/react/src/components/loader/Loader.tsx
@@ -1,6 +1,6 @@
 import { loaderSize } from "@design-system-rte/core/components/loader/loader.constants";
 import { LoaderProps as CoreLoaderProps } from "@design-system-rte/core/components/loader/loader.interface";
-import { generateId } from "@design-system-rte/core/dist";
+import { generateId } from "@design-system-rte/core";
 import { forwardRef } from "react";
 
 import { concatClassNames } from "../utils";
