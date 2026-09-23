9f39fe8929765ed594f4e6c1aae70bcd74db24b7
DiazAilan <DiazAilan@users.noreply.github.com>
2026-09-23 10:06:15 +0000

    deploy: 894a0deb90465ba67c0882a93d56b32ce0034f38

---
+++ preview/dsr-974/angular/components/avatar/docs/Overview.md
@@ -0,0 +1,7 @@
+```ts
+import { AvatarComponent } from "@design-system-rte/angular";
+```
+
+```html
+<rte-avatar layout="image" [imgSrc]="userImage" alt="Avatar de Jane Doe"></rte-avatar>
+```
---
+++ preview/dsr-974/react/components/icon/generated/ArrowCircleDownFilled.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from '../Icon';
+declare const SvgArrowCircleDownFilled: ({ width, height, fill, ...props }: IconProps) => import("react").JSX.Element;
+export default SvgArrowCircleDownFilled;
